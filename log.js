8ed8fa661683d91a646ca0d1a19842f45f7ca3cf
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2017-08-31 14:42:40 +0200

    removing config.json from log

--- log.js
+++ log.js
@@ -1,13 +1,17 @@
 const winston = require('winston');
-const config = require('./config.json');
+const config = {
+  "console": "debug",
+  "file": "error"
+}
+
 
 module.exports = new (winston.Logger)({
   transports: [
     new (winston.transports.Console)(
-      {timestamp: true, colorize: true, level: config.log.console}
+      {timestamp: true, colorize: true, level: config.console}
     ),
     new winston.transports.File(
-      {filename: './error.log', level: config.log.file}
+      {filename: './error.log', level: config.file}
     )
   ],
   exitOnError: true
