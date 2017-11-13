7df6e56ea7fef491863d28fbcaa8e5d1ca36f773
Adam Wegrzynek <awegrzyn@users.noreply.github.com>
2017-11-13 15:22:48 +0000

    Give up on jQuery UI widgets (OGUI-101) (#33)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   "root": true,
   "extends": ["eslint:recommended", "google"],
   "env": {
+    "es6": true,
     "node": true,
     "jquery" : true,
     "browser": true,
@@ -17,6 +18,7 @@ module.exports = {
     "max-len": ['error', {code: 100}], // default 80
     "indent": ['error', 2, {"SwitchCase": 1}], // not set by default
     "comma-dangle": ["error", "never"], // do not enforce trailing comma
-   "curly": [2, "all"] // enfornce curly braces even in single line if-else
+    "curly": [2, "all"], // enfornce curly braces even in single line if-else
+    "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }]
   }
 }
