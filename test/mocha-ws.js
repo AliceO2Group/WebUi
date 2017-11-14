1dbf64ec40a5d46e83b71c1e540ee2a55923d0c3
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2017-11-14 12:57:12 +0100

    removed console log

--- test/mocha-ws.js
+++ test/mocha-ws.js
@@ -9,7 +9,6 @@ let http;
 
 describe('websocket', () => {
   before(() => {
-    console.log(config.http);
     http = new HttpServer(config.http, config.jwt, config.oAuth);
   });
   it('connection should be dropped due to invalid oAuth token', (done) => {
