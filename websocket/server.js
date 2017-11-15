e16f2e91d8bfb6435be68af1722df97d2eab4a9f
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2017-11-15 14:12:19 +0100

    moved from try,catch to promises

--- websocket/server.js
+++ websocket/server.js
@@ -55,50 +55,46 @@ class WebSocket {
    * @return {object} message to be send back to the user
    */
   onmessage(message) {
-    if (typeof message === 'undefined') {
-      return;
-    }
-    const responseArray = [];
-    let feedback = this.jwtVerify(message.token);
-    if (feedback instanceof Response && feedback.getcode != 440) {
-      return [feedback];
-    } else if (feedback instanceof Response && feedback.getcode == 440) {
-      responseArray.push(feedback);
-      feedback = this.jwtVerify(feedback.getpayload.newtoken);
-    }
-    message.id = feedback.id;
-
-    log.debug('%d : command %s', message.id, message.command);
-    if (this.callbackArray.hasOwnProperty(message.command)) {
-      const response = this.callbackArray[message.command](message);
-      responseArray.push(response);
-    } else {
-      responseArray.push(new Response(404));
-    }
-    return responseArray;
+    return new Promise((resolve, reject) => {
+      if (typeof message === 'undefined') {
+        reject([new Response(403)]);
+      }
+      const responseArray = [];
+      this.jwtVerify(message.token)
+        .then((data) => {
+          if (data.newToken) {
+            responseArray.push(
+              new Response(440).command('new-token').payload({newtoken: data.newToken})
+            );
+          }
+          message.id = data.id;
+          log.debug('%d : command %s', message.id, message.command);
+          if (this.callbackArray.hasOwnProperty(message.command)) {
+            responseArray.push(this.callbackArray[message.command](message));
+          } else {
+            responseArray.push(new Response(404));
+          }
+          resolve(responseArray);
+        }, () => {
+          reject([new Response(401)]);
+        });
+    });
   }
 
   /**
    * Verifies token, if expired requests a new one.
    * @param {object} token - JWT token
-   * @param {bool} refresh - whether try to refresh token when expired or not
    * @return {object} includes either parsed token or response message
    */
-  jwtVerify(token, refresh = true) {
-    try {
-      return this.http.jwt.verify(token);
-    } catch (err) {
-      log.warn('jwt verify failed: %s', err.message);
-      if (err.name == 'TokenExpiredError' && refresh) {
-        const newtoken = this.http.jwt.refreshToken(token);
-        if (newtoken === false) {
-          return new Response(403);
-        }
-        return new Response(440).command('new-token').payload({newtoken: newtoken});
-      } else {
-        return new Response(401);
-      }
-    }
+  jwtVerify(token) {
+    return new Promise((resolve, reject) => {
+      this.jwt.verify(token)
+        .then((data) => {
+          resolve(data);
+        }, (err) => {
+          reject(err);
+        });
+    });
   }
 
   /**
@@ -117,19 +113,23 @@ class WebSocket {
           if (parsed.command == 'filter') {
             client.filter = new Function('return ' + parsed.filter.toString())();
           }
-          const response = this.onmessage(parsed);
-          for (let message of response) {
-            if (message.getcommand == undefined) {
-              message.command(parsed.command);
-            }
-            if (message.getbroadcast) {
-              log.debug('broadcast : command %s sent', message.getcommand);
-              this.broadcast(message.json);
-            } else {
-              log.debug('command %s sent', message.getcommand);
-              client.send(JSON.stringify(message.json));
-            }
-          }
+          this.onmessage(parsed)
+            .then((responses) => {
+              for (let response of responses) {
+                if (response.getcommand == undefined) {
+                  response.command(parsed.command);
+                }
+                if (response.getbroadcast) {
+                  log.debug('broadcast : command %s sent', response.getcommand);
+                  this.broadcast(response.json);
+                } else {
+                  log.debug('command %s sent', response.getcommand);
+                  client.send(JSON.stringify(response.json));
+                }
+              }
+            }, (response) => {
+              client.send(JSON.stringify(response.json));
+            });
         });
         client.on('close', () => this.onclose());
         client.on('pong', () => client.isAlive = true);
