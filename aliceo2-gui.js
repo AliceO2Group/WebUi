85a2ad6f72deba079fbd0582d21c9a7348964e5b
Adam Wegrzynek <awegrzyn@users.noreply.github.com>
2017-11-16 16:33:21 +0000

    Relese 1.1.0 (#42)

--- aliceo2-gui.js
+++ aliceo2-gui.js
@@ -9,13 +9,13 @@
 const ZeroMQClient = require('./zeromq/client');
 const WebSocket = require('./websocket/server');
 const HttpServer = require('./http/server');
-const Response = require('./websocket/response.js');
+const WebSocketMessage = require('./websocket/message.js');
 const Log = require('./log.js');
 const MySQL = require('./db/mysql.js');
 
 exports.ZeroMQClient = ZeroMQClient;
 exports.WebSocket = WebSocket;
 exports.HttpServer = HttpServer;
-exports.Response = Response;
+exports.WebSocketMessage = WebSocketMessage;
 exports.Log = Log;
 exports.MySQL = MySQL;
