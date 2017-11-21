96c15a4a413d1a4928c28ac21797ebb9a89a4a9b
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2017-11-21 16:57:12 +0100

    initial working winston logger

--- test/mocha-log.js
+++ test/mocha-log.js
@@ -1,12 +1,22 @@
 const assert = require('assert');
 const fs = require('fs');
-const log = require('./../log.js');
+const log = require('./../log/log.js');
+
+const config = {
+  winston: {
+    file: './error.log',
+    fileLvl: 'error',
+    consoleLvl: 'debug'
+  }
+};
 
 describe('error-log', () => {
-  it('should generate error file', () => {
+  it('should generate error file', (done) => {
+    log.configure(config);
     log.error('Test error log entry');
     setTimeout(() => {
       assert.ok(fs.existsSync('./error.log'));
-    }, 1000);
+      done();
+    }, 100);
   });
 });
