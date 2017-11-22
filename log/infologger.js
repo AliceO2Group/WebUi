7043c800b5dfd9a3c4f0d01dcf79720bbc4b675b
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2017-11-22 11:46:31 +0100

    reformatted

--- log/infologger.js
+++ log/infologger.js
@@ -1,3 +1,65 @@
+const net = require('net');
+const EventEmitter = require('events');
+
+const protocols = [
+  {
+    version: '1.3', 
+    fields: [
+      {type: String, name: 'severity'},
+      {type: Number, name: 'level'},
+      {type: Number, name: 'timestamp'},
+      {type: String, name: 'hostname'},
+      {type: String, name: 'rolename'},
+      {type: Number, name: 'pid'},
+      {type: String, name: 'username'},
+      {type: String, name: 'system'},
+      {type: String, name: 'facility'},
+      {type: String, name: 'detector'},
+      {type: String, name: 'partition'},
+      {type: String, name: 'dest'},
+      {type: Number, name: 'run'},
+      {type: Number, name: 'errcode'},
+      {type: Number, name: 'errline'},
+      {type: String, name: 'errsource'},
+      {type: String, name: 'message'}
+    ]
+  },
+  {
+    version: '1.4',
+    fields: [
+      {name: 'severity', type: String},
+      {name: 'level', type: Number},
+      {name: 'timestamp', type: String},
+      {name: 'hostname', type: String},
+      {name: 'rolename', type: String},
+      {name: 'pid', type: Number},
+      {name: 'username', type: String},
+      {name: 'system', type: String},
+      {name: 'facility', type: String},
+      {name: 'detector', type: String},
+      {name: 'partition', type: String},
+      {name: 'run', type: Number},
+      {name: 'errcode', type: Number},
+      {name: 'errline', type: Number},
+      {name: 'errsource', type: String},
+      {name: 'message', type: String}
+    ]
+  }
+];
+
+
 class InfoLogger {
+
+  connect() {
+  }
+
+  disconnect() {
+  }
+
+  send() {
+  }
+
+  parse() {
+  }
 }
 module.exports = InfoLogger;
--- log/winston.js
+++ log/winston.js
@@ -1,24 +1,42 @@
 const winston = require('winston');
 
+/**
+ * Creates Winston logger
+ * Uses two transports file and console (if properly configured)
+ * @author Adam Wegrzynek <adam.wegrzynek@cern.ch>
+ */
 class Winston {
+  /** Creates two transports and constructs a logger
+   * @param {object} config configuration for console and file transports
+   */
   constructor(config) {
     const consoleFormat = winston.format.printf((info) => {
       return `${info.timestamp} ${info.level}: ${info.message}`;
     });
 
-  
+    config.consoleLvl = config.consoleLvl || 'debug';
+
+    let transports = [
+      new (winston.transports.Console)({
+        level: config.consoleLvl,
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.colorize(),
+          consoleFormat
+        )}
+      )
+    ];
+
+    if (config.file) {
+      transports.push(new winston.transports.File(
+        {filename: config.file, level: config.fileLvl}
+      ));
+    }
+
     this.instance = winston.createLogger({
-      transports: [
-        new (winston.transports.Console)(
-          {level: config.consoleLvl,
-           format: winston.format.combine(winston.format.timestamp(), winston.format.colorize(), consoleFormat)}
-        ),  
-        new winston.transports.File(
-          {filename: config.file, level: config.fileLvl}
-        )   
-      ],  
+      transports: transports,
       exitOnError: true
-    });  
+    });
   }
 }
 module.exports = Winston;
