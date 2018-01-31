85d28be38e8adda9cbba8e8507696d7d2e4d4d80
Adam Wegrzynek <awegrzyn@users.noreply.github.com>
2018-01-31 12:09:35 +0100

    Remove chai from tests (#55)

--- test/mocha-http.js
+++ test/mocha-http.js
@@ -3,88 +3,102 @@ process.env.NODE_ENV = 'test';
 const assert = require('assert');
 const config = require('./../config.json');
 const JwtToken = require('./../jwt/token.js');
-const chai = require('chai');
-const chaiHttp = require('chai-http');
 const HttpServer = require('./../http/server');
+const http = require('http');
 
 // as CERN cerfiticates are not signed by any CA
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-chai.use(chaiHttp);
 
-let http;
+let httpServer;
 const jwt = new JwtToken(config.jwt);
 const token = jwt.generateToken(0, 'test', 1);
 
 describe('rest-api', () => {
   before(() => {
-    http = new HttpServer(config.http, config.jwt, config.oAuth);
-    http.get('/get-request', (req, res) => res.json({ok: 1}));
-    http.post('/post-request', (req, res) => res.json({ok: 1}));
+    httpServer = new HttpServer(config.http, config.jwt, config.oAuth);
+    httpServer.get('/get-request', (req, res) => res.json({ok: 1}));
+    httpServer.post('/post-request', (req, res) => res.json({ok: 1}));
   });
+
   it('GET with token should respond 200/JSON', (done) => {
-    chai.request(http.getServer)
-      .get('/api/get-request')
-      .query({token: token})
-      .end((err, res) => {
-        assert.strictEqual(err, null);
-        assert.strictEqual(res.status, 200);
-        assert.strictEqual(res.body.ok, 1);
-        done();
-      });
+    http.get('http://localhost:' + config.http.port + '/api/get-request?token=' + token,
+      (res) => {
+        assert.strictEqual(res.statusCode, 200);
+        let rawData = '';
+        res.on('data', (chunk) => {
+          rawData += chunk;
+        });
+        res.on('end', () => {
+          const parsedData = JSON.parse(rawData);
+          assert.strictEqual(parsedData.ok, 1);
+          done();
+        });
+      }
+    );
   });
 
   it('GET with an incorrect token should respond 403', (done) => {
-    chai.request(http.getServer)
-      .get('/api/get-request')
-      .query({token: 'wrong token'})
-      .end((err, res) => {
-        assert.strictEqual(res.status, 403);
+    http.get('http://localhost:' + config.http.port + '/api/get-request?token=wrong',
+      (res) => {
+        assert.strictEqual(res.statusCode, 403);
         done();
-      });
+      }
+    );
   });
 
   it('GET without a token should respond 403', (done) => {
-    chai.request(http.getServer)
-      .get('/api/get-request')
-      .end((err, res) => {
-        assert.strictEqual(res.status, 403);
+    http.get('http://localhost:' + config.http.port + '/api/get-request',
+      (res) => {
+        assert.strictEqual(res.statusCode, 403);
         done();
-      });
+      }
+    );
   });
 
   it('GET with an incorrect path should respond 404', (done) => {
-    chai.request(http.getServer)
-      .get('/api/get-wrong-request')
-      .query({token: token})
-      .end((err, res) => {
-        assert.strictEqual(res.status, 404);
+    http.get('http://localhost:' + config.http.port + '/api/get-wrong?token=' + token,
+      (res) => {
+        assert.strictEqual(res.statusCode, 404);
         done();
-      });
+      }
+    );
   });
 
   it('POST with correct token should respond 200/JSON', (done) => {
-    chai.request(http.getServer)
-      .post('/api/post-request')
-      .query({token: token})
-      .end((err, res) => {
-        assert.strictEqual(err, null);
-        assert.strictEqual(res.status, 200);
-        assert.strictEqual(res.body.ok, 1);
+    const req = http.request({
+      hostname: 'localhost',
+      port: config.http.port,
+      path: '/api/post-request?token=' + token,
+      method: 'POST'
+    }, (res) => {
+      assert.strictEqual(res.statusCode, 200);
+      let rawData = '';
+      res.on('data', (chunk) => {
+        rawData += chunk;
+      });
+      res.on('end', () => {
+        const parsedData = JSON.parse(rawData);
+        assert.strictEqual(parsedData.ok, 1);
         done();
       });
+    });
+    req.end();
   });
 
   it('POST with an incorrect token should respond 403', (done) => {
-    chai.request(http.getServer)
-      .post('/api/post-request')
-      .query({token: 'wrong token'})
-      .end((err, res) => {
-        assert.strictEqual(res.status, 403);
-        done();
-      });
+    const req = http.request({
+      hostname: 'localhost',
+      port: config.http.port,
+      path: '/api/post-request?token=wrong',
+      method: 'POST'
+    }, (res) => {
+      assert.strictEqual(res.statusCode, 403);
+      done();
+    });
+    req.end();
   });
 
   after(() => {
-    http.getServer.close();
+    httpServer.getServer.close();
   });
 });
