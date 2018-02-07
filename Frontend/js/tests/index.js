728418ec577d0f46a1db965cf5ccba44b440215f
Vladimir Kosmala <vladimirkosmala@users.noreply.github.com>
2018-02-07 15:59:19 +0100

    Added frontend tests

--- Frontend/js/tests/index.js
+++
@@ -1,8 +0,0 @@
-
-// TODO: Observable methods
-// TODO: hyperscript outputs
-// TODO: mount debounced
-// TODO: mount reacting to model
-// TODO: mount updating well
-// TODO: mount lifecyles
-// TODO: mount key sorting
