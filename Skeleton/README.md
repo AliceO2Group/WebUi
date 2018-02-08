c09f044f6131407fb3d3b1df11f63c74ebddc1d7
Vladimir Kosmala <vladimirkosmala@users.noreply.github.com>
2018-02-08 18:05:09 +0100

    Added skeleton

---
+++ Skeleton/README.md
@@ -0,0 +1,14 @@
+### Quick start a project
+
+```bash
+git clone this_repo
+mv this_repo/Skeleton MyProject
+rm this_repo
+cd MyProject
+npm install
+# configure config.js, see Backend Guide, TODO: link here
+# configure package.json if needed
+npm start
+open https://...
+```
+
---
+++ Skeleton/index.js
@@ -0,0 +1,35 @@
+// Import the backend framework
+const {HttpServer, Log, WebSocket, WebSocketMessage} = require('Gui');
+
+// Activate debug logs
+Log.configure({winston: {}});
+
+// Import config and utils
+const config = require('./config.js');
+const path = require('path');
+
+// Instanciate the HTTP and WebSocket servers
+const http = new HttpServer(config.http, config.jwt, config.oAuth);
+const ws = new WebSocket(http);
+
+// Declare a simple model to be modified
+let serverCount = 0;
+
+// Declare some HTTP API (under /api path)
+http.post('/setCounter', (req, res) => {
+  serverCount = req.query.count
+  res.json({count: serverCount});
+});
+
+// Declare some WS API
+ws.bind('hello', (body) => Log.info(JSON.stringify(body)));
+
+setInterval(() => {
+  ws.broadcast(new WebSocketMessage(200).setCommand('serverCountUpdate').setPayload({count: serverCount++}));
+}, 500);
+
+// Don't exit on errors
+process.on('uncaughtException', function (err) {
+  Log.error(err);
+  console.trace(err); // local debug
+});
