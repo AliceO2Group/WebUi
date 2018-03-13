d8de56044f05f75859788f326c5d951de8f930f3
Adam Wegrzynek <awegrzyn@users.noreply.github.com>
2018-03-13 15:26:46 +0100

    Init merging Gui repos into one (#95)

---
+++ .travis.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+get_changed_projects_list () {
+  UPSTREAM_BRANCH="$1"
+
+  # get list of files changed
+  CHANGED_FILES=$(git diff --name-only HEAD $(git merge-base HEAD ${UPSTREAM_BRANCH}))
+  for dir in $CHANGED_FILES; do
+
+    # remove filenames
+    dir=$(dirname $dir);
+
+    # keep root directories only
+    dir=$(echo "$dir" | awk -F "/" '{print $1}');
+
+    # append array
+    CHANGED_PROJECTS+=("$dir");
+  done
+
+  # remove duplicates
+  CHANGED_PROJECTS=($(echo "${CHANGED_PROJECTS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '));
+
+  # remove "." from array
+  CHANGED_PROJECTS=("${CHANGED_PROJECTS[@]/.}");
+}
+
+get_changed_projects_list $TRAVIS_BRANCH
+
+for project in ${CHANGED_PROJECTS[@]}; do
+  cd ${project}; touch TEST; cd ..
+done
