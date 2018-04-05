7cc1db76322910b567e8b191d4e88612f4cca5dc
Vladimir Kosmala <vladimirkosmala@users.noreply.github.com>
2018-04-05 18:20:52 +0200

    Simplification of chart rendering

--- QualityControl/public/app/object/objectDraw.js
+++ QualityControl/public/app/object/objectDraw.js
@@ -4,6 +4,11 @@ import {objectId, clone, pointerId} from '../utils.js';
 
 /**
  * Draw an object using JSROOT
+ * if name change, re-create entire div
+ * if data are loaded, redraw jsroot
+ * if data change, redraw jsroot
+ * if data did not change but model did, resize jsroot after CSS animations and debouncer
+ *
  * @param {object} model - root model object
  * @param {TabObject|string} tabObject - the tabObject to draw, can be the name of object
  * @param {object} options - optional options of presentation
@@ -29,59 +34,46 @@ export function draw(model, tabObject, options) {
     };
   }
 
-  const drawingOptions = tabObject.options.join(';');
-
-  function cacheHash(tabObject) {
-    // help to identify when some data have changed to tell jsroot to redraw
-    // pointerId returns a different number if object has been replaced by another
-    return `${tabObject.name}:${model.object.objects[tabObject.name] ? pointerId(model.object.objects[tabObject.name]) : null}:${drawingOptions}`;
-  }
-
   const attributes = {
-    alt: cacheHash(tabObject),
+    alt: cacheHash(model, tabObject),
     key: tabObject.name, // completly re-create this div if the chart is not the same at all
     class: options.className,
     style: {height: options.height, width: options.width},
 
     oncreate(vnode) {
       model.object.loadObject(tabObject.name);
-
-      // Already loaded, let's draw in the div created
-      if (model.object.objects[tabObject.name]) {
-        // cache control
-        vnode.dom.dataset.cacheHash = cacheHash(tabObject);
-        if (!vnode.dom.resizer) {
-          vnode.dom.resizer = timerDebouncer(() => JSROOT.resize(vnode.dom), 200);
-        }
-        setTimeout(() => {
-          JSROOT.redraw(vnode.dom, model.object.objects[tabObject.name], drawingOptions)
-        }, 0);
-      }
+      vnode.dom.onresize = timerDebouncer(() => JSROOT.resize(vnode.dom), 200);
+      window.addEventListener('resize', vnode.dom.onresize);
+      redrawOnDataUpdates(model, vnode.dom, tabObject);
     },
 
     onupdate(vnode) {
-      if (model.object.objects[tabObject.name] && vnode.dom.dataset.cacheHash !== cacheHash(tabObject)) {
-        vnode.dom.dataset.cacheHash = cacheHash(tabObject);
-        if (!vnode.dom.resizer) {
-          vnode.dom.resizer = timerDebouncer(() => JSROOT.resize(vnode.dom), 200);
-        }
-        setTimeout(() => {
-          JSROOT.redraw(vnode.dom, model.object.objects[tabObject.name], drawingOptions)
-        }, 0);
-      } else if (vnode.dom.dataset.cacheHash) {
-        // in case other data are updated we do a simple resize of jsroot if window has changed size
-        // but we do that after 0.2s to avoid slowing down CSS animations while computing resizing
-        // this increase user experience, animations need to be fluid
-        vnode.dom.resizer();
-      }
+      redrawOnDataUpdates(model, vnode.dom, tabObject);
     },
 
     onremove(vnode) {
       // remove jsroot binding to avoid memory leak
       model.object.unloadObject(tabObject.name);
       JSROOT.cleanup(vnode.dom);
+      window.removeEventListener('resize', vnode.dom.onresize);
     }
   };
 
   return h('div', attributes);
+}
+
+function redrawOnDataUpdates(model, dom, tabObject) {
+  if (model.object.objects[tabObject.name] && dom.dataset.cacheHash !== cacheHash(model, tabObject)) {
+    // cache control
+    dom.dataset.cacheHash = cacheHash(model, tabObject);
+    setTimeout(() => {
+      JSROOT.redraw(dom, model.object.objects[tabObject.name], tabObject.options.join(';'));
+    }, 0);
+  }
+}
+
+function cacheHash(model, tabObject) {
+  // help to identify when some data have changed to tell jsroot to redraw
+  // pointerId returns a different number if object has been replaced by another
+  return `${tabObject.name}:${model.object.objects[tabObject.name] ? pointerId(model.object.objects[tabObject.name]) : null}:${tabObject.options.join(';')}`;
 }
