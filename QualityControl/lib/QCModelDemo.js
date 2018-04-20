16361460df2889d3ccec096f14a7504ac676156d
Vladimir Kosmala <vladimirkosmala@users.noreply.github.com>
2018-04-20 13:08:46 +0200

    Eslint fixs, API in other file

--- QualityControl/lib/QCModelDemo.js
+++ QualityControl/lib/QCModelDemo.js
@@ -1,5 +1,3 @@
-const objectsFilenames = ['hpx', 'alice', 'canvas_tf1', 'gaussian', 'gaussian2', 'histo', 'root0'];
-
 // force user accounts during demo
 const ownerIdUser1 = 100;
 const ownerNameUser1 = 'John Doe';
@@ -12,7 +10,6 @@ const ownerNameUser2 = 'Samantha Smith';
  */
 
 // CRUD
-module.exports.readObject = readObject;
 module.exports.readObjectData = readObjectData;
 module.exports.listObjects = listObjects;
 
@@ -23,13 +20,11 @@ module.exports.createLayout = createLayout;
 module.exports.deleteLayout = deleteLayout;
 
 /**
- * Retrieve a monitoring object (TObject)
- * @param {string} agentName
- * @param {string} objectName
- * @return {object} javascript representation of monitoring object
+ * Fake promise latency
+ * @param {Any} data
+ * @return {Promise} data is returned
  */
-
-function PromiseResolveWithLatency(data) {
+function promiseResolveWithLatency(data) {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(data);
@@ -37,37 +32,50 @@ function PromiseResolveWithLatency(data) {
   });
 }
 
-function readObject(path) {
-  let object = objects.find(object => object.name === `${path}`);
-  return PromiseResolveWithLatency(object ? {name: object.name, quality: object.quality} : null);
-}
-
+/**
+ * Read object's data or null if it fails
+ * @param {string} name - Object's path like agentName/objectName/objectNameSub
+ * @return {Object|null}
+ */
 function readObjectData(name) {
-  let object = objects.find(object => object.name === `${name}`);
-  return PromiseResolveWithLatency(object ? object.data : null);
+  let object = objects.find((object) => object.name === `${name}`);
+  return promiseResolveWithLatency(object ? object.data : null);
 }
 
-// List all object without the data which are heavy
+/**
+ * List all object without the data which are heavy
+ * @return {Array<Layout>}
+ */
 function listObjects() {
-  return PromiseResolveWithLatency(objects.map(object => {
-    return {name: object.name, quality: object.quality}
+  return promiseResolveWithLatency(objects.map((object) => {
+    return {name: object.name, quality: object.quality};
   }));
 }
 
+/**
+ * Create a layout
+ * @param {Layout} layout
+ * @return {Object} Empty details
+ */
 function createLayout(layout) {
   layout.owner_id = ownerIdUser1;
   layout.owner_name = ownerNameUser1;
 
   layouts.push(layout);
-  return PromiseResolveWithLatency(layout);
+  return promiseResolveWithLatency({});
 }
 
+/**
+ * List layouts, can be filtered
+ * @param {Object} filter - undefined or {owner_id: XXX}
+ * @return {Array<Layout>}
+ */
 function listLayouts(filter = {}) {
   if (filter.owner_id) {
     filter.owner_id = ownerIdUser1;
   }
 
-  return PromiseResolveWithLatency(layouts.filter((layout) => {
+  return promiseResolveWithLatency(layouts.filter((layout) => {
     if (filter.owner_id && layout.owner_id !== filter.owner_id) {
       return false;
     }
@@ -76,27 +84,43 @@ function listLayouts(filter = {}) {
   }));
 }
 
+/**
+ * Retrieve a layout or null
+ * @param {string} layoutName - layout name
+ * @return {Layout|null}
+ */
 function readLayout(layoutName) {
-  return PromiseResolveWithLatency(layouts.find(layout => layout.name === layoutName));
+  return promiseResolveWithLatency(layouts.find((layout) => layout.name === layoutName));
 }
 
+/**
+ * Update a single layout by its name
+ * @param {string} layoutName
+ * @param {Layout} data
+ * @return {Object} Empty details
+ */
 function writeLayout(layoutName, data) {
-  const layout = layouts.find(layout => layout.name === layoutName);
+  const layout = layouts.find((layout) => layout.name === layoutName);
   if (!layout) {
     throw new Error('layout not found');
   }
   Object.assign(layout, data);
-  return PromiseResolveWithLatency(layout);
+  return promiseResolveWithLatency({});
 }
 
+/**
+ * Delete a single layout by its name
+ * @param {string} layoutName
+ * @return {Object} Empty details
+ */
 function deleteLayout(layoutName) {
-  const layout = layouts.find(layout => layout.name === layoutName);
+  const layout = layouts.find((layout) => layout.name === layoutName);
   if (!layout) {
     throw new Error(`layout ${layoutName} not found`);
   }
   const index = layouts.indexOf(layout);
   layouts.splice(index, 1);
-  return PromiseResolveWithLatency(null);
+  return promiseResolveWithLatency({});
 }
 
 // Fake data, not normalized but should be if inserted in relational DB
@@ -155,21 +179,34 @@ const objects = [
   {name: 'TST01/Default/hTOFRRawsTime', quality: 'good', data: graphs.canvas_tf1},
   {name: 'TST01/Default/hTOFRRawsToT', quality: 'good', data: graphs.histo},
   {name: 'TST01/Default/hTOFrefMap', quality: 'good', data: graphs.hpx},
-  ...Array.from({length: 2500}, (x, i) => ({name: `BIGTREE/120KB/${i}`, quality: 'good', data: graphs.hpx}))
+  ...Array.from({length: 2500}, (x, i) => (
+    {name: `BIGTREE/120KB/${i}`, quality: 'good', data: graphs.hpx}
+  ))
 ];
 
 const tabObject = [
-  {id: '5aba4a059b755d517e76ef51', options: ['logx'], name: 'DAQ01/EventSizeClasses/class_C0AMU-ABC', x: 0, y: 0, w: 2, h: 1},
-  {id: '5aba4a059b755d517e76ef52', options: ['logy'], name: 'DAQ01/EventSizeClasses/class_C0ALSR-ABC', x: 0, y: 0, w: 1, h: 1},
-  {id: '5aba4a059b755d517e76ef53', options: ['gridx', 'lego'], name: 'DAQ01/EquipmentSize/ACORDE/ACORDE', x: 0, y: 0, w: 1, h: 1},
-  {id: '5aba4a059b755d517e76ef54', options: ['gridx', 'lego'], name: 'DAQ01/EquipmentSize/CPV/CPV', x: 0, y: 0, w: 1, h: 1},
-  {id: '5aba4a059b755d517e76ef55', options: ['gridx', 'lego'], name: 'DAQ01/EquipmentSize/HMPID/HMPID', x: 0, y: 0, w: 1, h: 1},
-  {id: '5aba4a059b755d517e76ef56', options: [], name: 'DAQ01/EquipmentSize/ITSSDD/ITSSDD', x: 0, y: 0, w: 1, h: 1},
-  {id: '5aba4a059b755d517e76ef57', options: [], name: 'DAQ01/EquipmentSize/ITSSSD/ITSSSD', x: 0, y: 0, w: 1, h: 1},
-  {id: '5aba4a059b755d517e76ef58', options: [], name: 'DAQ01/EquipmentSize/TOF/TOF', x: 0, y: 0, w: 1, h: 1},
-  {id: '5aba4a059b755d517e76ef59', options: [], name: 'DAQ01/EquipmentSize/TPC/TPC', x: 0, y: 0, w: 1, h: 1},
-  {id: '5aba4a059b755d517e76ef50', options: [], name: 'DAQ01/EventSize/ACORDE/ACORDE', x: 0, y: 0, w: 1, h: 1},
-  {id: '5aba4a059b755d517e76ef60', options: [], name: 'DAQ01/EventSize/CPV/CPV', x: 0, y: 0, w: 1, h: 1},
+  {id: '5aba4a059b755d517e76ef51',
+    options: ['logx'], name: 'DAQ01/EventSizeClasses/class_C0AMU-ABC', x: 0, y: 0, w: 2, h: 1},
+  {id: '5aba4a059b755d517e76ef52',
+    options: ['logy'], name: 'DAQ01/EventSizeClasses/class_C0ALSR-ABC', x: 0, y: 0, w: 1, h: 1},
+  {id: '5aba4a059b755d517e76ef53',
+    options: ['gridx', 'lego'], name: 'DAQ01/EquipmentSize/ACORDE/ACORDE', x: 0, y: 0, w: 1, h: 1},
+  {id: '5aba4a059b755d517e76ef54',
+    options: ['gridx', 'lego'], name: 'DAQ01/EquipmentSize/CPV/CPV', x: 0, y: 0, w: 1, h: 1},
+  {id: '5aba4a059b755d517e76ef55',
+    options: ['gridx', 'lego'], name: 'DAQ01/EquipmentSize/HMPID/HMPID', x: 0, y: 0, w: 1, h: 1},
+  {id: '5aba4a059b755d517e76ef56',
+    options: [], name: 'DAQ01/EquipmentSize/ITSSDD/ITSSDD', x: 0, y: 0, w: 1, h: 1},
+  {id: '5aba4a059b755d517e76ef57',
+    options: [], name: 'DAQ01/EquipmentSize/ITSSSD/ITSSSD', x: 0, y: 0, w: 1, h: 1},
+  {id: '5aba4a059b755d517e76ef58',
+    options: [], name: 'DAQ01/EquipmentSize/TOF/TOF', x: 0, y: 0, w: 1, h: 1},
+  {id: '5aba4a059b755d517e76ef59',
+    options: [], name: 'DAQ01/EquipmentSize/TPC/TPC', x: 0, y: 0, w: 1, h: 1},
+  {id: '5aba4a059b755d517e76ef50',
+    options: [], name: 'DAQ01/EventSize/ACORDE/ACORDE', x: 0, y: 0, w: 1, h: 1},
+  {id: '5aba4a059b755d517e76ef60',
+    options: [], name: 'DAQ01/EventSize/CPV/CPV', x: 0, y: 0, w: 1, h: 1},
 ];
 
 const tabs = [
@@ -181,20 +218,36 @@ const tabs = [
 ];
 
 const layouts = [
-  {id: '5aba4a059b755d517e76ea10', owner_id: ownerIdUser1, name: 'PWG-HF', owner_name: ownerNameUser1, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea11', owner_id: ownerIdUser1, name: 'PWG-GA', owner_name: ownerNameUser1, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea12', owner_id: ownerIdUser1, name: 'AliRoot dashboard', owner_name: ownerNameUser1, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea13', owner_id: ownerIdUser1, name: 'PWG-CF', owner_name: ownerNameUser1, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea14', owner_id: ownerIdUser1, name: 'PWG-PP', owner_name: ownerNameUser1, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea15', owner_id: ownerIdUser2, name: 'Run Coordination', owner_name: ownerNameUser2, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea16', owner_id: ownerIdUser2, name: 'PWG-LF', owner_name: ownerNameUser2, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea17', owner_id: ownerIdUser2, name: 'PWG-DQ', owner_name: ownerNameUser2, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea18', owner_id: ownerIdUser2, name: 'PWG-JE', owner_name: ownerNameUser2, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea19', owner_id: ownerIdUser2, name: 'MC productions', owner_name: ownerNameUser2, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea21', owner_id: ownerIdUser2, name: 'Run Coordination 2017', owner_name: ownerNameUser2, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea22', owner_id: ownerIdUser2, name: 'O2 Milestones', owner_name: ownerNameUser2, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea23', owner_id: ownerIdUser2, name: 'O2 TDR', owner_name: ownerNameUser2, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea24', owner_id: ownerIdUser2, name: 'MC productions dashboard', owner_name: ownerNameUser2, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea25', owner_id: ownerIdUser2, name: 'DAQ System Dashboard', owner_name: ownerNameUser2, tabs: tabs},
-  {id: '5aba4a059b755d517e76ea26', owner_id: ownerIdUser2, name: 'PWG-LF (2)', owner_name: ownerNameUser2, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea10',
+    owner_id: ownerIdUser1, name: 'PWG-HF', owner_name: ownerNameUser1, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea11',
+    owner_id: ownerIdUser1, name: 'PWG-GA', owner_name: ownerNameUser1, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea12',
+    owner_id: ownerIdUser1, name: 'AliRoot dashboard', owner_name: ownerNameUser1, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea13',
+    owner_id: ownerIdUser1, name: 'PWG-CF', owner_name: ownerNameUser1, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea14',
+    owner_id: ownerIdUser1, name: 'PWG-PP', owner_name: ownerNameUser1, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea15',
+    owner_id: ownerIdUser2, name: 'Run Coordination', owner_name: ownerNameUser2, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea16',
+    owner_id: ownerIdUser2, name: 'PWG-LF', owner_name: ownerNameUser2, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea17',
+    owner_id: ownerIdUser2, name: 'PWG-DQ', owner_name: ownerNameUser2, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea18',
+    owner_id: ownerIdUser2, name: 'PWG-JE', owner_name: ownerNameUser2, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea19',
+    owner_id: ownerIdUser2, name: 'MC productions', owner_name: ownerNameUser2, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea21',
+    owner_id: ownerIdUser2, name: 'Run Coordination 2017', owner_name: ownerNameUser2, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea22',
+    owner_id: ownerIdUser2, name: 'O2 Milestones', owner_name: ownerNameUser2, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea23',
+    owner_id: ownerIdUser2, name: 'O2 TDR', owner_name: ownerNameUser2, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea24',
+    owner_id: ownerIdUser2, name: 'MC prod dashboard', owner_name: ownerNameUser2, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea25',
+    owner_id: ownerIdUser2, name: 'DAQ System Dashboard', owner_name: ownerNameUser2, tabs: tabs},
+  {id: '5aba4a059b755d517e76ea26',
+    owner_id: ownerIdUser2, name: 'PWG-LF (2)', owner_name: ownerNameUser2, tabs: tabs},
 ];
---
+++ QualityControl/lib/api.js
@@ -0,0 +1,191 @@
+const log = require('@aliceo2/web-ui').Log;
+let model; // lazy loaded on setup
+
+module.exports.setup = (config, http) => {
+  // Load data source (demo or DB)
+  model = config.app.demoData ? require('./QCModelDemo.js') : require('./QCModel.js');
+
+  http.post('/readObjectData', readObjectData);
+  http.get('/readObjectsData', readObjectsData);
+  http.post('/listObjects', listObjects);
+  http.post('/readLayout', readLayout);
+  http.post('/writeLayout', writeLayout);
+  http.get('/layout', listLayouts);
+  http.delete('/layout/:name', deleteLayout);
+  http.post('/layout', createLayout);
+};
+
+/**
+ * List all objects without data
+ * @param {Request} req
+ * @param {Response} res
+ */
+function listObjects(req, res) {
+  model.listObjects()
+    .then((data) => res.status(200).json(data))
+    .catch((err) => errorHandler(err, res));
+}
+
+/**
+ * List objects with data specified by objectName[]
+ * @param {Request} req
+ * @param {Response} res
+ */
+function readObjectsData(req, res) {
+  let objectName = req.query.objectName;
+
+  if (!objectName) {
+    res.status(400).send('parameter objectName is needed');
+    return;
+  }
+
+  if (!Array.isArray(objectName)) {
+    objectName = [objectName];
+  }
+
+  // Retrieve all data and fill a key-value object with it
+  const safeRetriever = (name) => model.readObjectData(name).catch((err) => errorHandler(err, res));
+  const promiseArray = objectName.map(safeRetriever);
+  Promise.all(promiseArray)
+    .then((results) => {
+      const resultsByName = {};
+      objectName.forEach((name, i) => {
+        resultsByName[name] = results[i];
+      });
+      res.status(200).json(resultsByName);
+    })
+    .catch((err) => errorHandler(err, res));
+}
+
+/**
+ * Read only data of an object specified by objectName
+ * @param {Request} req
+ * @param {Response} res
+ */
+function readObjectData(req, res) {
+  const objectName = req.query.objectName;
+
+  if (!objectName) {
+    res.status(400).send('parameter objectName is needed');
+    return;
+  }
+
+  model.readObjectData(objectName)
+    .then((data) => res.status(data ? 200 : 404).json(data))
+    .catch((err) => errorHandler(err, res));
+}
+
+/**
+ * List all layouts, can be filtered by owner_id
+ * @param {Request} req
+ * @param {Response} res
+ */
+function listLayouts(req, res) {
+  const filter = {
+    owner_id: parseInt(req.query.owner_id, 10)
+  };
+
+  model.listLayouts(filter)
+    .then((data) => res.status(200).json(data))
+    .catch((err) => errorHandler(err, res));
+}
+
+/**
+ * Read a single layout specified by layoutName
+ * @param {Request} req
+ * @param {Response} res
+ */
+function readLayout(req, res) {
+  const layoutName = req.query.layoutName;
+
+  if (!layoutName) {
+    res.status(400).send('layoutName parameter is needed');
+    return;
+  }
+
+  model.readLayout(layoutName)
+    .then((data) => res.status(data ? 200 : 404).json(data))
+    .catch((err) => errorHandler(err, res));
+}
+
+/**
+ * Update a single layout specified by layoutName and body
+ * @param {Request} req
+ * @param {Response} res
+ */
+function writeLayout(req, res) {
+  const layoutName = req.query.layoutName;
+  const data = req.body;
+
+  if (!layoutName) {
+    res.status(400).send('layoutName parameter is needed');
+    return;
+  }
+
+  if (!data) {
+    res.status(400).send('body is needed');
+    return;
+  }
+
+  model.writeLayout(layoutName, data)
+    .then((data) => res.status(200).json(data))
+    .catch((err) => errorHandler(err, res));
+}
+
+/**
+ * Delete a single layout specified by name
+ * @param {Request} req
+ * @param {Response} res
+ */
+function deleteLayout(req, res) {
+  const layoutName = req.params.name;
+
+  if (!layoutName) {
+    res.status(400).send('layoutName is needed');
+    return;
+  }
+
+  model.deleteLayout(layoutName)
+    .then((data) => res.status(204).json(data))
+    .catch((err) => errorHandler(err, res));
+}
+
+/**
+ * Create a layout specified by body
+ * @param {Request} req
+ * @param {Response} res
+ */
+function createLayout(req, res) {
+  const layout = req.body;
+
+  if (!layout.name) {
+    res.status(400).send('layout.name parameter is needed');
+    return;
+  }
+  if (!layout.owner_id) {
+    res.status(400).send('layout.owner_id parameter is needed');
+    return;
+  }
+  if (!layout.owner_name) {
+    res.status(400).send('layout.owner_name parameter is needed');
+    return;
+  }
+  if (!layout.tabs) {
+    res.status(400).send('layout.tabs parameter is needed');
+    return;
+  }
+
+  model.createLayout(layout)
+    .then((data) => res.status(201).json(data))
+    .catch((err) => errorHandler(err, res));
+}
+
+/**
+ * Global HTTP error handler, sends status 500
+ * @param {string} err - Message error
+ * @param {Response} res - Response object to send to
+ */
+function errorHandler(err, res) {
+  log.error(err);
+  res.status(500).send();
+}
