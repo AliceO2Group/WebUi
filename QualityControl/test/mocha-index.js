3f2ec00f1b498f21ca7166576c831c3c82d3e0b9
Vladimir Kosmala <vladimirkosmala@users.noreply.github.com>
2018-04-24 17:34:57 +0200

    Added tests index

---
+++ QualityControl/test/mocha-index.js
@@ -0,0 +1,90 @@
+const puppeteer = require('puppeteer');
+const assert = require('assert');
+
+// APIs:
+// https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md
+// https://mochajs.org/
+
+describe('QCG', function () {
+  let browser;
+  let page;
+  this.timeout(5000);
+  this.slow(1000);
+
+  before(async () => {
+    browser = await puppeteer.launch({
+      headless: true
+    });
+    page = await browser.newPage();
+  });
+
+  it('should load first page "/"', async () => {
+    await page.goto('http://localhost:8181/');
+  });
+
+  it('should have redirected to default page "/?page=layoutList"', async () => {
+    const location = await page.evaluate(() => window.location);
+    assert(location.search === '?page=layoutList');
+  });
+
+  it('should have a layout with header, sidebar and section', async () => {
+    const headerContent = await page.evaluate(() => document.querySelector('header').innerHTML);
+    const sidebarContent = await page.evaluate(() => document.querySelector('nav').innerHTML);
+    const sectionContent = await page.evaluate(() => document.querySelector('section').innerHTML);
+
+    assert(headerContent.includes('Quality Control'));
+    assert(sidebarContent.includes('Explore'));
+  });
+
+  describe('page layoutList', () => {
+    before(async () => {
+      await page.goto('http://localhost:8181/?page=layoutList', {waitUntil: 'networkidle0'});
+      const location = await page.evaluate(() => window.location);
+      assert(location.search === '?page=layoutList');
+    });
+
+    it('should have a table with rows', async () => {
+      const rowsCount = await page.evaluate(() => document.querySelectorAll('section table tbody tr').length);
+      assert(rowsCount > 1);
+    });
+
+    it('should have a table with one row after filtering', async () => {
+      await page.type('header input', 'AliRoot');
+      const rowsCount = await page.evaluate(() => document.querySelectorAll('section table tbody tr').length);
+      assert(rowsCount === 1);
+    });
+
+    it('should have a link to show a layout', async () => {
+      await page.evaluate(() => document.querySelector('section table tbody tr a').click());
+      const location = await page.evaluate(() => window.location);
+      assert(location.search === '?page=layoutShow&layout=AliRoot%20dashboard');
+    });
+  });
+
+  describe('page layoutShow', () => {
+    before(async () => {
+      await page.goto('http://localhost:8181/?page=layoutShow&layout=AliRoot%20dashboard', {waitUntil: 'networkidle0'});
+      const location = await page.evaluate(() => window.location);
+      assert(location.search === '?page=layoutShow&layout=AliRoot+dashboard');
+    });
+
+    it('should have tabs in the header', async () => {
+      const tabsCount = await page.evaluate(() => document.querySelectorAll('header .btn-tab').length);
+      assert(tabsCount > 1);
+    });
+
+    it('should have one edit button in the header', async () => {
+      const buttonCount = await page.evaluate(() => document.querySelectorAll('header > div > div:nth-child(3) > button').length);
+      assert.deepStrictEqual(buttonCount, 1);
+    });
+
+    it('should have jsroot svg plots in the section', async () => {
+      const plotsCount = await page.evaluate(() => document.querySelectorAll('section svg.jsroot').length);
+      assert(plotsCount > 1);
+    });
+  });
+
+  after(async () => {
+    await browser.close();
+  });
+});
