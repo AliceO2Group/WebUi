2cf1c0d4eac5640e8f64a5e933dd1d7786abf5cf
Vladimir Kosmala <vladimirkosmala@users.noreply.github.com>
2018-08-07 14:46:02 +0200

    Added logging menu dropdown to QCG

--- QualityControl/public/common/header.js
+++ QualityControl/public/common/header.js
@@ -1,35 +1,39 @@
-import {h, iconMenu} from '/js/src/index.js';
+import {h, iconMenu, iconPerson} from '/js/src/index.js';
 
 import spinner from '../loader/spinner.js'
 import layoutShowHeader from '../layout/layoutShowHeader.js'
 import layoutListHeader from '../layout/layoutListHeader.js'
 import objectTreeHeader from '../object/objectTreeHeader.js'
 
-export default function header(model) {
-  return h('.flex-row.p2', [
-    commonHeader(model),
-    headerSpecific(model)
-  ]);
-}
+export default (model) => h('.flex-row.p2', [
+  commonHeader(model),
+  headerSpecific(model)
+]);
 
-function headerSpecific(model) {
+const headerSpecific = (model) => {
   switch (model.page) {
     case 'layoutList': return layoutListHeader(model); break;
     case 'layoutShow': return layoutShowHeader(model); break;
     case 'objectTree': return objectTreeHeader(model); break;
-    default: return defaultHeader(model);
+    default: return null;
   }
 }
 
-function defaultHeader(model) {
-  return h(''); // fill the space
-}
+const commonHeader = (model) => h('.flex-grow', [
+  loginButton(model),
+  ' ',
+  h('button.btn.mh1', {onclick: e => model.toggleSidebar()}, iconMenu()),
+  ' ',
+  h('span.f4.gray', 'Quality Control'),
+  model.loader.active && h('span.f4.mh1.gray', spinner())
+]);
 
-function commonHeader(model) {
-  return h('.flex-grow', [
-    h('button.btn.mh1', {onclick: e => model.toggleSidebar()}, iconMenu()),
-    ' ',
-    h('span.f4.gray', 'Quality Control'),
-    model.loader.active && h('span.f4.mh1.gray', spinner())
-  ]);
-}
+const loginButton = (model) => h('.dropdown', {class: model.accountMenuEnabled ? 'dropdown-open' : ''}, [
+  h('button.btn', {onclick: () => model.toggleAccountMenu()}, iconPerson()),
+  h('.dropdown-menu', [
+    h('p.m3.mv2.text-ellipsis', `Welcome ${model.session.name}`),
+    model.session.personid === 0 // anonymous user has id 0
+     ? h('p.m3.gray-darker', 'You are connected as anonymous, no authentification needed for this application.')
+     : h('a.menu-item', {onclick: () => alert(`Not implemented`)}, 'Logout'),
+  ]),
+]);
