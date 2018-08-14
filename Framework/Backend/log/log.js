c15ab3876213340d121e80b89d574898cf9bfb6b
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2018-08-14 11:53:16 +0200

    Log is a class

--- Framework/Backend/log/log.js
+++ Framework/Backend/log/log.js
@@ -1,87 +1,74 @@
 const fs = require('fs');
 const Winston = require('./winston.js');
 const InfoLoggerSender = require('./InfoLoggerSender.js');
-const InfoLoggerReceiver = require('./InfoLoggerReceiver.js');
 
 let winston = null;
 let infologger = null;
 
-exports.InfoLoggerSender = InfoLoggerSender;
-exports.InfoLoggerReceiver = InfoLoggerReceiver;
+class Log {
 
-exports.configure = function(config) {
-  if (config && config.winston) {
-    winston = new Winston(config.winston);
+  constructor(label = null) {
+    this.label = label;
+    if (!winston) { 
+      winston = new Winston();
+      winston.instance.warn('Created default instance of logger');
+    }
   }
-  if (!infologger && config && config.infologger && fs.existsSync(config.infologger.execPath)) {
-    infologger = new InfoLoggerSender(winston, config.infologger.execPath);
-  }
-};
 
-/**
- *  Create default Winston instance
- */
-function createWinstonInstance() {
-  if (!winston) {
-    winston = new Winston();
-    winston.instance.warn('Created default instance of logger');
+  static configure(config) {
+    if (config && config.winston) {
+      winston = new Winston(config.winston);
+    }
+    if (!infologger && config && config.infologger && fs.existsSync(config.infologger.execPath)) {
+      infologger = new InfoLoggerSender(winston, config.infologger.execPath);
+    }
+  }
+
+  debug(log) {
+    const message = (this.label == null) ? log : {message: log, label: this.label};
+    winston.instance.debug(message);
+
+    if (infologger) {
+      const logObj = {severity: 'D', message: log, rolename: this.label};
+      infologger.send(logObj);
+    }
+  }
+
+  info(log) {
+    const message = (this.label == null) ? log : {message: log, label: this.label};
+    winston.instance.info(message);
+
+    if (infologger) {
+      const logObj = {severity: 'D', message: log, rolename: this.label};
+      infologger.send(logObj);
+    }
+  }
+
+  warn(log) {
+    const message = (this.label == null) ? log : {message: log, label: this.label};
+    winston.instance.warn(message);
+
+    if (infologger) {
+      const logObj = {severity: 'W', message: log, rolename: this.label};
+      infologger.send(logObj);
+    }
+  }
+
+  error(log) {
+    const message = (this.label == null) ? log : {message: log, label: this.label};
+    winston.instance.error(message);
+
+    if (infologger) {
+      const logObj = {severity: 'E', message: log, rolename: this.label};
+      infologger.send(logObj);
+    }
+  }
+
+  static trace(err) {
+    // Print more information for debugging
+    // eslint-disable-next-line
+    console.trace(err);
   }
 }
 
-exports.debug = function(log) {
-  createWinstonInstance();
-  winston.instance.log('debug', log);
-
-  if (infologger) {
-    const logObj = {
-      severity: 'D',
-      message: log
-    };
-    infologger.send(logObj);
-  }
-};
-
-exports.warn = function(log) {
-  createWinstonInstance();
-  winston.instance.warn(log);
-
-  if (infologger) {
-    const logObj = {
-      severity: 'W',
-      message: log
-    };
-    infologger.send(logObj);
-  }
-};
-
-exports.info = function(log) {
-  createWinstonInstance();
-  winston.instance.info(log);
-
-  if (infologger) {
-    const logObj = {
-      severity: 'I',
-      message: log
-    };
-    infologger.send(logObj);
-  }
-};
-
-exports.error = function(log) {
-  createWinstonInstance();
-  winston.instance.error(log);
-
-  if (infologger) {
-    const logObj = {
-      severity: 'E',
-      message: log
-    };
-    infologger.send(logObj);
-  }
-};
-
-exports.trace = function(err) {
-  // Print more information for debugging
-  // eslint-disable-next-line
-  console.trace(err);
-};
+module.exports = Log;
