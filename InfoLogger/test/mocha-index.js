5cc3bd4ece2b96902e904a1ed63a1ae0fed46ab8
George Raduta <george.raduta@cern.ch>
2019-05-06 12:13:46 +0200

    [OGUI-258] Update tests to include underscore

--- InfoLogger/test/mocha-index.js
+++ InfoLogger/test/mocha-index.js
@@ -65,11 +65,11 @@ describe('InfoLogger', function() {
   describe('LogFilter', async () => {
     it('should update URI with new encoded criteria', async () => {
       /* eslint-disable max-len */
-      const decodedParams = '?q={"hostname":{"match":"%alda%qdip01%"},"severity":{"in":"I W E F"},"level":{"max":1}}';
-      const expectedParams = '?q={%22hostname%22:{%22match%22:%22%25alda%25qdip01%25%22},%22severity%22:{%22in%22:%22I%20W%20E%20F%22},%22level%22:{%22max%22:1}}';
+      const decodedParams = '?q={"hostname":{"match":"%ald_qdip01%"},"severity":{"in":"I W E F"},"level":{"max":1}}';
+      const expectedParams = '?q={%22hostname%22:{%22match%22:%22%25ald_qdip01%25%22},%22severity%22:{%22in%22:%22I%20W%20E%20F%22},%22level%22:{%22max%22:1}}';
       /* eslint-enable max-len */
       const searchParams = await page.evaluate(() => {
-        window.model.log.filter.setCriteria('hostname', 'match', '%alda%qdip01%');
+        window.model.log.filter.setCriteria('hostname', 'match', '%ald_qdip01%');
         window.model.updateRouteOnModelChange();
         return window.location.search;
       });
