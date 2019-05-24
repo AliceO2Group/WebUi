a36bfd234ae51a4b93d3e337d87037bc74070506
George Raduta <george.raduta@cern.ch>
2019-05-24 11:34:56 +0200

    [OGUI-299] Clear dependencies

--- QualityControl/config-default.js
+++ QualityControl/config-default.js
@@ -24,6 +24,11 @@ module.exports = {
     port: 8080
   },
 
+  consul: {
+    hostname: 'locahost',
+    port: 8500
+  }
+
   // dbFile: '/var/db/qcg-db.json', // absolute path where to save layouts, default = root of this app
 
   // JWT manages user's session duration
