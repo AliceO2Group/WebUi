de2bba65dea3224b2a0bca897e74d696366989c4
George Raduta <george.raduta@cern.ch>
2019-06-20 16:02:36 +0200

    [OGUI-352] Move shutdown option to all envs page

--- Control/public/common/showTableList.js
+++ Control/public/common/showTableList.js
@@ -5,15 +5,17 @@ import parseObject from './utils.js';
  * Generic table to show list of objects
  * This can be forked to show more specific data (format date, colors, more buttons...)
  * @param {Array.<Object.<string, Any>>} list - things to be shown
- * @param {function(DOMEvent, item)} onclick - (optional) add a button for each line with object as argument
+ * @param {Array.<function(DOMEvent, item)>} actions - (optional) add a button for each line with object as argument
  * @return {vnode} table view
  */
-export default (list, onclick) => h('table.table', [
+export default (list, actions) => h('table.table', [
   h('thead', [
-    h('tr', [
-      list.length > 0 && Object.keys(list[0]).map((columnName) => h('th', columnName)),
-      onclick && h('th', '')
-    ])
+    h('tr',
+      [
+        list.length > 0 && Object.keys(list[0]).map((columnName) => h('th', {style: 'text-align:center'}, columnName)),
+        actions && h('th.text-center', {style: 'text-align:center'}, 'Actions')
+      ]
+    )
   ]),
   h('tbody', list.map((item) => h('tr', [
     Object.keys(item).map(
@@ -25,9 +27,11 @@ export default (list, onclick) => h('table.table', [
             style: 'font-weight: bold;'
           },
           item[columnName]
-
         )
     ),
-    onclick && h('td', h('button.btn', {onclick: (event) => onclick(event, item)}, 'Details'))
+    actions && h('td.btn-group',
+      h('button.btn.btn-primary', {onclick: (event) => actions[0](event, item)}, 'Details'),
+      h('button.btn.btn-danger', {onclick: (event) => actions[1](event, item)}, 'Shut Down'),
+    )
   ]))),
 ]);
