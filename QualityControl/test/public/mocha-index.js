616c343d9c1a83c03bbb5c9ffe8dbc74d8690c2c
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2019-09-17 14:07:47 +0200

    Fix QC test

--- QualityControl/test/public/mocha-index.js
+++ QualityControl/test/public/mocha-index.js
@@ -101,7 +101,7 @@ describe('QCG', function() {
     });
 
     it('should have a table with one row after filtering', async () => {
-      await page.type('header input', 'AliRoot');
+      await page.type('header input', 'PWG-HF');
       const rowsCount = await page.evaluate(() => document.querySelectorAll('section table tbody tr').length);
       assert(rowsCount === 1);
     });
@@ -109,7 +109,7 @@ describe('QCG', function() {
     it('should have a link to show a layout', async () => {
       await page.evaluate(() => document.querySelector('section table tbody tr a').click());
       const location = await page.evaluate(() => window.location);
-      assert.strictEqual(location.search, '?page=layoutShow&layoutId=5aba4a059b755d517e76ea12&layoutName=AliRoot%20dashboard');
+      assert.strictEqual(location.search, '?page=layoutShow&layoutId=5aba4a059b755d517e76ea12&layoutName=PWG-HF');
       // id 5aba4a059b755d517e76ea12 is set in QCModelDemo
     });
   });
