da75e57fe8b2d04009b5f788594527ed4e8d3d2c
George Raduta <george.raduta@cern.ch>
2019-11-26 15:05:54 +0100

    [OGUI-401] Run in headless mode

--- QualityControl/test/public/mocha-index.js
+++ QualityControl/test/public/mocha-index.js
@@ -34,7 +34,7 @@ describe('QCG', function() {
 
     this.ok = true;
     // Start browser to test UI
-    browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], headless: false});
+    browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], headless: true});
     page = await browser.newPage();
 
     // Listen to browser
