d36cf994c9554a047504bb4eed965275c14a8d92
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2020-03-12 20:45:10 +0100

    Use url.resolve instead of path.join

--- Framework/Backend/http/server.js
+++ Framework/Backend/http/server.js
@@ -177,7 +177,9 @@ class HttpServer {
       throw new Error(`static path ${localPath} does not exist`);
     }
     if (uriPath) {
-      this.routerStatics.use(path.join('/', uriPath), express.static(localPath));
+      // Workaround for path.join('/', uriPath) as on Windows as it replaces '/' with '\'
+      uriPath = (uriPath.charAt(0) == '/') ? uriPath : '/' + uriPath;
+      this.routerStatics.use(uriPath, express.static(localPath));
     } else {
       this.routerStatics.use(express.static(localPath));
     }
