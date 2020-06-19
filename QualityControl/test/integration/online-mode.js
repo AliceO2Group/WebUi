97ea3aba00b6a78183528dd9e55d7c1233d308df
George Raduta <george.raduta@cern.ch>
2020-06-19 12:49:18 +0200

    [OGUI-589] Add initial skeleton for QCG IT tests

---
+++ QualityControl/test/integration/online-mode.js
@@ -0,0 +1,43 @@
+/* eslint-disable max-len */
+const assert = require('assert');
+const qcg = require('./qcg-test');
+
+let url;
+let page;
+let objects;
+
+describe('`ONLINE` test-suite', async () => {
+  before(async () => {
+    url = qcg.url;
+    page = qcg.page;
+    objects = qcg.offlineObjects;
+  });
+
+  it('should successfully load objectTree page', async () => {
+    await page.goto(url + '?page=objectTree', {waitUntil: 'networkidle0'});
+    const location = await page.evaluate(() => window.location);
+    await page.waitFor(2000);
+    assert.strictEqual(location.search, '?page=objectTree', 'Could not load page objectTree');
+  });
+
+  it('should successfully receive a list of objects from Consul', async () => {
+
+  });
+  it('should successfully receive certain objects from Consul', async () => {
+
+  });
+  it('should successfully open subtree of object ???????', async () => {
+
+  });
+  it('should successfully open 50% size plot of the object', async () => {
+
+  });
+
+  it('should successfully open subtree of checker ???????', async () => {
+
+  });
+  it('should successfully open 50% size display of checker', async () => {
+
+  });
+});
+
---
+++ QualityControl/test/integration/qcg-test.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-invalid-this */
+/* eslint-disable no-console */
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+const puppeteer = require('puppeteer');
+const assert = require('assert');
+
+let page;
+describe('QCG', function() {
+  let browser;
+  this.timeout(10000);
+  this.slow(1000);
+  let url = 'http://localhost:8082/';
+
+  before(async () => {
+    this.ok = true;
+
+    // Start browser to test UI
+    browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], headless: true});
+    page = await browser.newPage();
+
+    // Listen to browser
+    page.on('error', (pageerror) => {
+      console.error('        ', pageerror);
+      this.ok = false;
+    });
+    page.on('pageerror', (pageerror) => {
+      console.error('        ', pageerror);
+      this.ok = false;
+    });
+    page.on('console', (msg) => {
+      for (let i = 0; i < msg.args().length; ++i) {
+        console.log(`        ${msg.args()[i]}`);
+      }
+    });
+    let testConfig;
+    try {
+      testConfig = require('./test-config');
+    } catch (error) {
+      console.warn('`test-config.js` file could not be found. Will use default values.');
+    }
+    exports.timeout = (testConfig && testConfig.timeout) ? testConfig.timeout : 2000;
+    exports.page = page;
+    exports.url = (testConfig && testConfig.hostname && testConfig.port) ? `http://${testConfig.hostname}:${testConfig.port}/` : url;
+    exports.offlineObjects = (testConfig && testConfig.offlineObjects) ? testConfig.offlineObjects : ['qc/checks/TST/QcCheck', 'qc/DAQ/dataDistribution/payloadSize'];
+  });
+
+  it('should load first page "/"', async () => {
+    // try many times until backend server is ready
+    for (let i = 0; i < 10; i++) {
+      try {
+        await page.goto(url, {waitUntil: 'networkidle0'});
+        break; // connection ok, this test passed
+      } catch (e) {
+        if (e.message.includes('net::ERR_CONNECTION_REFUSED')) {
+          await new Promise((done) => setTimeout(done, 500));
+          continue; // try again
+        }
+        throw e;
+      }
+    }
+  });
+
+  it('should have redirected to default page "/?page=objectTree"', async () => {
+    const location = await page.evaluate(() => window.location);
+    assert.strictEqual(location.search, '?page=objectTree', 'Could not load home page of QCG');
+  });
+
+  require('./offline-mode.js');
+
+  beforeEach(() => {
+    this.ok = true;
+  });
+
+  afterEach(() => {
+    if (!this.ok) {
+      throw new Error('Something went wrong. Please use "systemctl status o2-cog for more information"');
+    }
+  });
+
+  after(async () => {
+    await browser.close();
+  });
+});
