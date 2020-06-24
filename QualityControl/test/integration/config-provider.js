5664a38d70d9be76283d23ac634fdbe79c851202
George Raduta <george.raduta@cern.ch>
2020-06-24 15:29:26 +0200

    [OGUI-589] Improve tests through config file

---
+++ QualityControl/test/integration/config-provider.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+/* eslint-disable max-len */
+
+let testConfig;
+try {
+  testConfig = require('./test-config');
+} catch (error) {
+  console.warn('`test-config.js` file could not be found. Will use default values.');
+}
+
+const timeout = (testConfig && testConfig.timeout) ? testConfig.timeout : 2000;
+const url = (testConfig && testConfig.hostname && testConfig.port) ? `http://${testConfig.hostname}:${testConfig.port}/` : 'http://localhost:8082/';
+const offlineObjects = (testConfig && testConfig.offlineObjects) ? testConfig.offlineObjects : ['qc/checks/TST/QcCheck', 'qc/DAQ/dataDistribution/payloadSize'];
+const onlineObjects = (testConfig && testConfig.onlineObjects) ? testConfig.onlineObjects : ['qc/checks/TST/QcCheck', 'qc/DAQ/dataDistribution/payloadSize'];
+
+module.exports = {timeout, url, offlineObjects, onlineObjects};
--- QualityControl/test/integration/online-mode.js
+++ QualityControl/test/integration/online-mode.js
@@ -1,14 +1,14 @@
 /* eslint-disable max-len */
 const assert = require('assert');
 const qcg = require('./qcg-test');
+const config = require('./config-provider');
 
 let page;
-let objects;
+const objects = config.onlineObjects;
 
 describe('`ONLINE` test-suite', async () => {
-  before(async () => {
+  before(() => {
     page = qcg.page;
-    objects = qcg.offlineObjects;
   });
 
   it('should have a button for ONLINE mode', async () => {
@@ -37,39 +37,37 @@ describe('`ONLINE` test-suite', async () => {
     assert.strictEqual(contains.length, 0, `Could not find following objects from the expected ones: ${contains}`);
   });
 
-  // it('should successfully open subtree of OBJECT plot and select/open plot', async () => {
-  //   const paths = objects[0].split('/');
-  //   paths.shift();
 
-  //   for (let i = 0; i < paths.length; ++i) {
-  //     const [row] = await page.$x(`//tr[td[text()="${paths[i]}"]]`);
-  //     if (row) {
-  //       await row.click();
-  //       await page.waitFor(200);
-  //     } else {
-  //       assert.ok(false, `${paths[i]} could not be found in object tree`);
-  //     }
-  //   }
+  describe('Iterate over given objects and open them', function() {
+    for (let i = 0; i < objects.length; ++i) {
+      it(`should successfully open subtree of OBJECT  ${objects[i]} and select/open plot`, async () => {
+        const path = objects[i].split('/');
+        path.shift();
 
-  //   const panelWidth = await page.evaluate(() => document.querySelector('section > div > div > div:nth-child(2)').style.width);
-  //   assert.strictEqual(panelWidth, '50%', 'Panel containing object plot was not opened successfully');
-  // });
+        await toggleGivenObjectPath(page, path);
 
-  // it('should successfully open subtree of CHECKER plot and select/open plot', async () => {
-  //   const paths = objects[1].split('/');
-  //   paths.shift();
+        const panelWidth = await page.evaluate(() => document.querySelector('section > div > div > div:nth-child(2)').style.width);
+        assert.strictEqual(panelWidth, '50%', `Panel containing object ${objects[i]} plot was not opened successfully`);
 
-  //   for (let i = 0; i < paths.length; ++i) {
-  //     const [row] = await page.$x(`//tr[td[text()="${paths[i]}"]]`);
-  //     if (row) {
-  //       await row.click();
-  //       await page.waitFor(200);
-  //     } else {
-  //       assert.ok(false, `${paths[i]} could not be found in object tree`);
-  //     }
-  //   }
-
-  //   const panelWidth = await page.evaluate(() => document.querySelector('section > div > div > div:nth-child(2)').style.width);
-  //   assert.strictEqual(panelWidth, '50%', 'Panel containing object checker was not opened successfully');
-  // });
+        await toggleGivenObjectPath(page, path.reverse());
+      });
+    }
+  });
 });
+
+/**
+ * Method to open/close the tree for a given path
+ * @param {Page} page
+ * @param {Array<String>} path
+ */
+async function toggleGivenObjectPath(page, path) {
+  for (let i = 0; i < path.length; ++i) {
+    const [row] = await page.$x(`//tr[td[text()="${path[i]}"]]`);
+    if (row) {
+      await row.click();
+      await page.waitFor(200);
+    } else {
+      assert.ok(false, `${path[i]} could not be found in object tree`);
+    }
+  }
+}
