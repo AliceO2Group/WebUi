22b7216280b240c40567aa4922186b5061a01dde
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2020-07-15 14:33:20 +0200

    Working JIRA service

--- Framework/Backend/services/jira.js
+++ Framework/Backend/services/jira.js
@@ -11,53 +11,41 @@
  * or submit itself to any jurisdiction.
 */
 
-const http = require('http');
-const querystring = require('querystring');
+const https = require('https');
 
 /**
  * Handles creating JIRA issues
  */
 class Jira {
-
+  /**
+   * @param {object} config - JIRA configuration including URL, service account and project ID
+   */
   constructor(config) {
-    this.hostname = 'alice.its.cern.ch'
-    this.path = '/jira/rest/api/2/issue'
-    this.auth = 'user:pass'
+    this.url = config.url;
+    this.serviceUser = config.serviceUser;
+    this.servicePass = config.servicePass;
+    this.projectId = config.projectId;
+    this.issueTypes = {
+      bug: 1
+    };
   }
 
-  async createIssue() {
-    const postData = querystring.stringify({
-    "fields": {
-      "issuetype": {
-        "id": "1"
-      },
-      "project": {
-        "id": "11400"
-      },
-      "description": "Order entry fails when selecting supplier.",
-        "reporter": {
-        "name": "awegrzyn"
-      },
-      "assignee": {
-        "name": "awegrzyn"
-      }
-    }
-    });
-
+  /**
+   * Handles HTTP req/rep in order to create JIRA issue
+   * @param {object} postData - issue details
+   */
+  async createIssue(postData) {
     const requestOptions = {
-      hostname: this.hostname,
-      port: 80,
-      path: this.path,
       method: 'POST',
-      auth: this.auth,
+      auth: this.serviceUser + ':' + this.servicePass,
       headers: {
         'Content-Type': 'application/json',
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Length': Buffer.byteLength(postData)
       }
     };
     return new Promise((resolve, reject) => {
-      const requestHandler = (response) => {
+      const requestHandler = (response) => { // eslint-disable-line require-jsdoc
         if (response.statusCode < 200 || response.statusCode > 299) {
           reject(new Error('Non-2xx status code: ' + response.statusCode));
           return;
@@ -74,14 +62,42 @@ class Jira {
         });
       };
 
-      const request = http.request(requestOptions, requestHandler);
+      const request = https.request(this.url, requestOptions, requestHandler);
       request.on('error', (err) => reject(err));
       request.write(postData);
       request.end();
     });
   }
+
+  /**
+   * Creates bug issue
+   * @param {string} reporter - reporter of issue
+   * @param {string} assignee - asignee of issue
+   * @param {string} summary - title of issue
+   * @param {string} description - issue description (optional)
+   * @return {Promise} - resolve object contains issue ID (id) , key (key) and URL (self)
+   */
+  async createBugIssue(reporter, assignee, summary, description = '') {
+    const issue = JSON.stringify(
+      {
+        fields: {
+          issuetype: {
+            id: this.issueTypes.bug
+          },
+          project: {
+            id: this.projectId
+          },
+          summary: summary,
+          description: description,
+          reporter: {
+            name: reporter
+          },
+          assignee: {
+            name: reporter
+          }
+        }
+      });
+    return this.createIssue(issue);
+  }
 }
 module.exports = Jira;
-
-const jira = new Jira();
-jira.createIssue();
