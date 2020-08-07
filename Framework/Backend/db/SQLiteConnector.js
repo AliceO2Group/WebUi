e699ee5580731fe1dcbf0ee76352d4f11a1753d4
michaelabrezinova <brezina.michaela@gmail.com>
2020-08-07 13:22:56 +0200

    add sqlite connector

---
+++ Framework/Backend/db/SQLiteConnector.js
@@ -0,0 +1,122 @@
+const log = new (require('@aliceo2/web-ui').Log)('SQLiteConnector');
+const sqlite3 = require('sqlite3').verbose();
+
+/**
+ * SQLite database wrapper
+ */
+class SQLiteConnector {
+  /**
+     * Initialize connector and test the connection
+     * @param {string} pathname - path to SQLite DB file
+     */
+  constructor(pathname) {
+    if (!pathname) {
+      throw new Error('No pathname provided');
+    }
+    this.pathname = pathname;
+    this.db;
+
+    this.connectionQuery = 'SELECT * FROM profiles LIMIT 1';
+  }
+
+  /**
+     * Initialize the database connection
+     * @return {Promise}
+     */
+  async init() {
+    this.db = new sqlite3.Database(this.pathname, sqlite3.OPEN_READWRITE, (err) => {
+      if (err) {
+        log.error(err.message);
+        throw new Error('Cannot initiate the database');
+      }
+      log.info('Successfully connected to the database.');
+    });
+  }
+
+  /**
+     * Method to test connection of sqlite connector once initialized
+     * @return {Promise}
+     */
+  async testConnection() {
+    return new Promise((resolve, reject) => {
+      this.db.get(this.connectionQuery, [], function(err, row) {
+        if (err) {
+          log.error(err.message);
+          reject(err);
+        }
+        log.info(`Connection test was successful`); 
+        resolve();
+      });
+    });
+  }
+
+  /**
+     * Runs the passed query on the database,
+     * @param {string} query - SQLite query
+     * @param {array} values - values to be bound to the query
+     * @param {boolean} read - if true use #get otherwise #run
+     */
+  async query(query, values, read=true) {
+    return new Promise((resolve, reject) => {
+      if (read) {
+        this.readQuery(query, values).then((profile) => resolve(profile))
+          .catch((err) => reject(err));
+      } else {
+        this.generalQuery(query, values).then(() => resolve())
+          .catch((err) => reject(err));
+      }
+    });
+  }
+
+  /**
+     * Query that reads data from the database
+     * @param {string} query - SQLite query
+     * @param {array} values - values to be bound to the query
+     */
+  async readQuery(query, values) {
+    return new Promise((resolve, reject) => {
+      this.db.get(query, values, (err, row) => {
+        if (err) {
+          log.error(err.message);
+          reject(err);
+        }
+        if (row) {
+          log.info('Successfully fetched the required data');
+        } else {
+          log.warn('No result for this query');
+        }
+        resolve(row);
+      });
+    });
+  }
+
+  /**
+     * Query that writes and deletes data from the database
+     * @param {string} query - SQLite query
+     * @param {array} values - values to be bound to the query
+     */
+  async generalQuery(query, values) {
+    return new Promise((resolve, reject) => {
+      this.db.run(query, values, function(err) {
+        if (err) {
+          log.error(err.message);
+          reject(err);
+        }
+        log.info(`Query ${query} was successful`);
+        resolve();
+      });
+    });
+  }
+
+  /**
+     * Close database
+     */
+  async closeDatabase() {
+    this.db.close((err) => {
+      if (err) {
+        return log.error(err.message);
+      }
+    });
+  }
+}
+module.exports = SQLiteConnector;
---
+++ Framework/Backend/test/mocha-sqliteconnector.js
@@ -0,0 +1,64 @@
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const SQLiteConnector = require('../db/SQLiteConnector.js');
+const log = new (require('@aliceo2/web-ui').Log)('SQLite Test');
+
+
+describe('SQLite database', () => {
+  before(() => {
+    sqliteConnector = new SQLiteConnector(path.join(__dirname, '/../../INFOLOGGER;'));
+    sqliteConnector.init();
+  });
+  
+  describe('Creating a new SQLite instance', () => {
+    it('should throw an error if the pathname is not provided', () => {
+      assert.throws(() => new SQLiteConnector(), new Error('No pathname provided'));
+      assert.throws(() => new SQLiteConnector(null), new Error('No pathname provided'));
+      assert.throws(() => new SQLiteConnector(undefined), new Error('No pathname provided'));
+    });
+
+    it('should successfully initialize SQLiteConnector', () => {
+      assert.doesNotThrow(() => new SQLiteConnector(path.join(__dirname, '/../../INFOLOGGER;')));
+    });
+  });
+
+  describe('Test Connection', () => {
+    it('should successfully return a response', () => {
+        return assert.doesNotReject(async () => {
+            await sqliteConnector.testConnection('SELECT * FROM PROFILES LIMIT 1');
+        });
+    });
+  });
+
+  describe('Make Query', () => {
+    it('should successfully get profile by username', () => {
+        return assert.doesNotReject(async () => {
+            sqliteConnector.query('SELECT * FROM profiles WHERE profile_name=?', ['users'], true).then((profile) => {
+                assert.strictEqual(profile.profile_id, 1);
+            })
+        });
+    });
+    it('should successfully add a new profile', () => {
+        return assert.doesNotReject(async () => {
+            await sqliteConnector.query('INSERT INTO profiles(profile_name) VALUES(?)',['anonymous'],false);
+            const newProfile = await sqliteConnector.query('SELECT * FROM profiles WHERE profile_name=?', ['anonymous'], true);
+    
+            assert.ok(newProfile.profile_id);
+            assert.ok(newProfile.profile_name);
+            assert.strictEqual(newProfile.profile_name, 'anonymous');
+        });
+    });
+    it('should successfully delete a profile', () => {
+        return assert.doesNotReject(async () => {
+            await sqliteConnector.query('DELETE FROM profiles WHERE profile_name=?',['anonymous'],false);
+            const newProfile = await sqliteConnector.query('SELECT * FROM profiles WHERE profile_name=?', ['anonymous'], false);
+    
+            assert.equal(newProfile, null);
+        });
+    });
+  });
+    after(() => {
+        sqliteConnector.closeDatabase();
+    });
+});
