0ad368c7da05a32bfa0b546112c04be913e0c073
George Raduta <george.raduta@cern.ch>
2020-08-10 17:58:26 +0200

    [OGUI-628] Remove deprecated quality property

--- QualityControl/public/object/ObjectTree.class.js
+++ QualityControl/public/object/ObjectTree.class.js
@@ -29,7 +29,6 @@ export default class ObjectTree extends Observable {
     this.parent = parent || null; // <ObjectTree>
     this.path = []; // like ['A', 'B'] for node at path 'A/B' called 'B'
     this.pathString = ''; // 'A/B'
-    this.quality = null; // most negative quality from this subtree
   }
   /**
    * Toggle this node (open/close)
@@ -86,12 +85,6 @@ export default class ObjectTree extends Observable {
       this.addChild(object, path, []);
       this.notify();
       return;
-    }
-
-    // Keep quality of wrost quality of all leaf
-    // so the root has the wrost quality, easy to monitor
-    if (!this.quality || object.quality === 'bad') {
-      this.quality = object.quality;
     }
 
     // Case end of path, associate the object to 'this' node
