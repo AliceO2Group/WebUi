74a2df34f77bff5c41b62c64a9f5a5e6138e1341
George Raduta <george.raduta@cern.ch>
2020-08-17 16:03:59 +0200

    [OGUI-631] Use new converter in api

--- QualityControl/lib/QCModel.js
+++ QualityControl/lib/QCModel.js
@@ -5,6 +5,7 @@ const CCDBConnector = require('./CCDBConnector.js');
 const MySQLConnector = require('./MySQLConnector.js');
 const AMOREConnector = require('./AMOREConnector.js');
 const JsonFileConnector = require('./JsonFileConnector.js');
+const LayoutConnector = require('./connector/LayoutConnector.js');
 
 const log = new (require('@aliceo2/web-ui').Log)('QualityControlModel');
 
@@ -12,6 +13,8 @@ const log = new (require('@aliceo2/web-ui').Log)('QualityControlModel');
 // Initialization of model according to config file
 
 const jsonDb = new JsonFileConnector(config.dbFile || __dirname + '/../db.json');
+const layoutConnector = new LayoutConnector(jsonDb);
+module.exports.layoutConnector = layoutConnector;
 
 if (config.consul) {
   const consulService = new ConsulService(config.consul);
@@ -55,9 +58,3 @@ if (config.listingConnector === 'ccdb') {
 }
 
 // --------------------------------------------------------
-
-module.exports.readLayout = jsonDb.readLayout.bind(jsonDb);
-module.exports.updateLayout = jsonDb.updateLayout.bind(jsonDb);
-module.exports.listLayouts = jsonDb.listLayouts.bind(jsonDb);
-module.exports.createLayout = jsonDb.createLayout.bind(jsonDb);
-module.exports.deleteLayout = jsonDb.deleteLayout.bind(jsonDb);
