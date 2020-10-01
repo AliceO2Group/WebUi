1bef78d685463855a31f880a09d80d709e8d3dd3
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2020-10-01 16:06:05 +0200

    fix2

--- Control/public/common/appHeader.js
+++ Control/public/common/appHeader.js
@@ -47,6 +47,6 @@ const loginButton = (model) => h('.dropdown', {class: model.accountMenuEnabled ?
     model.session.personid === 0 // anonymous user has id 0
       ? h('p.m3.gray-darker', 'You are connected as anonymous, no authentification needed for this application.')
       : model.session.access === 2 ? h('a.menu-item', {onclick: () => model.lock.forceUnlock()}, 'Force lock') : '',
-      h('a.menu-item', {onclick: () => alert(`Not implemented`)}, 'Logout'),
+    h('a.menu-item', {onclick: () => alert(`Not implemented`)}, 'Logout'),
   ]),
 ]);
