bcf40965bb8a1f96b84f9b23afafb4ec2214d7e0
George R <georgeraduta25@yahoo.com>
2020-10-01 11:45:22 +0000

    [OGUI-637] sqlite connector in framework (#616)
    * add sqlite connector
    
    * [OGUI-637] Staging
    
    * [OGUI-637] Add documentation for SQLiteConnector
    
    * Bump version
    
    * [OGUI-637] Update naming
    
    * [OGUI-637] Add tests
    
    * [OGUI-637] Remove LGTM alert
    
    * [OGUI-637]  Export Connector
    
    * [OGUI-637] Update title of doc file
    
    Co-authored-by: michaelabrezinova <brezina.michaela@gmail.com>

---
+++ Framework/Backend/db/sqlite.connector.js
@@ -0,0 +1,125 @@
+const log = new (require('../log/Log.js'))('SQLiteConnector');
+const sqlite3 = require('sqlite3').verbose();
+
+/**
+ * SQLite database connector
+ * @example
+ * const sqlite = new SQLiteConnector(<path>);
+ * sqlite.init();
+ * sqlite.query('SELECT * FROM mytable, []);
+ * sqlite.query('INSERT INTO mytable (name) VALUES (?), ['user'], false);
+ */
+class SQLiteConnector {
+  /**
+   * Initialize connector
+   * @param {string} pathname - path to SQLite DB file
+   */
+  constructor(pathname) {
+    if (!pathname) {
+      throw new Error('No pathname provided');
+    }
+    this.pathname = pathname;
+  }
+
+  /**
+   * Initialize the database connection
+   * @return {Promise}
+   */
+  async init() {
+    return new Promise((resolve, reject) => {
+      this.db = new sqlite3.Database(this.pathname, sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+          log.error(err.message);
+          reject(new Error(`Cannot initiate the database with provided path: ${this.pathname}`));
+        } else {
+          log.info(`Successfully connected to the database on path: ${this.pathname}`);
+          resolve();
+        }
+      });
+    });
+  }
+
+  /**
+   * Runs the passed query on the database,
+   * @param {string} query - SQLite query
+   * @param {array} values - values to be bound to the query
+   * @param {boolean} read - if true use #all otherwise #run
+   * @return {Promise.<Array<JSON>, Error>}
+   */
+  async query(query, values = [], read = true) {
+    return new Promise((resolve, reject) => {
+      if (read) {
+        this.readQuery(query, values)
+          .then((profile) => resolve(profile))
+          .catch((err) => reject(err));
+      } else {
+        this.generalQuery(query, values)
+          .then(() => resolve([]))
+          .catch((err) => reject(err));
+      }
+    });
+  }
+
+  /**
+   * Query that reads data from the database
+   * It will return all rows matching the query
+   * @param {string} query - SQLite query
+   * @param {array} values - values to be bound to the query
+   * @return {Promise.<Array<JSON>, Error>}
+   */
+  async readQuery(query, values) {
+    return new Promise((resolve, reject) => {
+      this.db.all(query, values, (err, row) => {
+        if (err) {
+          log.error(err.message);
+          reject(err);
+        }
+        if (row) {
+          log.info(`Query ${query} was successful`);
+        } else {
+          log.warn('No result for this query');
+        }
+        resolve(row);
+      });
+    });
+  }
+
+  /**
+   * Query that writes and deletes data from the database;
+   * It does not return any data from the query
+   * @param {string} query - SQLite query
+   * @param {array} values - values to be bound to the query
+   * @return {Promise.<_, Error>}
+   */
+  async generalQuery(query, values) {
+    return new Promise((resolve, reject) => {
+      this.db.run(query, values, (err) => {
+        if (err) {
+          log.error(err.message);
+          reject(err);
+        }
+        log.info(`Query ${query} was successful`);
+        resolve();
+      });
+    });
+  }
+
+  /**
+   * Close database connection
+   * @return {Promise.<_, Error>}
+   */
+  async close() {
+    return new Promise((resolve, reject) => {
+      this.db.close((err) => {
+        if (err) {
+          log.error(err.message);
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+}
+
+module.exports = SQLiteConnector;
---
+++ Framework/docs/guide/sqlite.md
@@ -0,0 +1,83 @@
+
+
+# SQLite client
+Manages SQLite standard operations
+
+- [MySQL client](#mysql-client)
+  - [How to use](#how-to-use)
+  - [Public methods](#public-methods)
+    - [Init](#init)
+    - [Query](#query)
+    - [Close](#close)
+  - [Example](#example)
+
+
+## How to use
+Import the module
+```js
+require('@aliceo2/web-ui').SQLiteConnector
+```
+
+Create an instance
+```js
+new SQLiteConnector(pathname);
+```
+Where:
+ - `pathname` - path to where the SQLite file is stored
+
+
+## Public methods
+
+### Init
+```js
+init()
+```
+Initialize connection to specified location and returns a promise with the result of the action
+
+### Query
+```js
+query (<query_string>, [values], [read])
+```
+
+Where:
+- `query_string` -  It is a mandatory string containing the SQL query. Use `?` characters as placeholders for values you would like to have escaped
+- `values` - Optional Array containing values that are escaped from the query.
+- `read` - Optional Boolean specifying if the operation is read and should return values. Default `true`. For non-read operations, query will not return updated rows
+```js
+e.g
+
+sqlConnector.query('SELECT * FROM Layouts WHERE id = ?', [1])
+```
+
+### Close
+```js
+close()
+```
+Close connection to the SQLite database and returns a promise with the result of the action
+
+
+## Example
+
+```js
+const {SQLiteConnector} = require('@aliceo2/web-ui');
+const sqliteConnector = new SQLiteConnector('/usr/locatino/of/db');
+
+// Initialize connection
+await sqliteConnector.init();
+
+// Insert data
+sqliteConnector.query('INSERT INTO layout (name, owner_id, owner_name, tabs) value (?,?,?,?)', [1, 2, 3, 4], true)
+  .then(result => console.log(result))
+  .catch(err => console.error(err));
+
+// Select Data with await
+try {
+  const rows = await sqliteConnector.query('SELECT * FROM layout');
+  console.log(result);
+} catch(err) {
+  console.error(err);
+}
+
+// Close Connection
+sqliteConnector.close();
+```
