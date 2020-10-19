13c00d03b52f0bfcfdb24dcc42cc84ac401e8530
George Raduta <george.raduta@cern.ch>
2020-10-19 21:27:46 +0200

    [OGUI-681] Add status information to the user

--- QualityControl/public/frameworkInfo/frameworkInfoPage.js
+++ QualityControl/public/frameworkInfo/frameworkInfoPage.js
@@ -29,25 +29,55 @@ export default (model) => h('.p2.absolute-fill.text-center',
 );
 
 /**
-* Display a table with cog and its dependencies information
+* Display a table with QC GUI and its dependencies status
 * @param {Object} item
 * @return {vnode}
 */
-const showContent = (item) =>
-  Object.keys(item).map((columnName) => [
+const showContent = (componentList) =>
+  Object.keys(componentList).map((component) => [
     h('.shadow-level1', [
       h('table.table', {
         style: 'white-space: pre-wrap;'
       }, [
         h('tbody', [
           h('tr',
-            h('th.w-25', {style: 'text-decoration: underline'}, columnName.toUpperCase())),
-          Object.keys(item[columnName]).map((name) =>
-            h('tr', [
-              h('th.w-25', name),
-              h('td', JSON.stringify(item[columnName][name])),
-            ])
-          )])
+            h('th.flex-row', componentHeader(componentList[component].status, component))
+          ),
+          Object.keys(componentList[component]).map((name) => componentInfoRow(name, componentList[component]))])
       ])
     ])
   ]);
+
+/**
+ * Display the header of the component with a green check or
+ * red x depending on the status
+ * @param {JSON} status { <ok>: boolean, [message]: string}
+ * @param {string} component
+ * @return {vnode}
+ */
+const componentHeader = (status, component) => [
+  status && status.ok && h('.badge.bg-success.white.f6', '✓'),
+  status && !status.ok && h('.badge.bg-danger.white.f6', '✕'),
+  h('.mh2', {style: 'text-decoration: underline'}, component.toUpperCase()),
+];
+
+
+/**
+ * Create a row with 2 columns: name and value
+ * containing information about a sub-property of the component
+ * @param {string} name - sub-property of component
+ * @param {string} componentProps
+ * @return {vnode}
+ */
+const componentInfoRow = (name, componentProps) =>
+  name === 'status' ?
+    !componentProps.status.ok &&
+    h('tr.danger', [
+      h('th.w-25', 'error'),
+      h('td', componentProps.status.message),
+    ])
+    :
+    h('tr', [
+      h('th.w-25', name),
+      h('td', JSON.stringify(componentProps[name])),
+    ]);
