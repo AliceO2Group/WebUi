dc43025080c38e9a57bd1f476330f78bd6166819
George Raduta <george.raduta@cern.ch>
2020-11-13 12:35:05 +0100

    [OGUI-674] Remove empty line

--- QualityControl/public/object/QCObject.js
+++ QualityControl/public/object/QCObject.js
@@ -207,7 +207,6 @@ export default class QCObject extends Observable {
         const failureMessage = `Failed to retrieve list of objects due to ${errorMessage}`;
         this.model.notification.show(failureMessage, 'danger', Infinity);
       }
-
       this.sortListByField(offlineObjects, this.sortBy.field, this.sortBy.order);
       this.list = offlineObjects;
 
