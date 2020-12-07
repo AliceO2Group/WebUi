3f1d8bc96d8b5c0a495997b950d4fb17e89ea76f
George Raduta <george.raduta@cern.ch>
2020-12-07 18:40:25 +0100

    [OGUI-733] Log all http errors on ControlGUI

--- Control/lib/utils.js
+++ Control/lib/utils.js
@@ -20,12 +20,10 @@ const log = new (require('@aliceo2/web-ui').Log)('Control');
  * @param {number} status - status code 4xx 5xx, 500 will print to debug
  */
 function errorHandler(err, res, status = 500) {
-  if (status > 500) {
-    if (err.stack) {
-      log.trace(err);
-    }
-    log.error(err.message || err);
+  if (err.stack) {
+    log.trace(err);
   }
+  log.error(err.message || err);
   res.status(status);
   res.send({message: err.message || err});
 }
