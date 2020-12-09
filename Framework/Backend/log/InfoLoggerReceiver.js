58cd5433708f36e97f904d57fdc53c5a897eba0c
George Raduta <george.raduta@cern.ch>
2020-12-09 09:29:04 +0100

    [OGUI-710] Add connection status

--- Framework/Backend/log/InfoLoggerReceiver.js
+++ Framework/Backend/log/InfoLoggerReceiver.js
@@ -35,6 +35,8 @@ module.exports = class InfoLoggerReceiver extends EventEmitter {
     // Declare properties
     this.client = null;
     this.buffer = '';
+
+    this.isConnected = false;
   }
 
   /**
@@ -54,11 +56,13 @@ module.exports = class InfoLoggerReceiver extends EventEmitter {
     this.client.on('data', (messages) => this.onData(messages));
 
     this.client.on('connect', () => {
+      this.isConnected = true;
       log.info(`Connected to infoLoggerServer ${options.host}:${options.port}`);
     });
 
     this.client.on('end', () => {
       log.error('Connection to infoLoggerServer ended (FIN)');
+      this.isConnected = false;
       this.emit('close');
       this.client.setTimeout(1000, () => {
         log.info('Quickly reconnecting infoLoggerServer socket...');
@@ -67,6 +71,7 @@ module.exports = class InfoLoggerReceiver extends EventEmitter {
     });
 
     this.client.on('error', (error) => {
+      this.isConnected = false;
       log.error(`Failed to connect to infoLoggerServer ${this.host}:${this.port} - ${error.code}`);
       this.client.setTimeout(5000, () => {
         this.emit('connection-issue', 'Unable to connect to InfoLogger Server. Retrying in 5 seconds...');
@@ -83,7 +88,7 @@ module.exports = class InfoLoggerReceiver extends EventEmitter {
     if (!this.client) {
       return;
     }
-
+    this.isConnected = false;
     this.client.end();
     this.client = null; // gc
   }
