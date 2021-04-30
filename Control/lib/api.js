bcdba242c11683672ae7614ea6f6b58f5a358779
George Raduta <george.raduta@cern.ch>
2021-04-30 12:23:41 +0300

    [OGUI-848] Update routes

--- Control/lib/api.js
+++ Control/lib/api.js
@@ -13,21 +13,20 @@
 */
 
 const {WebSocketMessage, ConsulService} = require('@aliceo2/web-ui');
-const http = require('http');
-
 const log = new (require('@aliceo2/web-ui').Log)('Control');
-const errorHandler = require('./utils.js').errorHandler;
+const config = require('./config/configProvider.js');
 
-const Padlock = require('./Padlock.js');
-const KafkaConnector = require('./KafkaConnector.js');
+// services
+const Padlock = require('./services/Padlock.js');
+const StatusService = require('./services/StatusService.js');
 
+// connectors
+const KafkaConnector = require('./connectors/KafkaConnector.js');
+const ConsulConnector = require('./connectors/ConsulConnector.js');
+
+// AliECS Core
 const ControlProxy = require('./control-core/ControlProxy.js');
 const ControlService = require('./control-core/ControlService.js');
-
-const ConsulConnector = require('./ConsulConnector.js');
-
-const config = require('./config/configProvider.js');
-const projPackage = require('./../package.json');
 
 if (!config.grpc) {
   throw new Error('[GRPC] Configuration is missing');
@@ -46,20 +45,31 @@ consulConnector.testConsulStatus();
 const padLock = new Padlock();
 const ctrlProxy = new ControlProxy(config.grpc);
 const ctrlService = new ControlService(padLock, ctrlProxy, consulConnector, config.grpc);
+const statusService = new StatusService(config, ctrlService, consulService);
 
-module.exports.setup = (http, ws) => { 
+module.exports.setup = (http, ws) => {
   ctrlService.setWS(ws);
-  
+
   ctrlProxy.methods.forEach((method) =>
     http.post(`/${method}`, (req, res) => ctrlService.executeCommand(req, res))
   );
   http.post('/clean/resources', (req, res) => ctrlService.cleanResources(req, res));
   http.post('/executeRocCommand', (req, res) => ctrlService.executeRocCommand(req, res));
+
+  // Lock Service
   http.post('/lockState', (req, res) => res.json(padLock));
   http.post('/lock', lock);
   http.post('/unlock', unlock);
   http.post('/forceUnlock', forceUnlock);
-  http.get('/getFrameworkInfo', getFrameworkInfo);
+
+  // Status Service
+  http.get('/status/consul', (_, res) => statusService.getConsulStatus().then((data) => res.status(200).json(data)));
+  http.get('/status/core', (_, res) => statusService.getAliEcsCoreStatus().then((data) => res.status(200).json(data)));
+  http.get('/status/grafana', (_, res) => statusService.getGrafanaStatus().then((data) => res.status(200).json(data)));
+  http.get('/status/kafka', (_, res) => statusService.getKafkaStatus().then((data) => res.status(200).json(data)));
+  http.get('/status/gui', (req, res) => res.status(200).json(statusService.getGuiStatus()));
+
+  // Consul
   http.get('/getCRUs', (req, res) => consulConnector.getCRUs(req, res));
   http.get('/getFLPs', (req, res) => consulConnector.getFLPs(req, res));
   http.get('/getCRUsConfig', (req, res) => consulConnector.getCRUsWithConfiguration(req, res));
@@ -130,113 +140,3 @@ module.exports.setup = (http, ws) => {
     broadcastPadState();
   }
 };
-
-/**
- * Send back info about the framework
- * @param {Request} req
- * @param {Response} res
- */
-async function getFrameworkInfo(req, res) {
-  if (!config) {
-    errorHandler('[API] Unable to retrieve configuration of the framework', res, 502);
-  } else {
-    const result = {};
-    result['AliECS GUI'] = {};
-    if (projPackage && projPackage.version) {
-      result['AliECS GUI'].version = projPackage.version;
-    }
-    if (config.http) {
-      const con = {hostname: config.http.hostname, port: config.http.port};
-      result['AliECS GUI'] = Object.assign(result['AliECS GUI'], con);
-      result['AliECS GUI'].status = {ok: true};
-    }
-    if (config.grpc) {
-      result['AliECS Core'] = config.grpc;
-      try {
-        const coreInfo = await ctrlService.getAliECSInfo();
-        result['AliECS Core'] = Object.assign({}, result['AliECS Core'], coreInfo);
-        result['AliECS Core'].status = {ok: true};
-      } catch (err) {
-        log.error(`[Core] ${err}`);
-        result['AliECS Core'].status = {ok: false};
-        result['AliECS Core'].status.message = err.toString();
-      }
-    }
-    if (config.grafana) {
-      result.grafana = config.grafana;
-      result.grafana.status = {};
-      await httpGetJson(config.http.hostname, config.grafana.port, '/api/health')
-        .then((_result) => result.grafana.status.ok = true)
-        .catch((error) => {
-          result.grafana.status.ok = false; result.grafana.status.message = error.toString();
-        });
-    }
-    if (config.kafka) {
-      result.kafka = config.kafka;
-      result.kafka.status = {};
-      await httpGetJson(config.kafka.hostname, config.kafka.port, '/api/health')
-        .then((_result) => result.kafka.status.ok = true)
-        .catch((error) => {
-          result.kafka.status.ok = false; result.kafka.status.message = error.toString();
-        });
-    }
-    if (config.consul) {
-      result.consul = config.consul;
-      result.consul.status = {};
-      await consulService.getConsulLeaderStatus()
-        .then((_data) => result.consul.status.ok = true)
-        .catch((error) => {
-          result.consul.status.ok = false; result.consul.status.message = error.toString();
-        });
-    }
-
-    res.status(200).json(result);
-  }
-}
-
-/**
-  * Util to get JSON data (parsed) from server
-  * @param {string} host - hostname of the server
-  * @param {number} port - port of the server
-  * @param {string} path - path of the server request
-  * @return {Promise.<Object, Error>} JSON response
-  */
-function httpGetJson(host, port, path) {
-  return new Promise((resolve, reject) => {
-    const requestOptions = {
-      hostname: host,
-      port: port,
-      path: path,
-      method: 'GET',
-      headers: {
-        Accept: 'application/json'
-      }
-    };
-    /**
-     * Generic handler for client http requests,
-     * buffers response, checks status code and parses JSON
-     * @param {Response} response
-     */
-    const requestHandler = (response) => {
-      if (response.statusCode < 200 || response.statusCode > 299) {
-        reject(new Error('Non-2xx status code: ' + response.statusCode));
-        return;
-      }
-
-      const bodyChunks = [];
-      response.on('data', (chunk) => bodyChunks.push(chunk));
-      response.on('end', () => {
-        try {
-          const body = JSON.parse(bodyChunks.join(''));
-          resolve(body);
-        } catch (e) {
-          reject(new Error('Unable to parse JSON'));
-        }
-      });
-    };
-
-    const request = http.request(requestOptions, requestHandler);
-    request.on('error', (err) => reject(err));
-    request.end();
-  });
-}
