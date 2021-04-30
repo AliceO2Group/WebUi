88c957087a10e02555dca2554d088acd65987345
George Raduta <george.raduta@cern.ch>
2021-04-30 12:23:31 +0300

    [OGUI-848] Make use of new status service on client side

--- Control/public/common/pageLoading.js
+++ Control/public/common/pageLoading.js
@@ -19,19 +19,21 @@ import {h} from '/js/src/index.js';
  * @param {number} size
  * @return {vnode}
  */
-export default (size) => h('span.pageLoading',
-  {
-    style: size ? `font-size: ${size}em` : 'font-size: 10em'
-  },
-  h('.atom-spinner',
-    h('.spinner-inner',
-      [
-        h('.spinner-line'),
-        h('.spinner-line'),
-        h('.spinner-line'),
-        h('.spinner-circle',
-          h('div', '●')
-        )
-      ]
+export default (size = 10, margin = '2em') =>
+  h('span.pageLoading',
+    {
+      style: `font-size: ${size}em;margin:${margin}`
+    },
+    h('.atom-spinner',
+      h('.spinner-inner',
+        [
+          h('.spinner-line'),
+          h('.spinner-line'),
+          h('.spinner-line'),
+          h('.spinner-circle',
+            h('div', '●')
+          )
+        ]
+      )
     )
-  ));
+  );
