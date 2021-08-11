7371ced3b852576fab40a09f2c77b799c48d8d4f
George Raduta <george.raduta@cern.ch>
2021-08-11 11:34:20 +0200

    [OGUI-930] Remove mocked data

--- Control/public/environment/Environment.js
+++ Control/public/environment/Environment.js
@@ -144,9 +144,6 @@ export default class Environment extends Observable {
     if (COG.DETECTORS) {
       try {
         const hosts = JSON.parse(environment.userVars.hosts);
-        hosts.push('alio2-cr1-flp001')
-        hosts.push('alio2-cr1-flp003')
-        hosts.push('alio2-cr1-flp002')
         Object.keys(COG.DETECTORS)
           .forEach((detectorKey) => {
             const detector = COG.DETECTORS[detectorKey];
