ff4507d6cd997dfd710e47baadf29e975bcdede7
George Raduta <george.raduta@cern.ch>
2022-02-11 12:16:57 +0200

    Update FLP ITs

--- Control/test/integration/create-new-environment.js
+++ Control/test/integration/create-new-environment.js
@@ -61,16 +61,16 @@ describe('`pageNewEnvironment` test-suite', async () => {
   it('should successfully add provided K:V pairs', async () => {
     for (const key in confVariables) {
       if (key && confVariables[key] !== undefined) {
-        await page.focus('body > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(2) > div > div:nth-child(2) > div > div > div:nth-child(2) > div > div:nth-child(3) > div:nth-child(2) > div > input');
+        await page.focus('#keyInputField');
         page.keyboard.type(key);
         await page.waitForTimeout(500);
 
-        await page.focus('body > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(2) > div > div:nth-child(2) > div > div > div:nth-child(2) > div > div:nth-child(3) > div:nth-child(2) > div:nth-child(2) > textarea');
+        await page.focus('#valueTextAreaField');
         page.keyboard.type(confVariables[key]);
         await page.waitForTimeout(500);
 
         await page.evaluate(() => {
-          document.querySelector('body > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(2) > div > div:nth-child(2) > div > div > div:nth-child(2) > div > div:nth-child(3) > div:nth-child(2) > div:nth-child(3)').click();
+          document.querySelector('#addKVPairButton').click();
         });
         await page.waitForTimeout(500);
       }
@@ -98,7 +98,7 @@ describe('`pageNewEnvironment` test-suite', async () => {
 
   it(`should successfully create a new environment based on workflow '${workflowToTest}'`, async () => {
     await page.evaluate(() => document.querySelector(
-      'body > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(2) > button:nth-child(2)').click());
+      '#create-env').click());
     await waitForCoreResponse(page, reqTimeout);
 
     const location = await page.evaluate(() => window.location);
