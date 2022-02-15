0bdef680569de076c4e3c699cc7844082d6c9729
Adam Wegrzynek <awegrzyn@users.noreply.github.com>
2022-02-15 12:01:05 +0100

    [OGUI-798] Use token scopes for authorisation (#1095)

--- Framework/Backend/http/server.js
+++ Framework/Backend/http/server.js
@@ -244,7 +244,7 @@ class HttpServer {
       query.username = 'anonymous';
       query.name = 'Anonymous';
       query.token = this.jwt.generateToken(query.personid, query.username, query.name);
-      query.access = 0;
+      query.access = ''
 
       const homeUrlAuthentified = url.format({pathname: '/', query: query});
       return res.redirect(homeUrlAuthentified);
@@ -443,17 +443,14 @@ class HttpServer {
   /**
    * Provides access level number for JWT token depending on users' role
    * @param {object} details - user details
-   * @return {number} - access level based on role
+   * @return {string} - comma separated access roles
    */
   authorise(details) {
-    let accessLevel = 1;
+    let scope = [];
     if (details.hasOwnProperty('resource_access')) {
-      const roles = details.resource_access[Object.keys(details.resource_access)[0]].roles;
-      if (roles.includes('admin')) {
-        accessLevel = 2;
-      }
+      scope = details.resource_access[Object.keys(details.resource_access)[0]].roles;
     }
-    return accessLevel;
+    return scope.join(',');
   }
 
   /**
