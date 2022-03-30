84e11e15e5869b36c538a43180a10faeb2e270d3
George R <george.raduta@cern.ch>
2022-03-30 21:45:52 +0200

    [OGUI-1133] Dispaly description on hover of info template (#1200)
    * AliECS Core now sends descriptions for each template as set by their creators. This in turn will be displayed on-hover of the info icon from each template
    * If the repository it is a mirror, then the info icon will not redirect to the definition of the templates

--- Control/public/workflow/Workflow.js
+++ Control/public/workflow/Workflow.js
@@ -506,7 +506,8 @@ export default class Workflow extends Observable {
     } else {
       const templateList = [];
       result.workflowTemplates.map((templateObject) => {
-        templateList.push(templateObject.template);
+        const description = templateObject.description ? templateObject.description : '';
+        templateList.push({name: templateObject.template, description});
         if (templateObject.varSpecMap && Object.keys(templateObject.varSpecMap).length > 0) {
           this.templatesVarsMap[templateObject.template] = templateObject.varSpecMap;
         }
