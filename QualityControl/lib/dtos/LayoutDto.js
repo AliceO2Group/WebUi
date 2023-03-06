90c8d99f747d50d6d8d6fbc86cd28e981a0c2d63
George R <george.raduta@cern.ch>
2023-03-06 17:52:03 +0100

    [OGUI-1331] Increase limit of tabs per layout (#1827)

--- QualityControl/lib/dtos/LayoutDto.js
+++ QualityControl/lib/dtos/LayoutDto.js
@@ -41,7 +41,7 @@ const UserDto = Joi.object({
 export const LayoutDto = Joi.object({
   id: Joi.string().required(),
   name: Joi.string().min(3).max(40).required(),
-  tabs: Joi.array().min(1).max(20).items(TabsDto).required(),
+  tabs: Joi.array().min(1).max(45).items(TabsDto).required(),
   owner_id: Joi.number().min(0).required(),
   owner_name: Joi.string().required(),
   collaborators: Joi.array().items(UserDto).default([]),
