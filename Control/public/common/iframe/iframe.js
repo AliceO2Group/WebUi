0d7b106cc8360dbccbce1aed22405f14e78c8ed6
George Raduta <george.raduta@cern.ch>
2023-03-08 09:51:19 +0100

    Add iframe component to be reused for monitoring panels

---
+++ Control/public/common/iframe/iframe.js
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+ * See http://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+ * All rights not expressly granted are reserved.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+*/
+
+import {h} from '/js/src/index.js';
+
+/**
+ * Builds an iframe with specified source and no borders
+ * @param {string} src - source of data for iframe
+ * @param {string} [style] - optional style parameters
+ * @returns {vnode}
+ */
+export const iframe = (src, style = 'border:0;') => h('iframe', {src, style});
