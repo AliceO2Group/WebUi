fb4dfe6f74126ba8249ff0d9c539aa8075219e07
George Raduta <george.raduta@cern.ch>
2023-03-21 17:27:09 +0100

    Add export for miniCardTitle

--- Control/public/common/card/miniCard.js
+++ Control/public/common/card/miniCard.js
@@ -18,12 +18,28 @@ import {h} from '/js/src/index.js';
  * Builds a miniCard frame and adds inside the passed children
  * @param {Array<node>|string} title - components or string to be displayed as title
  * @param {Array<vnode>|vnode} [children] - child components to be added inside the miniCard
- * @param {object} style - object containing style parameters
+ * @param {object} [style] - object containing style parameters
  * @returns {vnode}
  */
 export const miniCard = (title, children = [], style = {}) => {
   return h('.miniCard.flex-column.shadow-level1.br2.p2.g2', {style}, [
-    title && h('h4', {style: 'text-decoration:underline'}, title),
+    typeof title === 'string' ?
+      miniCardTitle(title)
+      : title,
     children,
+  ]);
+}
+
+/**
+ * Builds a title group for a mini-card
+ * @param {string} main - main part of the title group
+ * @param {string} [sub] -  optional sub-title to be added at the end of the title group
+ * @returns {vnode}
+ */
+export const miniCardTitle = (main, sub = '') => {
+  const width = !sub ? '100' : '60';
+  return h(`.w-100.flex-row`, [
+    h(`h4.w-${width}`, {style: 'text-decoration: underline'}, main),
+    h('.w-40.text-right', {style: 'font-style: italic;'}, sub)
   ]);
 }
