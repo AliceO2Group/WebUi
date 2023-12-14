e1ff7bfb92a9263fdf2256d1dcb50a35796e2e47
martinboulais <31805063+martinboulais@users.noreply.github.com>
2023-12-14 15:20:04 +0100

    Limit change to Framework

--- Control/public/pages/CalibrationRuns/components/calibrationActionCard.js
+++ Control/public/pages/CalibrationRuns/components/calibrationActionCard.js
@@ -29,7 +29,7 @@ import {getTaskShortName} from './../../../common/utils.js';
  */
 export const calibrationActionCard = (calibrationConfiguration, ongoingCalibrationRun, detector, onclick) => {
   const {runType, configuration, label} = calibrationConfiguration;
-  if (!(ongoingCalibrationRun instanceof RemoteData)) {
+  if (!ongoingCalibrationRun?.kind) {
     ongoingCalibrationRun = RemoteData.success(ongoingCalibrationRun);
   }
 
@@ -49,7 +49,7 @@ export const calibrationActionCard = (calibrationConfiguration, ongoingCalibrati
       ]),
       h('small', `${configuration}`)
     ]), [
-      ongoingCalibrationRun?.match({
+      ongoingCalibrationRun && ongoingCalibrationRun.match({
         NotAsked: () => null,
         Loading: () => null,
         Success: (result) => calibrationEventsDisplay(result?.events),
