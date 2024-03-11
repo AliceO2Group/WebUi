7daeb4c9c480ce7873f49f5c4c85e84643d52f59
George Raduta <george.raduta@cern.ch>
2024-03-11 17:58:40 +0100

    Remove waitForTimeout usage

--- InfoLogger/test/public/live-mode-mocha.js
+++ InfoLogger/test/public/live-mode-mocha.js
@@ -16,26 +16,17 @@
 
 const assert = require('assert');
 const test = require('../mocha-index');
-const {createServer, closeServer} = require('../live-simulator/infoLoggerServer.js');
 
 describe('Live Mode test-suite', async () => {
   let baseUrl;
   let page;
-  let ilgServer;
   before(async () => {
     baseUrl = test.helpers.baseUrl;
     page = test.page;
-    // Start infologgerserver simulator
-    ilgServer = createServer();
-    await page.waitForTimeout(5000);
+
   });
 
-  after(async () => {
-    closeServer(ilgServer);
-    await page.waitForTimeout(5000);
-  });
-
-  it('should go to homepage', async function() {
+  it('should successfully go to homepage with predefined filters', async function() {
     await page.goto(baseUrl, {waitUntil: 'networkidle0'});
     const location = await page.evaluate(() => window.location);
     const search = decodeURIComponent(location.search);
@@ -64,24 +55,20 @@ describe('Live Mode test-suite', async () => {
     assert.strictEqual(criterias.level.$max, 21);
 
     // Wait for logs and count them (2-3 maybe, it's random)
-    await page.waitForTimeout(1500); // simulator is set to ~100ms per log
-    const list = await page.evaluate(() => {
-      return window.model.log.list;
-    });
+    await page.waitForFunction(`window.model.log.list.length > 0`, {timeout: 5000});
+    const list = await page.evaluate(() => window.model.log.list);
     assert.ok(!!list.length);
   });
 
   it('should filter messages based on `hostname` matching `aldaqecs01-v1` from live -> paused -> live', async () => {
     await page.evaluate(() => window.model.log.liveStop('Paused'));
-    await page.waitForTimeout(2000);
     await page.evaluate(() => {
       window.model.log.filter.resetCriteria();
       window.model.log.filter.setCriteria('hostname', 'match', 'aldaqecs01-v1');
     });
     await page.evaluate(() => window.model.log.liveStart());
-    await page.waitForTimeout(7000);
+    await page.waitForFunction(`window.model.log.list.length > 10`, {timeout: 5000});
     const list = await page.evaluate(() => window.model.log.list);
-    await page.waitForTimeout(1000);
     const isHostNameMatching = list.map((element) => element.hostname).every((hostname) => hostname === 'aldaqecs01-v1');
     assert.ok(list.length > 0);
     assert.ok(isHostNameMatching);
@@ -94,7 +81,8 @@ describe('Live Mode test-suite', async () => {
       window.model.log.filter.setCriteria('hostname', 'exclude', 'aldaqdip01');
     });
     await page.evaluate(() => window.model.log.liveStart());
-    await page.waitForTimeout(3000);
+    await page.waitForFunction(`window.model.log.list.length > 5`, {timeout: 5000});
+
     const list = await page.evaluate(() => window.model.log.list);
     const isHostNameMatching = list.map((element) => element.hostname).every((hostname) => hostname !== 'aldaqdip01');
 
@@ -109,7 +97,7 @@ describe('Live Mode test-suite', async () => {
       window.model.log.setCriteria('username', 'match', 'a_iceda_');
       window.model.log.empty();
     });
-    await page.waitForTimeout(3000);
+    await page.waitForFunction(`window.model.log.list.length > 5`, {timeout: 5000});
     const list = await page.evaluate(() => window.model.log.list);
     const isHostNameMatching = list.map((element) => element.hostname).every((hostname) => !new RegExp('.*ldaqdip.*').test(hostname));
     const isUserNameMatching = list.map((element) => element.username).every((username) => new RegExp('a.iceda.').test(username));
@@ -122,7 +110,7 @@ describe('Live Mode test-suite', async () => {
   it('successfully show indicator when user double pressed the log row', async () => {
     const tableRow = await page.$('body > div:nth-child(2) > div:nth-child(2) > main > div > div > div > table > tbody > tr');
     await tableRow.click({clickCount: 2});
-    await page.waitForTimeout(200);
+
     const indicatorOpen = await page.evaluate(() => window.model.inspectorEnabled);
     assert.ok(indicatorOpen);
   });
