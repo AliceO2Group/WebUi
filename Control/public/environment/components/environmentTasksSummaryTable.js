8d1d5125b949f57571b3b2ba135c07ca32c119ab
George Raduta <george.raduta@cern.ch>
2024-05-30 09:12:10 +0200

    Add check for environment not to be in a transition to display SOR

--- Control/public/environment/components/environmentTasksSummaryTable.js
+++ Control/public/environment/components/environmentTasksSummaryTable.js
@@ -24,12 +24,14 @@ import {h} from '/js/src/index.js';
  * @return {vnode} - component with an HTML table
  */
 export const environmentTasksSummaryTable = (environment, detectorsAvailability) => {
-  const {state, hardware, userVars} = environment;
+  const {state, hardware, userVars, currentTransition = undefined} = environment;
   const isDcsEnabled = userVars?.dcs_enabled === 'true';
+  const shouldDisplaySorAvailability = isDcsEnabled && state === 'CONFIGURED' && !currentTransition;
+  
   return h('table.table-ecs.table-ecs-sm.shadow-level1', [
     h('thead', [
       hardwareComponentsTableHeaderRow(hardware),
-      detectorsTableHeaderRow(hardware, detectorsAvailability, state, isDcsEnabled),
+      detectorsTableHeaderRow(hardware, detectorsAvailability, shouldDisplaySorAvailability)
     ]),
     h('tbody', [
       FLP_TASK_STATES.map((state) => rowForTaskSate(state, hardware)),
@@ -61,16 +63,15 @@ const hardwareComponentsTableHeaderRow = (hardware) => h('tr', [
  * Build the HTML header row of the table with the detectors present in the environment
  * @param {Object<String, Object>} detectorCounters - object with the detectors counters of tasks
  * @param {Object<String, {pfrAvailability, sorAvailability}} availability - object with the availability of the detectors
- * @param {String} state - state of the environment 
- * @param {Boolean} isDcsEnabled - flag to check if DCS is enabled
+ * @param {Boolean} shouldDisplaySorAvailability - flag to display the SOR availability
  * @return {vnode} - component with an HTML table row
  */
-const detectorsTableHeaderRow = ({flp: {detectorCounters = {}} = {}}, availability, state, isDcsEnabled) =>
+const detectorsTableHeaderRow = ({flp: {detectorCounters = {}} = {}}, availability, shouldDisplaySorAvailability) =>
   h('tr', [
     h('th', 'States'),
     Object.keys(detectorCounters).map((detector) => h('th.text-center', [
       detector,
-      isDcsEnabled && state === 'CONFIGURED' && h('.f6', dcsProperty(availability[detector].sorAvailability, 'SOR'))
+      shouldDisplaySorAvailability && h('.f6', dcsProperty(availability[detector].sorAvailability, 'SOR'))
     ])),
     h('th.text-center', {colspan: HARDWARE_COMPONENTS_WITHOUT_EPN.length - 1}, ''), // empty cell to align with the rest of the table
   ]);
