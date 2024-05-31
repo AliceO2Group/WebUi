ff00654acd456f1e4b4cf368c3e6afb1384576fc
George Raduta <george.raduta@cern.ch>
2024-05-31 21:40:49 +0200

    Add option in API to take/release all locks with/without force

--- Control/test/lib/controllers/mocha-lock.controller.test.js
+++ Control/test/lib/controllers/mocha-lock.controller.test.js
@@ -18,7 +18,9 @@ const sinon = require('sinon');
 
 const {LockController} = require('./../../../lib/controllers/Lock.controller.js');
 const {LockService} = require('./../../../lib/services/Lock.service.js');
+const {User} = require('./../../../lib/dtos/User.js');
 const {DetectorLockAction} = require('../../../lib/common/lock/detectorLockAction.enum.js');
+const {DetectorLockState} = require('../../../lib/common/lock/detectorLockState.enum.js');
 
 describe(`'LockController' test suite`, () => {
   const res = {
@@ -45,12 +47,12 @@ describe(`'LockController' test suite`, () => {
       assert.ok(res.json.calledWith({
         ABC: {
           name: 'ABC',
-          state: 'FREE',
+          state: DetectorLockState.FREE,
           owner: undefined,
         },
-        XYZ:  {
+        XYZ: {
           name: 'XYZ',
-          state: 'FREE',
+          state: DetectorLockState.FREE,
           owner: undefined,
         },
       }));
@@ -74,16 +76,16 @@ describe(`'LockController' test suite`, () => {
       assert.ok(res.json.calledWith({
         ABC: {
           name: 'ABC',
-          state: 'TAKEN',
+          state: DetectorLockState.TAKEN,
           owner: {
             personid: 0,
             fullName: 'Anonymous',
             username: 'anonymous',
           }
         },
-        XYZ:  {
+        XYZ: {
           name: 'XYZ',
-          state: 'FREE',
+          state: DetectorLockState.FREE,
           owner: undefined,
         },
       }));
@@ -107,7 +109,7 @@ describe(`'LockController' test suite`, () => {
     it('should reply with error when an already held lock is requested to be released by another user without force', () => {
       lockController.actionLockHandler({
         params: {
-          action: 'RELEASE',
+          action: DetectorLockAction.RELEASE,
           detectorId: 'ABC',
         }, session: {
           personid: 1,
@@ -122,7 +124,7 @@ describe(`'LockController' test suite`, () => {
     it('should successfully reply to a request to release lock held by correct owner', () => {
       lockController.actionLockHandler({
         params: {
-          action: 'RELEASE',
+          action: DetectorLockAction.RELEASE,
           detectorId: 'ABC',
         }, session: {
           personid: 0,
@@ -134,12 +136,12 @@ describe(`'LockController' test suite`, () => {
       assert.ok(res.json.calledWith({
         ABC: {
           name: 'ABC',
-          state: 'FREE',
+          state: DetectorLockState.FREE,
           owner: undefined,
         },
-        XYZ:  {
+        XYZ: {
           name: 'XYZ',
-          state: 'FREE',
+          state: DetectorLockState.FREE,
           owner: undefined,
         },
       }));
@@ -148,7 +150,7 @@ describe(`'LockController' test suite`, () => {
     it('should return InvalidInput error code for invalid request', () => {
       lockController.actionLockHandler({
         params: {
-          action: 'RELEASE',
+          action: DetectorLockAction.RELEASE,
         }, session: {
           personid: 0,
           name: 'Anonymous',
@@ -175,7 +177,7 @@ describe(`'LockController' test suite`, () => {
     it('should return 404 and not found error message for a detector that does not exist', () => {
       lockController.actionLockHandler({
         params: {
-          action: 'RELEASE',
+          action: DetectorLockAction.RELEASE,
           detectorId: 'NONEXISTENT'
         }, session: {
           personid: 0,
@@ -185,6 +187,71 @@ describe(`'LockController' test suite`, () => {
       }, res);
       assert.ok(res.status.calledWith(404));
       assert.ok(res.json.calledWith({message: 'Detector NONEXISTENT not found in the list of detectors'}));
+    });
+
+    it('should successfully lock all detectors available to lock', () => {
+      lockService.takeLock('ABC', new User('anonymous', 'Anonymous', 0, ['global']));
+      lockController.actionLockHandler({
+        params: {
+          action: DetectorLockAction.TAKE,
+          detectorId: 'ALL'
+        }, session: {
+          personid: 1,
+          name: 'OneAnonymous',
+          username: 'oneanonymous',
+        }
+      }, res);
+      assert.ok(res.status.calledWith(200));
+      assert.ok(res.json.calledWith({
+        ABC: {
+          name: 'ABC',
+          state: DetectorLockState.TAKEN,
+          owner: {
+            fullName: 'Anonymous',
+            personid: 0,
+            username: 'anonymous',
+          },
+        },
+        XYZ: {
+          name: 'XYZ',
+          state: DetectorLockState.TAKEN,
+          owner: {
+            fullName: 'OneAnonymous',
+            personid: 1,
+            username: 'oneanonymous',
+          },
+        },
+      }));
+    });
+
+    it('should successfully release all detectors available to lock', () => {
+      lockController.actionLockHandler({
+        params: {
+          action: DetectorLockAction.RELEASE,
+          detectorId: 'ALL'
+        }, session: {
+          personid: 1,
+          name: 'OneAnonymous',
+          username: 'oneanonymous',
+        }
+      }, res);
+      assert.ok(res.status.calledWith(200));
+      assert.ok(res.json.calledWith({
+        ABC: {
+          name: 'ABC',
+          state: DetectorLockState.TAKEN,
+          owner: {
+            fullName: 'Anonymous',
+            personid: 0,
+            username: 'anonymous',
+          },
+        },
+        XYZ: {
+          name: 'XYZ',
+          state: DetectorLockState.FREE,
+          owner: undefined,
+        },
+      }));
     });
   });
 
@@ -205,16 +272,16 @@ describe(`'LockController' test suite`, () => {
       assert.ok(res.json.calledWith({
         ABC: {
           name: 'ABC',
-          state: 'TAKEN',
+          state: DetectorLockState.TAKEN,
           owner: {
             personid: 0,
             fullName: 'Anonymous',
             username: 'anonymous',
           }
         },
-        XYZ:  {
+        XYZ: {
           name: 'XYZ',
-          state: 'FREE',
+          state: DetectorLockState.FREE,
           owner: undefined,
         },
       }));
@@ -235,16 +302,79 @@ describe(`'LockController' test suite`, () => {
       assert.ok(res.json.calledWith({
         ABC: {
           name: 'ABC',
-          state: 'TAKEN',
+          state: DetectorLockState.TAKEN,
           owner: {
             personid: 1,
             fullName: 'NotAnonymous',
             username: 'notanonymous',
           }
         },
-        XYZ:  {
+        XYZ: {
           name: 'XYZ',
-          state: 'FREE',
+          state: DetectorLockState.FREE,
+          owner: undefined,
+        },
+      }));
+    });
+
+    it('should successfully respond to a request to take ALL locks by force', () => {
+      lockController.actionForceLockHandler({
+        params: {
+          action: DetectorLockAction.TAKE,
+          detectorId: 'ALL',
+        }, session: {
+          personid: 22,
+          username: 'twotwoanonymous',
+          name: 'TwoTwoAnonymous',
+          access: ['admin']
+        }
+      }, res);
+      assert.ok(res.status.calledWith(200));
+      assert.ok(res.json.calledWith({
+        ABC: {
+          name: 'ABC',
+          state: DetectorLockState.TAKEN,
+          owner: {
+            personid: 22,
+            fullName: 'TwoTwoAnonymous',
+            username: 'twotwoanonymous',
+          }
+        },
+        XYZ: {
+          name: 'XYZ',
+          state: DetectorLockState.TAKEN,
+          owner: {
+            personid: 22,
+            fullName: 'TwoTwoAnonymous',
+            username: 'twotwoanonymous',
+          }
+        },
+      }));
+    });
+
+    it('should successfully respond to a request to release ALL locks by force', () => {
+      lockService.takeLock('ABC', new User('anonymous', 'Anonymous', 0, ['global']), true); // now one lock is under personid 0
+      lockController.actionForceLockHandler({
+        params: {
+          action: DetectorLockAction.RELEASE,
+          detectorId: 'ALL',
+        }, session: {
+          personid: 22,
+          username: 'twotwoanonymous',
+          name: 'TwoTwoAnonymous',
+          access: ['admin']
+        }
+      }, res);
+      assert.ok(res.status.calledWith(200));
+      assert.ok(res.json.calledWith({
+        ABC: {
+          name: 'ABC',
+          state: DetectorLockState.FREE,
+          owner: undefined,
+        },
+        XYZ: {
+          name: 'XYZ',
+          state: DetectorLockState.FREE,
           owner: undefined,
         },
       }));
