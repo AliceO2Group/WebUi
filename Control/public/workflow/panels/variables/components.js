2ec644c69bf0793a8492d5f87b03c0ae402d451f
George Raduta <george.raduta@cern.ch>
2024-07-17 11:16:00 +0200

    Arrange switch component on center vertically

--- Control/public/workflow/panels/variables/components.js
+++ Control/public/workflow/panels/variables/components.js
@@ -308,14 +308,13 @@ const checkBox = (variable, model) => {
     variableLabel(variable,
       () => model.workflow.updateBasicVariableByKey(variable.key, value === 'true' ? 'false' : 'true')
     ),
-    h('', {
-      class: 'w-50 flex-row flex-wrap text-left',
+    h('.w-50.flex-column.flex-center.actionable-row', {
       onclick: (e) => {
         e.stopPropagation();
         model.workflow.updateBasicVariableByKey(variable.key, value === 'true' ? 'false' : 'true');
       }
     }, [
-      h('label.form-check.switch', [
+      h('label.form-check.switch.m0', [
         h('input.form-check-input', {
           onclick: (e) => e.stopPropagation(),
           type: 'checkbox',
