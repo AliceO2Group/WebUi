ffe82d66e6ffa3b53ce1c94a38a550f6ecef7012
George Raduta <george.raduta@cern.ch>
2024-07-29 18:56:24 +0200

    [OGUI-1526] Extract model and component for reuse with regards to task pages and information (#2513)
    * adds new `TaskTableModel` to allow for tasks table new functionality to be added
    * extracts flp tasks table to be reused across `environment` and `taskList` pages
    * adds new utility function `jsonPost` to allow fetch of information without root model

---
+++ Control/public/common/task/TaskTableModel.js
@@ -0,0 +1,101 @@
+/**
+ * @license
+ * Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+ * See http://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+ * All rights not expressly granted are reserved.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+*/
+
+import { Observable, RemoteData } from '/js/src/index.js';
+import { jsonPost } from './../../utilities/jsonPost.js';
+
+/**
+ * Model representing tasks within a table
+ */
+export class TaskTableModel extends Observable {
+  /**
+   * Initializes model with default values
+   * @param {Model} model - reference to the main model
+   */
+  constructor(model) {
+    super();
+
+    this._model = model;
+    this._openedTaskViews = {}; // map of the view states of the tasks. If present, task view is open
+    this._tasksAsRemoteDataById = {}; // map of (taskId, RemoteData) fetched for in-depth details of the task
+  }
+
+  /**
+   * Getter for retrieving the view state of the tasks
+   * @return {object<id: string, isOpen: boolean} - view state of the tasks
+   */
+  get openedTaskViews() {
+    return this._openedTaskViews;
+  }
+
+  /**
+   * Getter for retrieving the tasks as RemoteData by their id
+   * @return {object<id: string, data: RemoteData>} - task as RemoteData by their id
+   */
+  get tasksAsRemoteDataById() {
+    return this._tasksAsRemoteDataById;
+  }
+
+  /**
+   * Toggle by task id the state of open/close to display in-depth details of a task
+   * @param {string} taskId
+  */
+  async toggleTaskView(taskId) {
+    if (this._openedTaskViews[taskId]) {
+      delete this._openedTaskViews[taskId];
+      this.notify();
+      return;
+    }
+    this._openedTaskViews[taskId] = true;
+    this._getTaskById({ taskId: taskId });
+  }
+
+  /**
+   * Method to make a POST HTTP Request to get all details about a task by its Id
+   * @param {object} body - {taskId: string}
+   */
+  async _getTaskById(body) {
+    const { taskId } = body;
+    this.tasksAsRemoteDataById[taskId] = RemoteData.loading();
+    this.notify();
+
+    try {
+      const result = await jsonPost(`/api/GetTask`, { body });
+      const { task } = result;
+      const commandInfo = this._parseTaskInfo(task, taskId);
+      this.tasksAsRemoteDataById[taskId] = RemoteData.success(commandInfo);
+    } catch (error) {
+      this.tasksAsRemoteDataById[taskId] = RemoteData.failure(error.message);
+    }
+    this.notify();
+  }
+
+  /**
+   * Method to add & remove fields regarding the task
+   * @param {object} taskInfo - task command info
+   * @param {string} taskId - task id for which to parse the command info
+   * @return {object} - parsed command info
+   */
+  _parseTaskInfo(task, taskId) {
+    const commandInfo = task.commandInfo;
+    delete commandInfo.shell;
+    commandInfo.env = commandInfo.env.join('\n');
+    commandInfo.arguments = commandInfo.arguments.join(' ');
+    commandInfo.taskId = taskId;
+
+    commandInfo.className = task.shortInfo.className;
+    commandInfo.mesosLog = task.shortInfo.sandboxStdout;
+    return commandInfo;
+  }
+}
--- Control/public/common/task/tasksPerFlpTable.js
+++ Control/public/common/task/tasksPerFlpTable.js
@@ -17,29 +17,32 @@
 import {
   h, iconChevronBottom, iconLockLocked, iconLockUnlocked, iconChevronTop, iconCircleX
 } from '/js/src/index.js';
-import { getTasksByFlp } from './../utils.js';
-import { infoLoggerButtonLink } from './../buttons/infoLoggerRedirectButton.js';
-import { redirectButtonLink } from './../buttons/redirectButtonLink.js';
-import pageLoading from './../pageLoading.js';
-import showTableItem from './../showTableItem.js';
-import { getTaskStateClassAssociation } from './../enums/TaskState.js';
+import { getTasksByFlp } from '../utils.js';
+import { infoLoggerButtonLink } from '../buttons/infoLoggerRedirectButton.js';
+import { redirectButtonLink } from '../buttons/redirectButtonLink.js';
+import pageLoading from '../pageLoading.js';
+import showTableItem from '../showTableItem.js';
+import { getTaskStateClassAssociation } from '../enums/TaskState.js';
 
 /**
  * Build multiple tables of the tasks grouped by FLP
  * @param {EnvironmentModel} environmentModel - environment model from which to extract what is needed
- * @param {TaskModel} environmentModel.task - task model
- * @param {EnvironmentInfo} environmentInfo - environment information DTO object
- * @return {vnode}
+ * @param {TaskTableModel} environmentModel.taskTableModel - task model
+ * @param {PartialEnvironmentInfo} environmentInfo - environment information DTO object
+ * @param {Array<Task>} [environmentInfo.tasks] - list of tasks to build table for
+ * @param {string} [environmentInfo.currentTransition] - current transition if any
+ * @param {number} [environmentInfo.currentRunNumber] - current run number
+ * @return {vnode} - multiple tables of the tasks grouped by their FLP/host
  */
-export const tasksPerFlpTable = ({ task: taskModel }, environmentInfo) => {
-  const {tasks = [], currentTransition = '', currentRunNumber: run} = environmentInfo;
+export const tasksPerFlpTable = ({ taskTableModel }, { tasks = [], currentTransition = '', currentRunNumber: run }) => {
   if (tasks.length === 0 && !currentTransition) {
     return h('.text-center.w-100', 'No tasks found');
   }
+
   const tasksByFlp = getTasksByFlp(tasks);
   return [
-    Object.keys(tasksByFlp).map((hostname) =>
-      h('', [
+    Object.keys(tasksByFlp)
+      .map((hostname) => h('', [
         h('.p2.flex-row.bg-primary.white', [
           h('h5.flex-grow-3', hostname),
           h('.flex-row.flex-grow-1.g2', [
@@ -47,46 +50,47 @@ export const tasksPerFlpTable = ({ task: taskModel }, environmentInfo) => {
             redirectButtonLink(tasksByFlp[hostname].stdout, 'Mesos', 'Download Mesos logs', true),
           ]),
         ]),
-        showTasksTable(taskModel, tasksByFlp[hostname].list)
-      ])
-    ),
+        showTasksTable(taskTableModel, tasksByFlp[hostname].list)
+      ])),
   ];
 };
 
 /**
  * Method to create and display a table with tasks details
- * @param {taskModel} taskModel - task model
+ * @param {TaskTableModel} taskTableModel - task table model
  * @param {Array<object>} tasks - list of tasks that is to be displayed
  * @return {vnode} - table with tasks details
  */
-const showTasksTable = (taskModel, tasks) => {
+export const showTasksTable = (taskTableModel, tasks) => {
+  const tableColumns = ['Name', 'PID', 'Locked', 'Status', 'State', 'Host Name', 'More'];
+
   return h('.scroll-auto.panel', [
     h('table.table.table-sm', {style: 'margin-bottom: 0'}, [
       h('thead',
         h('tr', [
-          ['Name', 'PID', 'Locked', 'Status', 'State', 'Host Name', 'More']
-            .map((header) => h('th', header))
+          tableColumns.map((header) => h('th', header)),
         ])
       ),
       h('tbody', [
-        tasks.map((task) => [h('tr', [
-          h('td.w-30', task.name),
-          h('td.w-10', task.pid),
-          h('td.w-10',
-            h('.flex-row.items-center.justify-center.w-33', task.locked ? iconLockLocked() : iconLockUnlocked())
-          ),
-          h('td.w-10', task.status),
-          h(`td.w-10${getTaskStateClassAssociation(task.state)}`, task.state),
-          h('td.w-20', task.deploymentInfo.hostname),
-          h('td.w-10',
-            h('button.btn-sm.btn-default', {
-              title: 'More Details',
-              onclick: () => taskModel.toggleTaskView(task.taskId),
-            }, taskModel.openedTasks[task.taskId] ? iconChevronTop() : iconChevronBottom())
-          ),
-        ]),
-        taskModel.openedTasks[task.taskId] && taskModel.list[task.taskId]
-        && showTaskDetailsTable(taskModel.list[task.taskId]),
+        tasks.map((task) => [
+          h('tr', [
+            h('td.w-30', task.name),
+            h('td.w-10', task.pid),
+            h('td.w-10',
+              h('.flex-row.items-center.justify-center.w-33', task.locked ? iconLockLocked() : iconLockUnlocked())
+            ),
+            h('td.w-10', task.status),
+            h(`td.w-10${getTaskStateClassAssociation(task.state)}`, task.state),
+            h('td.w-20', task.deploymentInfo?.hostname),
+            h('td.w-10',
+              h('button.btn-sm.btn-default', {
+                title: 'More Details',
+                onclick: () => taskTableModel.toggleTaskView(task.taskId),
+              }, taskTableModel.openedTaskViews[task.taskId] ? iconChevronTop() : iconChevronBottom())
+            ),
+          ]),
+          taskTableModel.openedTaskViews[task.taskId] && taskTableModel.tasksAsRemoteDataById[task.taskId]
+          && showTaskDetailsTable(taskTableModel.tasksAsRemoteDataById[task.taskId]),
         ]),
       ])
     ])
