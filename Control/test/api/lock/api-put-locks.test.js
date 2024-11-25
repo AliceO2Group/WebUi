bcfb562cfb28947fc280a0fc427cd55c9e5c1a2e
pdik <pepijn@pdik.nl>
2024-11-25 10:18:21 +0100

    Added Tests: Locks/All  needs Global role

--- Control/test/api/lock/api-put-locks.test.js
+++ Control/test/api/lock/api-put-locks.test.js
@@ -165,20 +165,36 @@ describe(`'API - PUT - /locks/:action/:detectorId' test suite`, () => {
       });
   });
 
-  it('should return 403 error for Guest user on locks ALL', async () => {
-    await request(`${TEST_URL}/api/locks`)
-      .put(`/${DetectorLockAction.TAKE}/ALL?token=${GUEST_TEST_TOKEN}`)
-      .expect(403, { message: 'Not enough permissions for this operation' });
-  });
-
-  it('should allow access for Global user on locks ALL', async () => {
+  it('should successfully Take ALL locks when >=global ', async () => {
+    // first we retake a lock to ensure we have a lock to release from different types of users
     await request(`${TEST_URL}/api/locks`)
       .put(`/${DetectorLockAction.TAKE}/ALL?token=${GLOBAL_TEST_TOKEN}`)
-      .expect(200,{
-        MID: { name: 'MID', state: 'TAKEN',owner: { username: 'global', fullName: 'Global User', personid: 1 }},
-        DCS: { name: 'DCS', state: 'TAKEN',owner: { username: 'global', fullName: 'Global User', personid: 1 } },
+      .expect(200, {
+
+        DCS: { name: 'DCS', state: 'TAKEN', owner: { username: 'global', fullName: 'Global User', personid: 1 } },
+        MID: { name: 'MID', state: 'TAKEN', owner: { username: 'global', fullName: 'Global User', personid: 1 } },
         ODC: { name: 'ODC', state: 'TAKEN', owner: { username: 'global', fullName: 'Global User', personid: 1 } },
+      
       });
+
+    await request(`${TEST_URL}/api/locks`)
+      .put(`/${DetectorLockAction.RELEASE}/ALL?token=${GLOBAL_TEST_TOKEN}`)
+      .expect(200, {
+        MID: { name: 'MID', state: 'FREE' },
+        DCS: { name: 'DCS', state: 'FREE' },
+        ODC: { name: 'ODC', state: 'FREE' },
+      });
+  });
+
+  it('should fail taking  ALL locks when <=global ', async () => {
+    // first we retake a lock to ensure we have a lock to release from different types of users
+    await request(`${TEST_URL}/api/locks`)
+      .put(`/${DetectorLockAction.TAKE}/ALL?token=${GUEST_TEST_TOKEN}`)
+      .expect(403);
+
+    await request(`${TEST_URL}/api/locks`)
+      .put(`/${DetectorLockAction.RELEASE}/ALL?token=${GUEST_TEST_TOKEN}`)
+      .expect(403);
   });
 });
 
