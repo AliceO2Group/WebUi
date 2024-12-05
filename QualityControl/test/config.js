b830346e4252c84e6508279f31b5d9ac276becdf
Alejandro Mariscal Romero <87366244+mariscalromeroalejandro@users.noreply.github.com>
2024-12-05 11:18:38 +0100

    refactor test config to match bookkeeping key

--- QualityControl/test/config.js
+++ QualityControl/test/config.js
@@ -35,7 +35,7 @@ export const config = {
   },
   dbFile: './test/setup/seeders/qcg-mock-data.json',
 
-  bkp: {
+  bookkeeping: {
     url: 'http://alio2-cr1-hv-mvs00.cern.ch:4000',
     token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg3OTM3OSIsInVzZXJuYW1lIjoiYWxtYXJpc2MiLCJuYW1lIjoiQWxlamFuZHJvIE1hcmlzY2FsIFJvbWVybyIsImFjY2VzcyI6ImRlZmF1bHQtcm9sZSIsImlhdCI6MTczMzMyNzI1NywiZXhwIjoxNzMzOTMyMDU3LCJpc3MiOiJvMi11aSJ9.tvfVW9fuVMlYZ0BnduAXQhaaYnFYxbONZofa0B9E2n4',
     refreshRate: 10,
