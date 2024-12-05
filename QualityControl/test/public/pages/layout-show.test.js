c897dbad428fd461692175e56ec6eadb16ac2a45
Alejandro Mariscal Romero <87366244+mariscalromeroalejandro@users.noreply.github.com>
2024-12-05 16:10:11 +0100

    Merge changes from dev

--- QualityControl/test/public/pages/layout-show.test.js
+++ QualityControl/test/public/pages/layout-show.test.js
@@ -13,6 +13,7 @@
 
 import { strictEqual, ok, deepStrictEqual } from 'node:assert';
 import { delay } from '../../testUtils/delay.js';
+import { editedMockedLayout } from '../../setup/seeders/layout-show/json-file-mock.js';
 
 /**
  * Performs a series of automated tests on the layoutShow page using Puppeteer.
@@ -215,12 +216,21 @@ export const layoutShowTests = async (url, page, timeout = 5000, testParent) => 
   );
 
   await testParent.test(
-    'should have one edit button in the header to go in edit mode',
+    'should have two options for editing the layout',
     { timeout },
     async () => {
-      const buttonPath = 'header > div > div:nth-child(3) > div > button:nth-child(3)';
-      const editButton = await page.evaluate((buttonPath) => document.querySelector(buttonPath).title, buttonPath);
-      strictEqual(editButton, 'Edit layout');
+      const editButtonPath = 'header > div > div:nth-child(3) > div > div > button';
+      await page.locator(editButtonPath).click();
+      const titles = await page.evaluate(() => {
+        const firstLinkPath = 'header > div > div:nth-child(3) > div > div > div > div > a:nth-child(1)';
+        const secondLinkPath = 'header > div > div:nth-child(3) > div > div > div > div > a:nth-child(2)';
+        const firstLinkTitle = document.querySelector(firstLinkPath).title;
+        const secondLinkTitle = document.querySelector(secondLinkPath).title;
+        return [firstLinkTitle, secondLinkTitle];
+      });
+
+      strictEqual(titles[0], 'Edit via GUI');
+      strictEqual(titles[1], 'Edit via JSON');
     },
   );
 
@@ -228,8 +238,8 @@ export const layoutShowTests = async (url, page, timeout = 5000, testParent) => 
     'should click the edit button in the header and enter edit mode',
     { timeout },
     async () => {
-      const editButtonPath = 'header > div > div:nth-child(3) > div > button:nth-child(3)';
-      await page.locator(editButtonPath).click();
+      const editViaGUIButtonPath = 'header > div > div:nth-child(3) > div > div > div > div > a:nth-child(1)';
+      await page.locator(editViaGUIButtonPath).click();
     },
   );
 
@@ -304,4 +314,133 @@ export const layoutShowTests = async (url, page, timeout = 5000, testParent) => 
       await page.waitForSelector('nav .menu-title', { timeout: 5000 });
     },
   );
+
+  await testParent.test(
+    'should open JSON editor when clicking "Edit via JSON"',
+    { timeout },
+    async () => {
+      const editDropdownButtonPath = 'header > div > div:nth-child(3) > div > div > button';
+      const editViaJSONButtonPath = 'header > div > div:nth-child(3) > div > div > div > div > a:nth-child(2)';
+      await page.locator(editDropdownButtonPath).click();
+      await delay(100);
+      await page.locator(editViaJSONButtonPath).click();
+      await delay(100);
+      const result = await page.evaluate(() => {
+        const titlePath = 'body > div > div > div > h4';
+        const textareaPath = 'body > div > div > div > div > textarea';
+        const updateButtonPath = 'body > div > div > div > div > button:nth-child(1)';
+        const cancelButtonPath = 'body > div > div > div > div > button:nth-child(2)';
+        const title = document.querySelector(titlePath).textContent;
+        const textAreaId = document.querySelector(textareaPath).id;
+        const updateButtonTitle = document.querySelector(updateButtonPath).textContent;
+        const cancelButtonTitle = document.querySelector(cancelButtonPath).textContent;
+        return { title, textAreaId, updateButtonTitle, cancelButtonTitle };
+      });
+
+      strictEqual(result.title, 'Edit JSON file of a layout');
+      strictEqual(result.textAreaId, 'layout-json-editor');
+      strictEqual(result.updateButtonTitle, 'Update layout');
+      strictEqual(result.cancelButtonTitle, 'Cancel');
+    },
+  );
+
+  await testParent.test(
+    'should not have ID as an editable key in the JSON editor',
+    { timeout },
+    async () => {
+      const result = await page.evaluate(() => {
+        const textareaPath = 'body > div > div > div > div > textarea';
+        const textAreaValue = document.querySelector(textareaPath).value;
+        const json = JSON.parse(textAreaValue);
+        const jsonDoesNotContainId = !Object.prototype.hasOwnProperty.call(json, 'id');
+        return jsonDoesNotContainId;
+      });
+      strictEqual(result, true);
+    },
+  );
+
+  await testParent.test(
+    'should disable the "Update layout" button when JSON has an "id" key and display error message',
+    { timeout },
+    async () => {
+      const mockJSONWithId = '{ "id" : "test" }';
+      const expectedErrorMessage =
+      'Error: Manual entry of an ID is not allowed, as it is automatically assigned by the system.';
+
+      await checkInvalidJSON(page, mockJSONWithId, expectedErrorMessage);
+    },
+  );
+
+  await testParent.test(
+    'should disable the "Update layout" button when JSON is invalid and display error message',
+    { timeout },
+    async () => {
+      const mockJSONInvalid = '{ "name" : "test" ';
+      const expectedErrorMessage =
+        'Expected \',\' or \'}\' after property value in JSON at position 18 (line 1 column 19)';
+
+      await checkInvalidJSON(page, mockJSONInvalid, expectedErrorMessage);
+    },
+  );
+
+  await testParent.test(
+    'should close JSON editor when clicking "Cancel"',
+    { timeout },
+    async () => {
+      const cancelButtonPath = 'body > div > div > div > div > button:nth-child(2)';
+      await page.locator(cancelButtonPath).click();
+      await delay(50);
+      const childrenCount = await page.evaluate(() => {
+        const bodyPath = 'body';
+        const body = document.querySelector(bodyPath);
+        return body.children.length;
+      });
+      strictEqual(childrenCount, 2);
+    },
+  );
+
+  await testParent.test(
+    'should update layout when clicking "Update layout"',
+    { timeout },
+    async () => {
+      const pencilButtonPath = 'header > div > div:nth-child(3) > div > div > button';
+      await page.locator(pencilButtonPath).click();
+      const editViaJSONButtonPath =
+        'header > div > div:nth-child(3) > div > div > div > div > a:nth-child(2)';
+      page.locator(editViaJSONButtonPath).click();
+
+      const textareaPath = 'body > div > div > div > div > textarea';
+      const mockedJSON = JSON.stringify(editedMockedLayout);
+      await page.locator(textareaPath).fill(mockedJSON);
+
+      const updateButtonPath = 'body > div > div > div > div > button:nth-child(1)';
+      await page.locator(updateButtonPath).click();
+      await delay(50);
+
+      const buttonsPath = 'header > div > div:nth-child(2) > div > button';
+      const result = await page.evaluate((buttonsPath) => {
+        const tabs = document.querySelectorAll(buttonsPath);
+        return tabs.length === 3 && tabs[2].textContent === 'test';
+      }, buttonsPath);
+
+      strictEqual(result, true);
+    },
+  );
+};
+
+const checkInvalidJSON = async (page, mockedJSON, errorMessage) => {
+  const textareaPath = 'body > div > div > div > div > textarea';
+  await page.locator(textareaPath).fill(mockedJSON);
+  await delay(50);
+
+  const [updateButtonIsDisabled, message] = await page.evaluate(() => {
+    const updateButtonPath = 'body > div > div > div > div > button:nth-child(1)';
+    const updateButton = document.querySelector(updateButtonPath);
+    const errorTextPath = 'body > div:nth-child(1) > div > div > div:nth-child(3)';
+    const errorText = document.querySelector(errorTextPath).textContent;
+    return [updateButton.disabled, errorText];
+  });
+
+  strictEqual(updateButtonIsDisabled, true);
+  strictEqual(message, errorMessage);
 };
