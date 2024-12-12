f1630858baffb0545d440326331d1ec2debb555d
George Raduta <george.raduta@cern.ch>
2024-12-12 16:04:28 +0100

    [OGUI-1590] Add support for final ECS operation (error/timeout) in DCS SOR pane (#2697)
    * Add support for ECS operation done in SOR
    * Fix lint issues

---
+++ Control/lib/common/ecsOperationAndStepStatus.enum.js
@@ -0,0 +1,27 @@
+/**
+ * @license
+ * Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+ * See http://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+ * All rights not expressly granted are reserved.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+*/
+
+/**
+ * Available ECS Statuses of operations for Kafka Events
+ * These operations can be under the label:
+ * * operationStatus
+ * * operationStepStatus
+ */
+const EcsOperationAndStepStatus = Object.freeze({
+  DONE_OK: 'DONE_OK',
+  DONE_ERROR: 'DONE_ERROR',
+  DONE_TIMEOUT: 'DONE_TIMEOUT',
+});
+
+exports.EcsOperationAndStepStatus = EcsOperationAndStepStatus;
