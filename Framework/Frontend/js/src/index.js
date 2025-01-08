fb7626581e38ff524ff79d73d1dda09ce339b1dc
martinboulais <31805063+martinboulais@users.noreply.github.com>
2025-01-08 15:39:06 +0100

    [OGUI-1497] Add dropdown component

--- Framework/Frontend/js/src/index.js
+++ Framework/Frontend/js/src/index.js
@@ -23,6 +23,7 @@ export { default as QueryRouter } from './QueryRouter.js';
 
 // Utils
 export { default as switchCase } from './switchCase.js';
+export { documentClickTaggedEventRegistry } from './utilities/documentClickTaggedEventRegistry.js';
 
 // Formatters
 export { formatTimeDuration } from './formatter/formatTimeDuration.js';
@@ -39,6 +40,12 @@ export { default as BrowserStorage } from './BrowserStorage.js';
 // Reusable components
 export { StatefulComponent } from './components/StatefulComponent.js';
 export { CopyToClipboardComponent } from './components/CopyToClipboardComponent.js';
+export { createPortal } from './components/createPortal.js';
+export { DropdownComponent } from './components/DropdownComponent.js';
+export { getUniqueId } from './components/getUniqueId.js';
+export { popover } from './components/popover.js';
+export { PopoverAnchors } from './components/PopoverEngine.js';
+export { PopoverTriggerPreConfiguration } from './components/PopoverPreConfigurations.js';
 
 // All icons helpers, namespaced with prefix 'icon*'
 export * from './icons.js';
