54eb324f370a6c6acca8fb5a6bb6dcfa03a8ce74
Alejandro Mariscal Romero <87366244+mariscalromeroalejandro@users.noreply.github.com>
2025-01-21 16:27:12 +0100

    add tests to test-index.js

--- QualityControl/test/test-index.js
+++ QualityControl/test/test-index.js
@@ -56,6 +56,8 @@ import { statusServiceTestSuite } from './lib/services/StatusService.test.js';
 
 import { commonLibraryQcObjectUtilsTestSuite } from './common/library/qcObject/utils.test.js';
 import { commonLibraryUtilsDateTimeTestSuite } from './common/library/utils/dateTimeFormat.test.js';
+import { layoutIdMiddlewareTest } from './lib/middlewares/layouts/layoutId.middleware.test.js';
+import { layoutOwnerMiddlewareTest } from './lib/middlewares/layouts/layoutOwner.middleware.test.js';
 
 const FRONT_END_PER_TEST_TIMEOUT = 5000; // each front-end test is allowed this timeout
 // remaining tests are based on the number of individual tests in each suite
@@ -156,6 +158,11 @@ suite('All Tests - QCG', { timeout: FRONT_END_TIMEOUT + BACK_END_TIMEOUT }, asyn
       suite('UserServiceTest test suite', async () => {
         // TODO - bring inline with current tests
       });
+    });
+
+    suite('Middleware - Test Suite', async () => {
+      suite('LayoutIdMiddleware test suite', async () => layoutIdMiddlewareTest());
+      suite('LayoutOwnerMiddleware test suite', async () => layoutOwnerMiddlewareTest());
     });
 
     suite('Controllers - Test Suite', async () => {
