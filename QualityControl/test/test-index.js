f9c0ba84653e696c769fd336dead8da9987ea0d8
Alejandro Mariscal Romero <87366244+mariscalromeroalejandro@users.noreply.github.com>
2025-02-07 15:32:16 +0100

    add test for middleware

--- QualityControl/test/test-index.js
+++ QualityControl/test/test-index.js
@@ -59,6 +59,7 @@ import { commonLibraryUtilsDateTimeTestSuite } from './common/library/utils/date
 import { layoutIdMiddlewareTest } from './lib/middlewares/layouts/layoutId.middleware.test.js';
 import { layoutOwnerMiddlewareTest } from './lib/middlewares/layouts/layoutOwner.middleware.test.js';
 import { layoutServiceMiddlewareTest } from './lib/middlewares/layouts/layoutService.middleware.test.js';
+import { statusComponentMiddlewareTest } from './lib/middlewares/status/statusComponent.middleware.test.js';
 
 const FRONT_END_PER_TEST_TIMEOUT = 5000; // each front-end test is allowed this timeout
 // remaining tests are based on the number of individual tests in each suite
@@ -82,7 +83,7 @@ const FRONT_END_TIMEOUT = INITIAL_PAGE_SETUP_TIMEOUT
 const BACK_END_TIMEOUT = 10000; // back-end test suite timeout
 
 suite('All Tests - QCG', { timeout: FRONT_END_TIMEOUT + BACK_END_TIMEOUT }, async () => {
-  suite('Front-end test suite', { timeout: FRONT_END_TIMEOUT }, async () => {
+  suite.skip('Front-end test suite', { timeout: FRONT_END_TIMEOUT }, async () => {
     let url = undefined;
     let page = undefined;
     let browser = undefined;
@@ -165,6 +166,7 @@ suite('All Tests - QCG', { timeout: FRONT_END_TIMEOUT + BACK_END_TIMEOUT }, asyn
       suite('LayoutServiceMiddleware test suite', async () => layoutServiceMiddlewareTest());
       suite('LayoutIdMiddleware test suite', async () => layoutIdMiddlewareTest());
       suite('LayoutOwnerMiddleware test suite', async () => layoutOwnerMiddlewareTest());
+      suite('StatusComponentMiddleware test suite', async () => statusComponentMiddlewareTest());
     });
 
     suite('Controllers - Test Suite', async () => {
