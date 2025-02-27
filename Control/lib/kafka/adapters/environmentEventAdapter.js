c70c705e39577fcde050ba676417fa6010fd1cd9
George Raduta <george.raduta@cern.ch>
2025-02-27 11:57:31 +0100

    Update environmentEventAdapter.js

--- Control/lib/kafka/adapters/environmentEventAdapter.js
+++ Control/lib/kafka/adapters/environmentEventAdapter.js
@@ -14,10 +14,11 @@
 /**
  * Adapter for event messages received on environment topic
  * @param {Event.proto} eventMessage - the event message to adapt
+ * @param {int64.proto} eventMessage.timestamp - the timestamp of the event
+ * @param {Ev_EnvironmentEvent.proto} eventMessage.environmentEvent - the environment event to adapt
  * @return {EnvironmentEvent} - the adapted event message without the timestampNano field
  */
-exports.environmentEventAdapter = (eventMessage) => {
-  const { timestamp, environmentEvent } = eventMessage;
+exports.environmentEventAdapter = ({ timestamp, environmentEvent }) => {
   const {
     environmentId: id,
     state, runNumber, error, message, transition, transitionStep, transitionStatus, vars, lastRequestUser
