1a51a27160dba2acfe886596310de0c3e3060e0c
Alejandro Mariscal Romero <87366244+mariscalromeroalejandro@users.noreply.github.com>
2025-03-17 11:13:39 +0100

    remove dotenv dependencies

--- QualityControl/lib/database/config/config.js
+++ QualityControl/lib/database/config/config.js
@@ -10,8 +10,6 @@
  * granted to it by virtue of its status as an Intergovernmental Organization
  * or submit itself to any jurisdiction.
  */
-import dotenv from 'dotenv';
-dotenv.config();
 
 const host = process.env?.DATABASE_HOST || 'qcg-database';
 const port = process.env?.DATABASE_PORT || 3306;
