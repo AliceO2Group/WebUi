1cf72f3d3a8af7626ec15da4958fdc96fba17df5
George Raduta <george.raduta@cern.ch>
2025-03-25 15:52:56 +0100

    Use new middleware

--- Control/lib/api.js
+++ Control/lib/api.js
@@ -21,7 +21,8 @@ const config = require('./config/configProvider.js');
 const {minimumRoleMiddleware} = require('./middleware/minimumRole.middleware.js');
 const {addDetectorIdMiddleware} = require('./middleware/addDetectorId.middleware.js');
 const {DetectorId} = require('./common/detectorId.enum.js');
-const {verifyEnvironmentLockOwnershipMiddleware} = require('./middleware/verifyEnvironmentLockOwnership.middleware.js');
+const { setDetectorsFromEnvironmentMiddlewareFactory } = require('./middleware/setDetectorsFromEnvironmentMiddlewareFactory.js');
+const { getDetectorsLockOwnershipMiddlewareFactory } = require('./middleware/getDetectorsLockOwnershipMiddlewareFactory.js');
 
 // controllers
 const {ConsulController} = require('./controllers/Consul.controller.js');
@@ -132,7 +133,6 @@ module.exports.setup = (http, ws) => {
     } catch (error) {
       logger.errorMessage(`Kafka initialization failed: ${error.message}`);
     }
-  
   }
 
   const statusService = new StatusService(
@@ -148,6 +148,8 @@ module.exports.setup = (http, ws) => {
   const coreMiddleware = [
     ctrlService.isConnectionReady.bind(ctrlService),
   ];
+  const setDetectorsFromEnvironmentMiddleware = setDetectorsFromEnvironmentMiddlewareFactory(envService);
+  const verifyLockOwnershipMiddleware = getDetectorsLockOwnershipMiddlewareFactory(lockService);
 
   ctrlProxy.methods.forEach(
     (method) => http.post(`/${method}`, coreMiddleware, (req, res) => ctrlService.executeCommand(req, res)),
@@ -172,7 +174,8 @@ module.exports.setup = (http, ws) => {
   http.delete('/environment/:id',
     coreMiddleware,
     minimumRoleMiddleware(Role.DETECTOR),
-    verifyEnvironmentLockOwnershipMiddleware(lockService, envService),
+    setDetectorsFromEnvironmentMiddleware,
+    verifyLockOwnershipMiddleware,
     envCtrl.destroyEnvironmentHandler.bind(envCtrl),
   );
 
