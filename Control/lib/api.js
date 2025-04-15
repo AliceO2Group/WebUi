3f2ba145324d4de0b6e819b50c46d2a59c5e2a09
George Raduta <george.raduta@cern.ch>
2025-04-15 17:43:04 +0200

    Remove unused import

--- Control/lib/api.js
+++ Control/lib/api.js
@@ -27,9 +27,6 @@ const {
 const {
   getDetectorsLockOwnershipMiddlewareFactory
 } = require('./middleware/getDetectorsLockOwnershipMiddlewareFactory.js');
-const {
-  verifyDetectorsAvailabilityMiddlewareFactory
-} = require('./middleware/verifyDetectorsAvailabilityMiddlewareFactory.middleware.js');
 
 // controllers
 const {ConsulController} = require('./controllers/Consul.controller.js');
