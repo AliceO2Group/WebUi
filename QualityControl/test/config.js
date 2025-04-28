d42155fbca32ee237de29a8b1d0e635a6e215686
mariscalromeroalejandro <mariscalromeroalejandro@gmail.com>
2025-04-28 14:45:20 +0200

    add config for test environment

--- QualityControl/test/config.js
+++ QualityControl/test/config.js
@@ -39,4 +39,16 @@ export const config = {
     issuer: 'test-gui',
     expiration: '1d',
   },
+  database: {
+    host: 'database',
+    port: 3306,
+    username: 'cern',
+    password: 'cern',
+    database: 'qcg',
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_general_ci',
+    timezone: '+00:00',
+    logging: false,
+    retryThrottle: 5000,
+  },
 };
