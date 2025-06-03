49e96f5c1a89210a171e5d8cff527474802f643b
NarrowsProjects <metzguust@gmail.com>
2025-06-03 12:42:16 +0200

    refactor: remove unneeded else from code

--- QualityControl/lib/services/QcObject.service.js
+++ QualityControl/lib/services/QcObject.service.js
@@ -104,12 +104,12 @@ export class QcObjectService {
   ) {
     if (!filters && useCache && this._cache.objects?.length) {
       return this._cache.objects.filter((object) => object.name.startsWith(prefix));
-    } else {
-      const objects = filters === undefined
-        ? await this._dbService.getObjectsTreeList(prefix) // TreeList links to the latest too, but is more efficient.
-        : await this._dbService.getObjectsLatestVersionList(prefix, filters, fields);
-      return this._parseObjects(objects);
     }
+    const objects = filters === undefined
+      ? await this._dbService.getObjectsTreeList(prefix) // TreeList links to the latest too, but is more efficient.
+      : await this._dbService.getObjectsLatestVersionList(prefix, filters, fields);
+
+    return this._parseObjects(objects);
   }
 
   /**
