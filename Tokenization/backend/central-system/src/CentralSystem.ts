e92100ad502cdd201fb765a09238aceea76eb113
dwedr <d.wedrocha@gmail.com>
2025-06-11 13:23:08 +0200

    Added descriptions and changed the wrapper name

--- Tokenization/backend/central-system/src/CentralSystem.ts
+++ Tokenization/backend/central-system/src/CentralSystem.ts
@@ -1,20 +1,49 @@
 import type { Request, Response } from "express";
 
+/*
+ * CentralSystem class to handle token management.
+ * It includes methods to get tokens, create a new token, revoke tokens and provide tokens to relecant clients.
+ * The class uses a static Map to simulate a database of tokens.
+ * The tokens are stored with a tokenId, validity status, and payload.
+ */
 class CentralSystem {
   private static fakeTokens = new Map([
     [1, { tokenId: 1, validity: "good", payload: "payload1" }],
     [2, { tokenId: 2, validity: "bad", payload: "payload2" }],
   ]);
 
+  /**
+   * @description Retrieves all tokens from the Central System. Returns a serialized binary payload of the tokens.
+   * @param req - The request object.
+   * @param res - The response object.
+   * @return {Promise<void>}
+   */
   public static async getTokens(req: Request, res: Response): Promise<void> {}
 
+  /**
+   * @description Creates a new token with the provided payload.
+   * @param req - The request object containing the payload.
+   * @param res - The response object.
+   * @return {Promise<void>}
+   */
   public static async createToken(req: Request, res: Response): Promise<void> {}
-
+  /**
+   * @description Provides a token to a client based on the client ID.
+   * @param clientID - The ID of the client to whom the token is provided.
+   * @param token - The token to be provided.
+   * @return {Promise<void>}
+   */
   public static async provideTokeToClient(
     clientID: number,
     token: string
   ): Promise<void> {}
 
+  /**
+   * @description Revokes a token for a specific client.
+   * @param client - The client ID for which the token is revoked.
+   * @param tokenEncypted - The encrypted token to be revoked.
+   * @return {Promise<void>}
+   */
   public static async revokeToken(
     client: string,
     tokenEncypted: string
