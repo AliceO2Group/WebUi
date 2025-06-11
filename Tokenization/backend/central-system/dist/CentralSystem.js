f6b018927df2d89d5d628db8f645e63c2248eca0
dwedr <d.wedrocha@gmail.com>
2025-06-11 13:23:08 +0200

    Added descriptions and changed the wrapper name

--- Tokenization/backend/central-system/dist/CentralSystem.js
+++ Tokenization/backend/central-system/dist/CentralSystem.js
@@ -1,7 +1,37 @@
+/*
+ * CentralSystem class to handle token management.
+ * It includes methods to get tokens, create a new token, revoke tokens and provide tokens to relecant clients.
+ * The class uses a static Map to simulate a database of tokens.
+ * The tokens are stored with a tokenId, validity status, and payload.
+ */
 class CentralSystem {
+    /**
+     * @description Retrieves all tokens from the Central System. Returns a serialized binary payload of the tokens.
+     * @param req - The request object.
+     * @param res - The response object.
+     * @return {Promise<void>}
+     */
     static async getTokens(req, res) { }
+    /**
+     * @description Creates a new token with the provided payload.
+     * @param req - The request object containing the payload.
+     * @param res - The response object.
+     * @return {Promise<void>}
+     */
     static async createToken(req, res) { }
+    /**
+     * @description Provides a token to a client based on the client ID.
+     * @param clientID - The ID of the client to whom the token is provided.
+     * @param token - The token to be provided.
+     * @return {Promise<void>}
+     */
     static async provideTokeToClient(clientID, token) { }
+    /**
+     * @description Revokes a token for a specific client.
+     * @param client - The client ID for which the token is revoked.
+     * @param tokenEncypted - The encrypted token to be revoked.
+     * @return {Promise<void>}
+     */
     static async revokeToken(client, tokenEncypted) { }
 }
 CentralSystem.fakeTokens = new Map([
