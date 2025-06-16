fc4354003176622086f6a8035b284c348ad2b0e8
NarrowsProjects <metzguust@gmail.com>
2025-06-16 12:10:31 +0200

    refactor: remove unused logger

--- QualityControl/lib/controllers/ObjectController.js
+++ QualityControl/lib/controllers/ObjectController.js
@@ -31,7 +31,6 @@ export class ObjectController {
      * @type {QCObjectService}
      */
     this._objService = objService;
-    this._logger = LogManager.getLogger('object/controller');
   }
 
   /**
