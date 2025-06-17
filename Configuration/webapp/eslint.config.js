de4ddd1d8d2c79e4166b9a648c88086dd045c368
Deaponn <bartek.sajo@interia.pl>
2025-06-17 23:02:56 +0200

    build: configure ESLint and Prettier

---
+++ Configuration/webapp/eslint.config.js
@@ -0,0 +1,279 @@
+/**
+ * @license
+ * Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+ * See http://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+ * All rights not expressly granted are reserved.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+*/
+
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import jsdoc from 'eslint-plugin-jsdoc';
+import stylistic from '@stylistic/eslint-plugin';
+import react from 'eslint-plugin-react';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  jsdoc.configs['flat/recommended'],
+  pluginJs.configs.recommended,
+  react.configs.flat.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    ignores: ['./*.config.js', 'eslint_rules/*', 'custom_eslint_rules/*']
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.cts", "**.*.mts"],
+    ignores: ['config-default.js', 'config.js', 'jsconfig.json'],
+    plugins: {
+      jsdoc,
+      '@stylistic': stylistic,
+    },
+    languageOptions: {
+      parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        process: 'readonly',
+        Model: 'readonly',
+        vnode: 'readonly',
+        Express: 'readonly',
+        setImmediate: 'readonly',
+      },
+    },
+    rules: {
+      "@typescript-eslint/only-throw-error": "off",
+      'react/react-in-jsx-scope': 'off',
+      'arrow-body-style': ['error', 'as-needed'],
+      curly: 'error',
+      indent: [
+        'error',
+        2,
+        {
+          SwitchCase: 1,
+        },
+      ],
+      'init-declarations': ['error', 'always'],
+      'no-console': 'error',
+      'no-implicit-coercion': 'error',
+      'no-return-assign': 'error',
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^(_|((request|response|next)$))',
+        },
+      ],
+      'no-var': 'error',
+      'one-var': [
+        'error',
+        'never',
+      ],
+      'prefer-arrow-callback': [
+        'error',
+        {
+          allowUnboundThis: true,
+        },
+      ],
+      'prefer-const': 'error',
+      'prefer-destructuring': 'error',
+      'prefer-object-spread': 'error',
+      'prefer-template': 'error',
+      radix: 'error',
+      'jsdoc/require-description': ['error'],
+      'jsdoc/check-values': ['off'],
+      'jsdoc/no-multi-asterisks': ['off'],
+      'jsdoc/no-undefined-types': ['off'],
+      'jsdoc/check-tag-names': ['warn', { definedTags: ['rejects'] }],
+      'jsdoc/no-defaults': ['off'],
+      '@stylistic/array-bracket-newline': [
+        'error',
+        {
+          multiline: true,
+        },
+      ],
+      '@stylistic/array-bracket-spacing': [
+        'error',
+        'never',
+        {
+          singleValue: false,
+        },
+      ],
+      '@stylistic/array-element-newline': [
+        'error',
+        'consistent',
+      ],
+      '@stylistic/arrow-parens': [
+        'error',
+        'always',
+      ],
+      '@stylistic/brace-style': [
+        'error',
+        '1tbs',
+        {
+          allowSingleLine: false,
+        },
+      ],
+      '@stylistic/comma-dangle': [
+        'error',
+        'always-multiline',
+      ],
+      '@stylistic/comma-spacing': [
+        'error',
+        {
+          before: false,
+          after: true,
+        },
+      ],
+      '@stylistic/comma-style': [
+        'error',
+        'last',
+      ],
+      '@stylistic/computed-property-spacing': 'error',
+      '@stylistic/dot-location': [
+        'error',
+        'property',
+      ],
+      '@stylistic/eol-last': [
+        'error',
+        'always',
+      ],
+      '@stylistic/function-call-argument-newline': [
+        'error',
+        'consistent',
+      ],
+      '@stylistic/function-paren-newline': [
+        'error',
+        'multiline',
+      ],
+      '@stylistic/indent': [
+        'error',
+        2,
+        {
+          SwitchCase: 1,
+        },
+      ],
+      '@stylistic/key-spacing': 'error',
+      '@stylistic/keyword-spacing': 'error',
+      '@stylistic/linebreak-style': 'off',
+      '@stylistic/lines-around-comment': [
+        'error',
+        {
+          allowBlockStart: true,
+          allowClassStart: true,
+          beforeBlockComment: true,
+        },
+      ],
+      '@stylistic/lines-between-class-members': [
+        'error',
+        'always',
+      ],
+      '@stylistic/max-len': [
+        'error',
+        {
+          code: 120,
+        },
+      ],
+      '@stylistic/no-multi-spaces': 'error',
+      '@stylistic/no-multiple-empty-lines': [
+        'error',
+        {
+          max: 1,
+          maxBOF: 0,
+          maxEOF: 0,
+        },
+      ],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/object-curly-spacing': [
+        'error',
+        'always',
+      ],
+      '@stylistic/object-property-newline': [
+        'error',
+        {
+          allowAllPropertiesOnSameLine: true,
+        },
+      ],
+      '@stylistic/padded-blocks': [
+        'error',
+        'never',
+      ],
+      '@stylistic/padding-line-between-statements': [
+        'error',
+        {
+          blankLine: 'always',
+          prev: 'cjs-import',
+          next: '*',
+        },
+        {
+          blankLine: 'any',
+          prev: 'cjs-import',
+          next: 'cjs-import',
+        },
+        {
+          blankLine: 'always',
+          prev: '*',
+          next: 'cjs-export',
+        },
+      ],
+      '@stylistic/quote-props': [
+        'error',
+        'as-needed',
+      ],
+      '@stylistic/quotes': [
+        'error',
+        'single',
+        {
+          avoidEscape: true,
+        },
+      ],
+      '@stylistic/semi': 'error',
+      '@stylistic/semi-style': [
+        'error',
+        'last',
+      ],
+      '@stylistic/space-before-blocks': [
+        'error',
+        {
+          functions: 'always',
+          keywords: 'always',
+          classes: 'always',
+        },
+      ],
+      '@stylistic/space-before-function-paren': [
+        'error',
+        {
+          anonymous: 'always',
+          named: 'never',
+          asyncArrow: 'always',
+        },
+      ],
+      '@stylistic/space-infix-ops': 'error',
+      '@stylistic/space-in-parens': [
+        'error',
+        'never',
+      ],
+      '@stylistic/template-curly-spacing': [
+        'error',
+        'never',
+      ],
+      'no-magic-numbers': 'off', // TODO: enable
+    },
+  },
+);
---
+++ Configuration/webapp/prettier.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+const config = {
+  printWidth: 100,
+  trailingComma: "es5",
+  semi: true,
+  singleQuote: true,
+};
+
+export default config;
