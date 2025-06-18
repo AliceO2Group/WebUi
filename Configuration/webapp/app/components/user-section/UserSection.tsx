c2664e30776806ddb55212bd0e039a5ac14f0efc
Krzysztof Śliwiński <krzysztof.sliwinski@swmansion.com>
2025-06-18 21:31:19 +0200

    chore: add license sections

--- Configuration/webapp/app/components/user-section/UserSection.tsx
+++ Configuration/webapp/app/components/user-section/UserSection.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+/**
+ * @license
+ * Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+ * See http://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+ * All rights not expressly granted are reserved.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+*/
+
 import { Box, IconButton, Menu, MenuItem, Avatar } from '@mui/material';
+import { useState, type FC, type MouseEvent } from 'react';
 
 interface UserSectionProps {
   userName: string;
 }
 
-const UserSection: React.FC<UserSectionProps> = ({ userName }) => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+const UserSection: FC<UserSectionProps> = ({ userName }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+  const handleClick = (event: MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
 
