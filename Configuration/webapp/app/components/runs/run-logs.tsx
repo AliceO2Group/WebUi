adf4fec8a486bc4f83d1bb8dae4a218f8df19d34
Deaponn <bartek.sajo@interia.pl>
2025-06-18 13:10:23 +0200

    chore: apply Prettier formatting rules and fix ESLint warning and errors

--- Configuration/webapp/app/components/runs/run-logs.tsx
+++ Configuration/webapp/app/components/runs/run-logs.tsx
@@ -1,25 +1,47 @@
-import type {Log} from "~/components/logs/log";
+/**
+ * @license
+ * Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+ * See http://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+ * All rights not expressly granted are reserved.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+import type { Log } from '~/components/logs/log';
 
 interface RunLogsProps {
-    logs: Log[];
+  logs: Log[];
 }
 
-export const RunLogs = ({logs}: RunLogsProps) => {
-    if (logs.length === 0) {
-        return <p><em>No logs linked to this run</em></p>
-    }
-    return <table className={'table'}>
-        <thead>
+export const RunLogs = ({ logs }: RunLogsProps) => {
+  if (logs.length === 0) {
+    return (
+      <p>
+        <em>No logs linked to this run</em>
+      </p>
+    );
+  }
+  return (
+    <table className={'table'}>
+      <thead>
         <tr>
-            <th>Title</th>
-            <th>Content</th>
+          <th>Title</th>
+          <th>Content</th>
         </tr>
-        </thead>
-        <tbody>
-        {logs.map(({id, title, content}) => <tr key={id}>
+      </thead>
+      <tbody>
+        {logs.map(({ id, title, content }) => (
+          <tr key={id}>
             <td>{title}</td>
             <td>{content}</td>
-        </tr>)}
-        </tbody>
+          </tr>
+        ))}
+      </tbody>
     </table>
-}
+  );
+};
