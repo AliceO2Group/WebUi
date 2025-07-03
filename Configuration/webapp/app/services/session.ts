f79ba108d770d9822bdb23f160f5c4699de821f4
Deaponn <bartek.sajo@interia.pl>
2025-07-03 11:57:42 +0200

    feature: move proxy from /api to /control

--- Configuration/webapp/app/services/session.ts
+++ Configuration/webapp/app/services/session.ts
@@ -18,7 +18,7 @@ export const fetchSessionData = async () => {
   // only to get the data from server redirect
   // (line 264, commit 3ba4600 of github.com/AliceO2Group/WebUi/blob/dev/Framework/Backend/http/server.js)
   // this should be replaced with endpoint designed for authentication only
-  const response = await fetch('http://localhost:8080/api/');
+  const response = await fetch('http://localhost:8080/control');
   const { searchParams } = new URL(response.url);
   searchParams.forEach((value, key) => {
     sessionStorage.setItem(key, value);
