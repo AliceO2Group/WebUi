cda26b2d43a03fdf6e04476d4c0c0a7a0b78de02
George Raduta <george.raduta@cern.ch>
2025-08-12 17:31:24 +0200

    Add middleware for logging deployment request

---
+++ Control/lib/middleware/logDeploymentRequest.middleware.js
@@ -0,0 +1,45 @@
+/**
+ * @license
+ * Copyright 2019-2024 CERN and copyright holders of ALICE O2.
+ * See http://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+ * All rights not expressly granted are reserved.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+*/
+
+const {LogManager, LogLevel} = require('@aliceo2/web-ui');
+
+/**
+ * Middleware to log deployment requests with user and request context.
+ * @param {Express.Request} req - Request object
+ * @param {Express.Response} _ - Response object
+ * @param {Express.Next} next - Next middleware function
+ * @return {void}
+ */
+const logDeploymentRequestMiddleware = (req, _, next) => {
+  /**
+   * @type {DeploymentRequest}
+   */
+  const { selectedConfiguration, workflowTemplate, detectors = [] } = req.body;
+  const { username } = req.session;
+
+  const detectorsString = detectors.length > 0 ? `detectors: ${detectors.join(', ')}` : 'no detectors specified';
+  const logMessage = `Deployment request from user: ${ username }`
+    + (workflowTemplate ? ` for workflow: ${workflowTemplate}` : '')
+    + (selectedConfiguration ? ` with configuration: ${selectedConfiguration}` : '')
+    + ` with detectors: ${detectorsString}`;
+
+  LogManager
+    .getLogger(`${process.env.npm_config_log_label ?? 'cog'}/deployment-request`)
+    .infoMessage(logMessage, {
+      level: LogLevel.OPERATIONS,
+    });
+  next();
+}
+
+module.exports = { logDeploymentRequestMiddleware };
