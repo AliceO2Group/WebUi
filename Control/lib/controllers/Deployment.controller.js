b0a9cd82587de737a80bbacacfaf549b34f25c0f
George Raduta <george.raduta@cern.ch>
2025-08-12 17:36:14 +0200

    Add deployment controller for migration to newEnvAsync

---
+++ Control/lib/controllers/Deployment.controller.js
@@ -0,0 +1,87 @@
+/**
+ * @license
+ * Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+ * See http://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+ * All rights not expressly granted are reserved.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+*/
+const {
+  LogManager,
+  LogLevel,
+  updateAndSendExpressResponseFromNativeError,
+  InvalidInputError
+} = require('@aliceo2/web-ui');
+
+const {User} = require('./../dtos/User.js');
+
+/**
+ * Controller Class for managing deployments via the AliECS system
+ */
+class DeploymentController {
+
+  /**
+   * Constructor for initializing controller with a deployment service
+   * @param {DeploymentService} deploymentService - service to use to request AliECS a new deployment
+   */
+  constructor(deploymentService) {
+    this._logger = LogManager.getLogger(`${process.env.npm_config_log_label ?? 'cog'}/deployment-ctrl`);
+
+    /**
+     * @type {DeploymentService}
+     */
+    this._deploymentService = deploymentService;
+  }
+
+  /**
+   * Handles the request to make a deployment by:
+   * - validating the user built request
+   * - preparing the request payload for ECS
+   * - calling the ECS service to deploy the environment
+   *
+   * User must be authenticated and authorized to perform this action and this is verified via middlewares
+   * 
+   * The result of a deployment is an environment
+   * 
+   * @param {Express.Request} req - the request object
+   * @param {Express.Response} res - the response object
+   * @returns {Promise<void>}
+   */
+  async newAsyncDeploymentHandler(req, res) {
+    /**
+     * @type {DeploymentRequest}
+     */
+    const { workflowTemplate, selectedConfiguration, userVars } = req.body;
+
+    if (!workflowTemplate && !selectedConfiguration) {
+      updateAndSendExpressResponseFromNativeError(
+        res,
+        new InvalidInputError('Invalid input: workflowTemplate or selectedConfiguration must be provided')
+      )
+      return;
+    }
+
+    const { personid, name, username } = req.session || {};
+    const user = new User(username, name, personid);
+
+    try {
+      const environment = await this._deploymentService.deployEnvironment({
+        userVars,
+        selectedConfiguration,
+        workflowTemplate,
+        user,
+      });
+      res.status(201).json(environment);
+    } catch (error) {
+      this._logger.errorMessage(error, { level: LogLevel.SUPPORT });
+      updateAndSendExpressResponseFromNativeError(res, error);
+    }
+  }
+}
+
+module.exports = { DeploymentController };
---
+++ Control/test/lib/controllers/mocha-deployment.controller.js
@@ -0,0 +1,85 @@
+/**
+ * @license
+ * Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+ * See http://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+ * All rights not expressly granted are reserved.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+*/
+
+const assert = require('assert');
+const sinon = require('sinon');
+const { User } = require('./../../../lib/dtos/User.js');
+const { DeploymentController } = require('./../../../lib/controllers/Deployment.controller.js');
+
+describe('DeploymentController test suite', function() {
+  let deploymentController, req, res;
+  let mockDeploymentService;
+
+  beforeEach(function () {
+    mockDeploymentService = { deployEnvironment: sinon.stub() };
+    deploymentController = new DeploymentController(mockDeploymentService);
+    req = {
+      body: {},
+      session: { username: 'testuser', name: 'Test User', personid: '123' }
+    };
+    res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub()
+    };
+  });
+
+  it('should return 400 if both workflowTemplate and selectedConfiguration arenp missing', async function() {
+    req.body = { workflowTemplate: null, selectedConfiguration: null };
+    await deploymentController.newAsyncDeploymentHandler(req, res);
+    assert.ok(res.status.calledWith(400));
+    assert.ok(res.json.calledWith({
+      message: 'Invalid input: workflowTemplate or selectedConfiguration must be provided',
+      status: 400,
+      title: 'Invalid Input'
+    }));
+  });
+
+  it('should call deployEnvironment with correct parameters', async function() {
+    req.body = {
+      workflowTemplate: 'readout-dataflow',
+      userVars: { var1: 'value1' }
+    };
+    mockDeploymentService.deployEnvironment.returns({ id: 'env123' });
+
+    await deploymentController.newAsyncDeploymentHandler(req, res);
+
+    assert.ok(mockDeploymentService.deployEnvironment.calledOnce);
+    assert.deepStrictEqual(mockDeploymentService.deployEnvironment.firstCall.args[0], {
+      workflowTemplate: 'readout-dataflow',
+      selectedConfiguration: undefined,
+      userVars: { var1: 'value1' },
+      user: new User(req.session.username, req.session.name, req.session.personid)
+    });
+
+    assert.ok(res.status.calledWith(201));
+    assert.ok(res.json.calledWith({ id: 'env123' }));
+  });
+
+  it('should handle errors from deployEnvironment', async function () {
+    const error = new Error('Deployment failed');
+    mockDeploymentService.deployEnvironment.throws(error);
+    req.body = {
+      workflowTemplate: 'readout-dataflow',
+      userVars: { var1: 'value1' }
+    };
+    await deploymentController.newAsyncDeploymentHandler(req, res);
+
+    assert.ok(res.status.calledWith(500));
+    assert.ok(res.json.calledWith({
+      message: 'Deployment failed',
+      status: 500,
+      title: 'Unknown Error'
+    }));
+  });
+});
