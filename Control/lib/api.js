2423adfa2d8b58875094f9fe00ece0fe94b70067
George Raduta <george.raduta@cern.ch>
2025-08-12 17:56:14 +0200

    Remove old and deprecated environment deployment

--- Control/lib/api.js
+++ Control/lib/api.js
@@ -174,7 +174,7 @@ module.exports.setup = (http, ws) => {
   ctrlProxy.methods.forEach(
     (method) => http.post(`/${method}`, coreMiddleware, (req, res) => ctrlService.executeCommand(req, res)),
   );
-  http.post('/core/request', coreMiddleware, (req, res) => aliecsReqHandler.add(req, res));
+
   http.get('/core/requests', coreMiddleware, (req, res) => aliecsReqHandler.getAll(req, res));
   http.post('/core/removeRequest/:id', coreMiddleware, (req, res) => aliecsReqHandler.remove(req, res));
 
