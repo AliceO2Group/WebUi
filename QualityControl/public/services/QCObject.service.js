80fd39da8e5ab4f7b93019e30eab0dba8ff16e25
Alejandro Mariscal Romero <87366244+mariscalromeroalejandro@users.noreply.github.com>
2025-08-29 12:44:03 +0200

    api endpoints usage

--- QualityControl/public/services/QCObject.service.js
+++ QualityControl/public/services/QCObject.service.js
@@ -168,13 +168,20 @@ export default class QCObjectService {
 
   /**
    * Ask server for all available objects from CCDB
+   * @param {boolean} inRunMode - if true, inRunMode is added to the path
    * @returns {JSON} List of Objects
    * @deprecated
    */
-  async getObjects() {
+  async getObjects(inRunMode = false) {
     const hasFilters = Object.values(this.filterModel.filterMap).some(Boolean);
     const fields = hasFilters ? ['path'] : undefined; // If there are filters more unneeded fields are sent down.
-    const url = this._buildURL('/api/objects?', undefined, undefined, undefined, fields);
+    const url = this._buildURL(
+      `/api/objects?${inRunMode ? 'inRunMode=true' : ''}`,
+      undefined,
+      undefined,
+      undefined,
+      fields,
+    );
     const { result, ok } = await this.model.loader.get(url);
     return ok ? RemoteData.success(result) : RemoteData.failure(result);
   }
