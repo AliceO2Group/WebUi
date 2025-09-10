d5179f2acd214193bbd8d8e71b38e9f207648615
George Raduta <george.raduta@cern.ch>
2025-09-10 17:04:55 +0200

    [qcg] Refactor/remove unused refresh timer (#3053)
    * removes dead code from the old Online Mode of QCG, this has been replaced by the RunMode

--- QualityControl/public/Model.js
+++ QualityControl/public/Model.js
@@ -66,7 +66,6 @@ export default class Model extends Observable {
     this.aboutViewModel = new AboutViewModel(this);
     this.aboutViewModel.bubbleTo(this);
 
-    this.refreshTimer = 0;
     this.refreshInterval = 0; // Seconds
     this.sidebar = true;
     this.accountMenuEnabled = false;
@@ -314,12 +313,6 @@ export default class Model extends Observable {
 
     // Clear filter model runs mode interval
     this.filterModel.clearRunsModeInterval();
-
-    // Clear main refresh timer if it exists
-    if (this._refreshTimer) {
-      clearInterval(this._refreshTimer);
-      this._refreshTimer = null;
-    }
   }
 
   /**
