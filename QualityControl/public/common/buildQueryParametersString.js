7ba9602ed689b9ddec68cefe172bc50f564a1014
George Raduta <george.raduta@cern.ch>
2025-09-18 17:39:21 +0200

    Expand util function for building query param

--- QualityControl/public/common/buildQueryParametersString.js
+++ QualityControl/public/common/buildQueryParametersString.js
@@ -14,6 +14,10 @@
 
 /**
  * Given an existing object of used parameters and new ones to add/update, return a string with desired URL format
+ * @example
+ * { page: 'test', filter: { name: 'mylayout', objectPathPrefix: 'test' } }
+ * results in:
+ * ?page=test&filter[name]=mylayout&filter[objectPathPrefix]=test
  * @param {object} currentParameters - current parameters in the URL
  * @param {object} parameters - object containing what parameters should be added or updated
  * @returns {string} - updated URL
@@ -22,8 +26,25 @@ export function buildQueryParametersString(currentParameters, parameters) {
   Object.assign(currentParameters, parameters);
   let url = '?';
 
-  Object.keys(currentParameters).forEach((param) => {
-    url += `${param}=${currentParameters[param]}&`;
-  });
-  return url.substring(0, url.length - 1);
+  /**
+   * Encodes a parameter for URL query string, supporting nested objects.
+   * @param {string} key - The parameter key
+   * @param {string|object} value - The parameter value, string or nested object
+   * @returns {string} Encoded parameter string
+   */
+  function encodeParam(key, value) {
+    if (value && typeof value === 'object' && !Array.isArray(value)) {
+      return Object.keys(value)
+        .map((subKey) => `${key}[${subKey}]=${encodeURIComponent(value[subKey])}`)
+        .join('&');
+    } else {
+      return `${key}=${encodeURIComponent(value)}`;
+    }
+  }
+
+  url += Object.keys(currentParameters)
+    .map((param) => encodeParam(param, currentParameters[param]))
+    .filter(Boolean)
+    .join('&');
+  return url;
 }
