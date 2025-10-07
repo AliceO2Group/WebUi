baa4c3d015d13499bd3e65a9f541873368de9feb
Alejandro Mariscal Romero <87366244+mariscalromeroalejandro@users.noreply.github.com>
2025-10-07 08:40:14 +0200

    create seeders

---
+++ QualityControl/lib/database/seeders/20250930071334-seed-chart-options.mjs
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+'use strict';
+
+/**
+ * Seed chart options
+ * @param {*} queryInterface - The query interface
+ */
+export const up = async (queryInterface) => {
+  await queryInterface.bulkInsert('chart_options', [
+    {
+      chart_id: 1,
+      option_id: 1,
+    },
+    {
+      chart_id: 1,
+      option_id: 2,
+    },
+    {
+      chart_id: 2,
+      option_id: 1,
+    },
+    {
+      chart_id: 3,
+      option_id: 3,
+    },
+    {
+      chart_id: 4,
+      option_id: 4,
+    },
+    {
+      chart_id: 5,
+      option_id: 5,
+    },
+    {
+      chart_id: 5,
+      option_id: 6,
+    },
+    {
+      chart_id: 6,
+      option_id: 7,
+    },
+  ], {});
+};
+
+export const down = async (queryInterface) => {
+  await queryInterface.sequelize.transaction(async (transaction) => {
+    await queryInterface.bulkDelete('chart_options', null, { transaction });
+  });
+};
