ab171f9917355db15ed323813fe7001a9b7e6041
dwedr <d.wedrocha@gmail.com>
2025-10-31 10:31:46 +0100

    feat: added license header for test files

--- Tokenization/backend/central-system/tests/TokensGetService.spec.ts
+++ Tokenization/backend/central-system/tests/TokensGetService.spec.ts
@@ -1,10 +1,27 @@
-import { TokensGetService } from '../src/services/TokensGetService';
+/**
+ * @license
+ * Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+ * See http://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+ * All rights not expressly granted are reserved.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
 
-describe('TokensGetService', () => {
-  it('maps tokens and truncates payload to last 5 chars', async () => {
-    const tokens = new Map<number, { tokenId: number; validity: string; payload: string }>([
-      [1, { tokenId: 1, validity: 'good', payload: 'abc12345' }],
-      [2, { tokenId: 2, validity: 'bad',  payload: 'payload2' }]
+import { TokensGetService } from "../src/services/TokensGetService";
+
+describe("TokensGetService", () => {
+  it("maps tokens and truncates payload to last 5 chars", async () => {
+    const tokens = new Map<
+      number,
+      { tokenId: number; validity: string; payload: string }
+    >([
+      [1, { tokenId: 1, validity: "good", payload: "abc12345" }],
+      [2, { tokenId: 2, validity: "bad", payload: "payload2" }],
     ]);
 
     const svc = new TokensGetService();
@@ -12,8 +29,8 @@ describe('TokensGetService', () => {
 
     // Assert
     expect(out).toEqual([
-      { tokenId: 1, validity: 'good', payload: '12345' },
-      { tokenId: 2, validity: 'bad',  payload: 'load2' }
+      { tokenId: 1, validity: "good", payload: "12345" },
+      { tokenId: 2, validity: "bad", payload: "load2" },
     ]);
   });
 });
