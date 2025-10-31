b3c0c39a4bbeaeb9d11688198baf2d2b249cdfe3
Jasper Houweling <jasper@familiehouweling.nl>
2025-10-31 11:49:24 +0100

    Make runmodetests more reliable, Fix Aliecs sync test race condition

--- QualityControl/test/lib/services/external/AliEcsSynchronizer.test.js
+++ QualityControl/test/lib/services/external/AliEcsSynchronizer.test.js
@@ -50,12 +50,13 @@ export const aliecsSynchronizerTestSuite = async () => {
   test('should emit a run track event when a valid run message is received', () => {
     const runNumber = 123;
     const transition = Transition.START_ACTIVITY;
-    const timestamp = { toNumber: () => Date.now() } ;
+    const timeNumber = Date.now();
+    const timestamp = { toNumber: () => timeNumber };
     aliecsSynchronizer._onRunMessage({ runEvent: { runNumber, transition }, timestamp });
     ok(eventEmitterMock.emit.called);
     deepStrictEqual(eventEmitterMock.emit.firstCall.args[0], EmitterKeys.RUN_TRACK);
     deepStrictEqual(eventEmitterMock.emit.firstCall.args[1], {
-      runNumber, transition, timestamp: timestamp.toNumber()
+      runNumber, transition, timestamp: timestamp.toNumber(),
     });
   });
 };
