9a27e24c36972fe37906c3dc4a769fd4b694fc17
Krzysztof Śliwiński <krzysztof.sliwinski@swmansion.com>
2025-11-03 10:26:19 +0100

    fix: prettier fix

--- Configuration/webapp/app/root.tsx
+++ Configuration/webapp/app/root.tsx
@@ -50,10 +50,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <PersistQueryClientProvider
-          client={queryClient}
-          persistOptions={{ persister }}
-        >
+        <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
           <MainLayout>
             <LeftDrawer>
               <ConfigNavigator />
@@ -97,9 +94,7 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   if (isRouteErrorResponse(error)) {
     message = error.status === 404 ? '404' : 'Error';
     details =
-      error.status === 404
-        ? 'The requested page could not be found.'
-        : error.statusText || details;
+      error.status === 404 ? 'The requested page could not be found.' : error.statusText || details;
   } else if (import.meta.env.DEV && error && error instanceof Error) {
     details = error.message;
     ({ stack } = error);
