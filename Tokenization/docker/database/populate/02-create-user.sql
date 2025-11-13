b2b3802f75e39ae5c3e0589d8d8efcc635da682e
dwedr <d.wedrocha@gmail.com>
2025-11-13 16:30:19 +0100

    fix: changed dev password for database

--- Tokenization/docker/database/populate/02-create-user.sql
+++ Tokenization/docker/database/populate/02-create-user.sql
@@ -1,4 +1,4 @@
-CREATE USER IF NOT EXISTS 'central-system'@'%' IDENTIFIED BY 'super_secret_password_for_dev_purposes_only';
+CREATE USER IF NOT EXISTS 'central-system'@'%' IDENTIFIED BY 'cern;
 
 GRANT ALL PRIVILEGES ON `tokenization`.* TO 'central-system'@'%';
 
