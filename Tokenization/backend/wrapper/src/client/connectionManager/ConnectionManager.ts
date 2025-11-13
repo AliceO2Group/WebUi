e20eea53e1b40dc4f8c72420e84cc206730da688
dwedr <d.wedrocha@gmail.com>
2025-11-13 18:30:22 +0100

    fix: reverted changes in other projects

---
+++ Tokenization/backend/wrapper/src/client/connectionManager/ConnectionManager.ts
@@ -0,0 +1,153 @@
+/**
+ * @license
+ * Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+ * See http://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+ * All rights not expressly granted are reserved.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+import * as grpc from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
+import { CentralConnection } from './CentralConnection';
+import { CentralCommandDispatcher } from './eventManagement/CentralCommandDispatcher';
+import { Connection } from '../connection/Connection';
+import { LogManager } from '@aliceo2/web-ui';
+import type { Command, CommandHandler } from 'models/commands.model';
+import type { DuplexMessageEvent } from '../../models/message.model';
+import { ConnectionDirection } from '../../models/message.model';
+
+/**
+ * @description Manages all the connection between clients and central system.
+ */
+/**
+ * Manages the lifecycle and connection logic for a gRPC client communicating with the central system.
+ *
+ * This class is responsible for:
+ * - Initializing the gRPC client using the provided proto definition and address.
+ * - Delegating stream handling to CentralConnection.
+ * - Managing sending/receiving connections to other clients.
+ *
+ * @remarks
+ * - `centralConnection`: Handles the duplex stream with the central gRPC server.
+ * - `centralDispatcher`: Dispatcher for central system events
+ * - `sendingConnections`: Map of active outbound connections.
+ * - `receivingConnections`: Map of active inbound connections.
+ */
+export class ConnectionManager {
+  private _logger = LogManager.getLogger('ConnectionManager');
+  private _centralDispatcher: CentralCommandDispatcher;
+  private _centralConnection: CentralConnection;
+  private _sendingConnections = new Map<string, Connection>();
+  private _receivingConnections = new Map<string, Connection>();
+
+  /**
+   * Initializes a new instance of the ConnectionManager class.
+   *
+   * This constructor sets up the gRPC client for communication with the central system.
+   *
+   * @param protoPath - The file path to the gRPC proto definition.
+   * @param centralAddress - The address of the central gRPC server (default: "localhost:50051").
+   */
+  constructor(protoPath: string, centralAddress: string = 'localhost:50051') {
+    const packageDef = protoLoader.loadSync(protoPath, {
+      keepCase: true,
+      longs: String,
+      enums: String,
+      defaults: true,
+      oneofs: true,
+    });
+
+    const proto = grpc.loadPackageDefinition(packageDef) as any;
+    const wrapper = proto.webui.tokenization;
+
+    const client = new wrapper.CentralSystem(centralAddress, grpc.credentials.createInsecure());
+
+    // Event dispatcher for central system events
+    this._centralDispatcher = new CentralCommandDispatcher();
+    this._centralConnection = new CentralConnection(client, this._centralDispatcher, centralAddress);
+  }
+
+  /**
+   * Registers new Command Handler for specific central event
+   * @param commandHandlers Array of event names and handler instances
+   */
+  registerCommandHandlers(
+    commandHandlers: {
+      event: DuplexMessageEvent;
+      handler: CommandHandler<Command>;
+    }[]
+  ): void {
+    commandHandlers.forEach(({ event, handler }) => {
+      this._centralDispatcher.register(event, handler);
+    });
+  }
+
+  /**
+   * Starts the connection to the central system.
+   */
+  connectToCentralSystem(): void {
+    this._centralConnection.start();
+  }
+
+  /**
+   * Disconnects from the central system.
+   */
+  disconnectFromCentralSystem(): void {
+    this._centralConnection.disconnect();
+  }
+
+  /**
+   * Creates new connection
+   * @param address Target (external) address of the connection
+   * @param direction Direction of connection
+   * @param token Optional token for connection
+   */
+  createNewConnection(address: string, direction: ConnectionDirection, token?: string) {
+    const conn = new Connection(token ?? '', address, direction);
+
+    if (direction === ConnectionDirection.RECEIVING) {
+      this._receivingConnections.set(address, conn);
+    } else {
+      this._sendingConnections.set(address, conn);
+    }
+
+    return conn;
+  }
+
+  /**
+   * Gets the connection instance by address.
+   * @returns{Connection} connection instance.
+   */
+  getConnectionByAddress(address: string, direction: ConnectionDirection): Connection | undefined {
+    switch (direction) {
+      case ConnectionDirection.SENDING:
+        return this._sendingConnections.get(address);
+      case ConnectionDirection.RECEIVING:
+        return this._receivingConnections.get(address);
+      default:
+        this._logger.errorMessage(`Invalid connection direction: ${direction}`);
+        return undefined;
+    }
+  }
+
+  /**
+   * Returns all saved connections.
+   *
+   * @returns An object containing the sending and receiving connections.
+   */
+  public get connections(): {
+    sending: Connection[];
+    receiving: Connection[];
+  } {
+    return {
+      sending: [...this._sendingConnections.values()],
+      receiving: [...this._receivingConnections.values()],
+    };
+  }
+}
