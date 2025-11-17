e5744271b989a7961ba89a1c5ea4b9904f5f21a0
Alex Janson <alex@hoofddorp.nl>
2025-11-17 11:42:48 +0100

    test: add test for the closing button

--- QualityControl/test/public/pages/object-tree.test.js
+++ QualityControl/test/public/pages/object-tree.test.js
@@ -79,6 +79,19 @@ export const objectTreePageTests = async (url, page, timeout = 5000, testParent)
     },
   );
 
+  await testParent.test(
+    'should close the object plot upon clicking the close button',
+    { timeout },
+    async () => {
+      await page.evaluate(() => document.querySelector('#close-button').click());
+      const selectedObject = await page.evaluate(() => model.object.selected);
+      const numberOfChildren = await page.evaluate(() =>
+        document.querySelector('section > div > div > div:nth-child(2)').children.length);
+      strictEqual(selectedObject, undefined);
+      strictEqual(numberOfChildren, 0);
+    }
+  )
+
   await testParent.test('should sort list of histograms by name in descending order', async () => {
     await page.locator('#sortTreeButton').click();
     const sortingByNameOptionPath = '#sortTreeButton > div > a:nth-child(2)';
