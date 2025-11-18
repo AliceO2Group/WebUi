8faf560145313c101155c37e442267fa7fab0ff9
Alex Janson <alex@hoofddorp.nl>
2025-11-18 11:18:19 +0100

    style: add jsdoc and fix eslint errors

--- QualityControl/public/common/object/objectInfoCard.js
+++ QualityControl/public/common/object/objectInfoCard.js
@@ -19,20 +19,6 @@ const SPECIFIC_KEY_LABELS = {
   id: 'ID (etag)',
 };
 
-const defaultKeyTransform = (key) => {
-  const spaced = key.replace(/([A-Z])/g, ' $1');
-  const titleCase = spaced.charAt(0).toUpperCase() + spaced.slice(1);
-  return titleCase;
-};
-
-const getUILabel = (key) => {
-  if (Object.hasOwn(SPECIFIC_KEY_LABELS, key)) {
-    return SPECIFIC_KEY_LABELS[key];
-  }
-
-  return defaultKeyTransform(key);
-};
-
 const DATE_FIELDS = ['validFrom', 'validUntil', 'createdAt', 'lastModified'];
 const TO_REMOVE_FIELDS = ['etag', 'qcObject', 'versions', 'name', 'location'];
 const HIGHLIGHTED_FIELDS = ['runNumber', 'runType', 'path', 'qcVersion'];
@@ -52,7 +38,7 @@ export const qcObjectInfoPanel = (model, qcObject, style = {}) =>
       KEY_TO_RENDER_FIRST,
       ...Object.keys(qcObject)
         .filter((key) =>
-          key !== KEY_TO_RENDER_FIRST && !TO_REMOVE_FIELDS.includes(key))
+          key !== KEY_TO_RENDER_FIRST && !TO_REMOVE_FIELDS.includes(key)),
     ]
       .map((key) => infoRow(model, key, qcObject[key])),
   ]);
@@ -66,15 +52,44 @@ export const qcObjectInfoPanel = (model, qcObject, style = {}) =>
  */
 const infoRow = (model, key, value) => {
   const highlightedClasses = HIGHLIGHTED_FIELDS.includes(key) ? '.info-row' : '';
-  const formattedValue = infoPretty(key, value)
+  const formattedValue = infoPretty(key, value);
 
   return h(`.flex-row.g2${highlightedClasses}`, [
     h('b.w-25.w-wrapped', getUILabel(key)),
     h('.w-75', {
       ...infoRowAttributes(model, formattedValue),
-      style: 'cursor: pointer; user-select: text;'
+      style: 'cursor: pointer; user-select: text;',
     }, formattedValue),
   ]);
+};
+
+/**
+ * Transforms a camelCase string into human-readable Title Case format,
+ * inserts a space before every uppercase letter and ensures the first
+ * character is capatilized
+ * @param {string} key - key of the object info
+ * @returns {string} - formatted label for the given key
+ */
+const defaultKeyTransform = (key) => {
+  const spaced = key.replace(/([A-Z])/g, ' $1');
+  const titleCase = spaced.charAt(0).toUpperCase() + spaced.slice(1);
+  return titleCase;
+};
+
+/**
+ * Retrieves the final UI-friendly label for given data key
+ * * Priority:
+ * 1. Manual override using `SPECIFIC_KEY_LABELS`
+ * 2. Use `defaultKeyTransform` to generate a label
+ * @param {string} key - key of the object info
+ * @returns {string} - formatted label for the given key
+ */
+const getUILabel = (key) => {
+  if (Object.hasOwn(SPECIFIC_KEY_LABELS, key)) {
+    return SPECIFIC_KEY_LABELS[key];
+  }
+
+  return defaultKeyTransform(key);
 };
 
 /**
@@ -94,6 +109,12 @@ const infoPretty = (key, value) => {
   return value;
 };
 
+/**
+ * Configure the info row vnode attributes
+ * @param {Model} model - root object of the framework
+ * @param {string|number|object|undefined} value - value of the object info
+ * @returns {object} - object containing the constructed vnode attributes
+ */
 const infoRowAttributes = (model, value) => {
   let clickTimeout = undefined;
   const DOUBLE_CLICK_DELAY = 300;
