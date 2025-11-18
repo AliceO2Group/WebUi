8f4106182a74e5897abf45e70aa261c87495e0a7
Alex Janson <alex@hoofddorp.nl>
2025-11-18 13:57:07 +0100

    fix: failing tests due to reordering of qc object info

--- QualityControl/test/public/pages/object-view-from-object-tree.test.js
+++ QualityControl/test/public/pages/object-view-from-object-tree.test.js
@@ -98,14 +98,14 @@ export const objectViewFromObjectTreeTests = async (url, page, timeout = 5000, t
         const commonSelectorPath = '.layout-selectable > div';
         const { title } = document.querySelector(`${commonSelectorPath} > div:nth-child(2) > div > button`);
         const infoCommonSelectorPath = `${commonSelectorPath} > div:nth-child(2) > div > div > div > div`;
-        const path = document.querySelector(`${infoCommonSelectorPath} > div:nth-child(2) > div > div`).innerText;
-        const pathTitle = document.querySelector(`${infoCommonSelectorPath} > div:nth-child(2) > b`).innerText;
+        const path = document.querySelector(`${infoCommonSelectorPath} > div:nth-child(1) > div > div`).innerText;
+        const pathTitle = document.querySelector(`${infoCommonSelectorPath} > div:nth-child(1) > b`).innerText;
         const lastModifiedTitle = document.querySelector(`${infoCommonSelectorPath} > div:nth-child(6) > b`).innerText;
         return { title, pathTitle, path, lastModifiedTitle };
       });
       strictEqual(result.title, 'View details about histogram');
-      strictEqual(result.pathTitle, 'path');
+      strictEqual(result.pathTitle, 'Path');
       strictEqual(result.path, 'qc/test/object/1');
-      strictEqual(result.lastModifiedTitle, 'lastModified');
+      strictEqual(result.lastModifiedTitle, 'Last Modified');
     });
 };
