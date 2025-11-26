99b6a495894b249a02cd885d1cd0d85b8d2c572b
crashdance <kevin11bakker@gmail.com>
2025-11-26 16:37:26 +0100

    fix: revert removed lines and object tree page timeout

--- QualityControl/test/public/pages/object-tree.test.js
+++ QualityControl/test/public/pages/object-tree.test.js
@@ -80,6 +80,66 @@ export const objectTreePageTests = async (url, page, timeout = 5000, testParent)
   );
 
   await testParent.test(
+    'should correctly render the path as first in the object info panel',
+    { timeout },
+    async () => {
+      const firstRowKey = await page.evaluate(() =>
+        document.querySelector('#qcObjectInfoPanel > div:first-child > b').textContent);
+      strictEqual(firstRowKey, 'Path');
+    },
+  );
+
+  await testParent.test(
+    'should contain four highlighted rows',
+    { timeout },
+    async () => {
+      const highlightedClasses = '.info-row.highlighted';
+      const rowCount = await page.evaluate((selector) =>
+        document.querySelectorAll(`#qcObjectInfoPanel > div${selector}`).length, highlightedClasses);
+      strictEqual(rowCount, 4);
+    },
+  );
+
+  await testParent.test(
+    'should copy the value of the element clicked to the clipboard',
+    { timeout },
+    async () => {
+      const context = page.browserContext();
+      await context.overridePermissions(url, ['clipboard-read', 'clipboard-write', 'clipboard-sanitized-write']);
+
+      await page.click('#qcObjectInfoPanel > div > div');
+
+      const clipboard = await page.evaluate(async () => {
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        return navigator.clipboard.readText();
+      });
+
+      strictEqual(clipboard, 'qc/test/object/1');
+      context.clearPermissionOverrides();
+    }
+  );
+
+  await testParent.test(
+    'should not copy the value of the clicked element if there is no value',
+    { timeout },
+    async () => {
+      const context = page.browserContext();
+      await context.overridePermissions(url, ['clipboard-read', 'clipboard-write', 'clipboard-sanitized-write']);
+
+      await page.click('#qcObjectInfoPanel > div > div'); // copy path
+      await page.click('#qcObjectInfoPanel > div:nth-child(7) > div'); // try to copy empty value
+
+      const clipboard = await page.evaluate(async () => {
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        return navigator.clipboard.readText();
+      });
+
+      strictEqual(clipboard, 'qc/test/object/1');
+      context.clearPermissionOverrides();
+    }
+  );
+
+  await testParent.test(
     'should close the object plot upon clicking the close button',
     { timeout },
     async () => {
