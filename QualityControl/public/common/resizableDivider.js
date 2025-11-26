366d5448298c49d6011e9c18d9c6d4ab7a5b5cea
crashdance <kevin11bakker@gmail.com>
2025-11-26 15:16:45 +0100

    fix: update license header and adjust divider width to 8px

--- QualityControl/public/common/resizableDivider.js
+++ QualityControl/public/common/resizableDivider.js
@@ -1,3 +1,17 @@
+/**
+ * @license
+ * Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+ * See http://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+ * All rights not expressly granted are reserved.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.p
+ */
+
 import { h } from '/js/src/index.js';
 
 /**
@@ -24,7 +38,7 @@ export const resizableDivider = (onResize) =>
           position: absolute;
           top: 0;
           height: 100%;
-          width: 6px;
+          width: 8px;
           background: rgba(0, 123, 255, 0.8);
           pointer-events: none;
           z-index: 1001;
