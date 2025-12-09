69c0ed22129368f5d383b43389d4d9f4396322e6
Krzysztof Śliwiński <krzysztof.sliwinski@swmansion.com>
2025-12-09 15:26:34 +0100

    chore: add saving configuration to consul

---
+++ Configuration/webapp/app/api/mutations/useConfigurationMutation.ts
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+ * See http://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+ * All rights not expressly granted are reserved.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axiosInstance from '../axiosInstance';
+import type { FormItem } from '~/components/form/Form';
+import { CONFIGURATION_QUERY_KEY } from '../query/useConfigurationQuery';
+import { CONFIGURATION_RESTRICTIONS_QUERY_KEY } from '../query/useConfigurationRestrictionsQuery';
+
+/**
+ * useConfigurationMutation hook
+ * Provides a mutation to save configuration data.
+ * @param {string} configurationName - The name of the configuration to save.
+ * @returns {UseMutationResult} The mutation result.
+ */
+export const useConfigurationMutation = (configurationName: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (configuration: FormItem) => {
+      const response = await axiosInstance.put<FormItem>(
+        `configurations/${configurationName}`,
+        JSON.stringify(configuration),
+      );
+      return response.data;
+    },
+    onSuccess: () => {
+      void queryClient.invalidateQueries({
+        queryKey: [CONFIGURATION_QUERY_KEY, configurationName],
+      });
+      void queryClient.invalidateQueries({
+        queryKey: [CONFIGURATION_RESTRICTIONS_QUERY_KEY, configurationName],
+      });
+    },
+  });
+};
--- Configuration/webapp/app/hooks/useConfigurationForm.tsx
+++ Configuration/webapp/app/hooks/useConfigurationForm.tsx
@@ -16,23 +16,30 @@ import { useEffect, useMemo } from 'react';
 import type { InputsType } from '~/routes/configuration';
 import { useForm, type KeepStateOptions, type SubmitHandler } from 'react-hook-form';
 import { getDefaultValuesFromConfigObject } from '~/components/form/utils/getDefaultValuesFromConfigObject';
+import { convertFormValuesToConfigObject } from '~/components/form/utils/convertFormValuesToConfigObject';
 import { useLocation } from 'react-router';
 import type { FormItem } from '~/components/form/Form';
+import { useConfigurationMutation } from '~/api/mutations/useConfigurationMutation';
 
 const RESET_PROPS: KeepStateOptions = { keepDirty: false };
 
 /**
  * useConfigurationForm hook
  * Provides form state management to child components.
- * @param {FormItem | undefined} configuration - The configuration object.
+ * @param {object} options - The options for the form.
+ * @param {FormItem | undefined} options.configuration - The configuration object.
+ * @param {string} options.configurationName - The name of the configuration.
  * @returns {FormContextValue} Form context value
  */
 export const useConfigurationForm = ({
   configuration,
+  configurationName,
 }: {
   configuration: FormItem | undefined;
+  configurationName: string;
 }) => {
   const { pathname } = useLocation();
+  const mutation = useConfigurationMutation(configurationName);
 
   const defaultValues = useMemo(
     () => getDefaultValuesFromConfigObject(configuration, pathname),
@@ -44,14 +51,11 @@ export const useConfigurationForm = ({
   });
 
   const onSubmit: SubmitHandler<InputsType> = (data) => {
-    // for now only logging the values
-    // eslint-disable-next-line no-console
-    console.log(data);
-    // eslint-disable-next-line no-console
-    console.log(getValues());
+    const configurationData = convertFormValuesToConfigObject(data, pathname);
+    mutation.mutate(configurationData);
   };
 
-  useEffect(() => reset(defaultValues, RESET_PROPS), [defaultValues]);
+  useEffect(() => reset(defaultValues, RESET_PROPS), [defaultValues, reset]);
 
   return {
     control,
