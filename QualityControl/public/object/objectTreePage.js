b5634c964b6f3165daec013bd3e48f788a926191
hehoon <100522372+hehoon@users.noreply.github.com>
2025-12-16 21:38:04 +0100

    Add `.25rem` left padding (`--space-xs`) to each tree row element

--- QualityControl/public/object/objectTreePage.js
+++ QualityControl/public/object/objectTreePage.js
@@ -211,7 +211,7 @@ function treeRow(model, tree, level = 0) {
       iconBarChart,
       className,
       {
-        paddingLeft: `${level}em`,
+        paddingLeft: `${level + 0.25}em`,
       },
     );
     rows.push(leaf);
@@ -225,7 +225,7 @@ function treeRow(model, tree, level = 0) {
       open ? iconCaretBottom : iconCaretRight,
       '',
       {
-        paddingLeft: `${level}em`,
+        paddingLeft: `${level + 0.25}em`,
       },
     );
     rows.push(branch);
