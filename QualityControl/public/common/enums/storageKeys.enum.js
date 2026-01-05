ba3675dd5da30026bc0724cb035c79ae347509e7
crashdance <kevin11bakker@gmail.com>
2026-01-05 12:39:42 +0100

    chore: remove unused storage key

--- QualityControl/public/common/enums/storageKeys.enum.js
+++ QualityControl/public/common/enums/storageKeys.enum.js
@@ -21,5 +21,4 @@ export const StorageKeysEnum = Object.freeze({
   OBJECT_VIEW_LEFT_PANEL_WIDTH: 'object-view-left-panel-width',
   OBJECT_VIEW_INFO_VISIBILITY_SETTING: 'object-view-info-visibility-setting',
   OBJECT_TREE_OPEN_NODES: 'object-tree-open-nodes',
-  OBJECT_TREE_FOCUSED_NODE: 'object-tree-focused-node',
 });
