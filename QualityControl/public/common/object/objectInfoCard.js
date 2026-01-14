a0e2b912e11ddd4139250ef2f2a74e97afad9c56
crashdance <kevin11bakker@gmail.com>
2026-01-14 13:05:35 +0100

    fix: correct typo in license comment

--- QualityControl/public/common/object/objectInfoCard.js
+++ QualityControl/public/common/object/objectInfoCard.js
@@ -9,7 +9,7 @@
  *
  * In applying this license CERN does not waive the privileges and immunities
  * granted to it by virtue of its status as an Intergovernmental Organization
- * or submit itself to any jurisdiction.p
+ * or submit itself to any jurisdiction.
  */
 
 import { h, isContextSecure } from '/js/src/index.js';
