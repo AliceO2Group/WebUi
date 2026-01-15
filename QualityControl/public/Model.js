bc49b48b5f2311cc7024277f9cdd238e6900666d
hehoon <100522372+hehoon@users.noreply.github.com>
2026-01-15 16:36:20 +0100

    [OGUI-1854] Notify user via native browser notification when a new run has started (#3258)
    - Added a websocket to broadcast new runs from backend towards front-end
    - Added a menu item in the profile (click on top left profile button to open menu) to enable browser notifications.
      - Front-end browser checks whether the user enabled this settings before displaying a notification

--- QualityControl/public/Model.js
+++ QualityControl/public/Model.js
@@ -29,6 +29,7 @@ import AboutViewModel from './pages/aboutView/AboutViewModel.js';
 import LayoutListModel from './pages/layoutListView/model/LayoutListModel.js';
 import { RequestFields } from './common/RequestFields.enum.js';
 import FilterModel from './common/filters/model/FilterModel.js';
+import NotificationRunStartModel from './common/notifications/model/NotificationRunStartModel.js';
 
 /**
  * Represents the application's state and actions as a class
@@ -84,6 +85,9 @@ export default class Model extends Observable {
     this.ws = new WebSocketClient();
     this.ws.addListener('authed', this.handleWSAuthed.bind(this));
     this.ws.addListener('close', this.handleWSClose.bind(this));
+
+    this.notificationRunStartModel = new NotificationRunStartModel(this);
+    this.notificationRunStartModel.bubbleTo(this);
 
     this.initModel();
   }
