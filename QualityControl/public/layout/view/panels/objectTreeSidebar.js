70148d1078d3fd0aa9412e6dcdb07d425d6e05f3
crashdance <kevin11bakker@gmail.com>
2026-01-18 23:07:48 +0100

    fix: update virtual table layout to use 'main' context in object tree sidebar

--- QualityControl/public/layout/view/panels/objectTreeSidebar.js
+++ QualityControl/public/layout/view/panels/objectTreeSidebar.js
@@ -40,7 +40,7 @@ export default (model) =>
         searchForm(model),
         h('.flex-column.flex-grow', {}, [
           searchInput.trim() !== ''
-            ? virtualTable(model, 'side', objectsToDisplay, { maxHeightRows: 500 })
+            ? virtualTable(model, 'main', objectsToDisplay)
             : h('.scroll-y', treeTable(model)),
         ]),
         objectPreview(model),
