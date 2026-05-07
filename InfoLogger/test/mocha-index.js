6337802b52cdf0fc3d9a1822bb81e8f1fe166628
George Raduta <george.raduta@cern.ch>
2026-05-07 09:34:17 +0200

    Fix eslint in mocha-index

--- InfoLogger/test/mocha-index.js
+++ InfoLogger/test/mocha-index.js
@@ -10,15 +10,17 @@
  * In applying this license CERN does not waive the privileges and immunities
  * granted to it by virtue of its status as an Intergovernmental Organization
  * or submit itself to any jurisdiction.
-*/
+ */
 
 /* eslint-disable no-console */
+/* eslint-disable init-declarations */
+
 const puppeteer = require('puppeteer');
 const assert = require('assert');
-const {spawn} = require('child_process');
+const { spawn } = require('child_process');
 
 const config = require('./test-config.js');
-const {createServer, closeServer} = require('./live-simulator/infoLoggerServer.js');
+const { createServer, closeServer } = require('./live-simulator/infoLoggerServer.js');
 
 // APIs:
 // https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md
@@ -28,16 +30,16 @@ const {createServer, closeServer} = require('./live-simulator/infoLoggerServer.j
 // Network and rendering can have delays this can leads to random failures
 // if they are tested just after their initialization.
 
-describe('InfoLogger', function() {
+describe('InfoLogger', function () {
   let browser;
   let page;
   let subprocess; // web-server runs into a subprocess
   let subprocessOutput = '';
   let ilgServer;
-  
+
   this.timeout(30000);
   this.slow(1000);
-  
+
   const baseUrl = `http://${config.http.hostname}:${config.http.port}/`;
 
   before(async () => {
@@ -62,25 +64,30 @@ describe('InfoLogger', function() {
     ilgServer = createServer();
 
     // Start web-server in background
-    subprocess = spawn('node', ['index.js', 'test/test-config.js'], {stdio: 'pipe'});
-    subprocess.stdout.on('data', (chunk) => subprocessOutput += chunk.toString());
-    subprocess.stderr.on('data', (chunk) => subprocessOutput += chunk.toString());
-    subprocess.on('error', (error) => console.error(`Server failed due to: ${error}`))
+    subprocess = spawn('node', ['index.js', 'test/test-config.js'], { stdio: 'pipe' });
+    subprocess.stdout.on('data', (chunk) => {
+      subprocessOutput += chunk.toString();
+    });
+    subprocess.stderr.on('data', (chunk) => {
+      subprocessOutput += chunk.toString();
+    });
+    subprocess.on('error', (error) => console.error(`Server failed due to: ${error}`));
 
     // Start browser to test UI
-    browser = await puppeteer.launch({headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     page = await browser.newPage();
 
     // Export page and configurations for the other mocha files
     exports.page = page;
-    exports.helpers = {baseUrl, jwt: config.jwt};
+
+    exports.helpers = { baseUrl, jwt: config.jwt };
   });
 
   it('should load first page "/"', async () => {
     // try many times until backend server is ready
     for (let i = 0; i < 10; i++) {
       try {
-        await page.goto(baseUrl, {waitUntil: 'networkidle0'});
+        await page.goto(baseUrl, { waitUntil: 'networkidle0' });
         break; // connection ok, this test passed
       } catch (e) {
         if (e.message.includes('net::ERR_CONNECTION_REFUSED')) {
@@ -92,8 +99,8 @@ describe('InfoLogger', function() {
     }
   });
 
-  it('should have redirected to default page "/?q={"severity":{"in":"I W E F"}}"', async function() {
-    await page.goto(baseUrl, {waitUntil: 'networkidle0'});
+  it('should have redirected to default page "/?q={"severity":{"in":"I W E F"}}"', async () => {
+    await page.goto(baseUrl, { waitUntil: 'networkidle0' });
     const location = await page.evaluate(() => window.location);
     const search = decodeURIComponent(location.search);
 
@@ -115,7 +122,5 @@ describe('InfoLogger', function() {
     console.log('---------------------------------------------');
     subprocess.kill();
     closeServer(ilgServer);
-
   });
 });
-
