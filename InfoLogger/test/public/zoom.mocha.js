80eba669a70a8a0564b8f82a3f938790bb8cb87f
Isaac Hill <71404865+isaachilly@users.noreply.github.com>
2026-05-21 09:19:23 +0200

    [OGUI-1216] Revert to original, still unsure why failing

--- InfoLogger/test/public/zoom.mocha.js
+++ InfoLogger/test/public/zoom.mocha.js
@@ -194,9 +194,7 @@ describe('Zoom test-suite', async () => {
   });
 
   it('should have reset button enabled when zoomed', async () => {
-    await page.keyboard.down('Control');
-    await page.keyboard.press('Equal');
-    await page.keyboard.up('Control');
+    await page.evaluate(() => window.model.zoomIn());
 
     await page.waitForFunction(() => {
       const resetBtn = document.querySelector('#reset-zoom-button');
