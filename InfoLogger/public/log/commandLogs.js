39c7b98a0b8f584b72e543313a0e25371a4e3f81
Isaac Hill <71404865+isaachilly@users.noreply.github.com>
2026-05-22 16:32:34 +0200

    [OGUI-1216] Extract zoom controls into zoomButtonGroup

--- InfoLogger/public/log/commandLogs.js
+++ InfoLogger/public/log/commandLogs.js
@@ -66,27 +66,7 @@ export default (model) => [
     title: 'Go to last log message (ALT + down arrow)',
   }, '↓'),
   downloadButtonGroup(model.log),
-  h('.btn-group', [
-    h('button.btn', {
-      onclick: () => model.zoom.zoomOut(),
-      disabled: model.zoom.level <= model.zoom.min,
-      id: 'zoom-out-button',
-      title: 'Zoom out (Ctrl/Cmd + -)',
-      // span makes the icons off center vertically, but just a div makes the height wrong
-    }, h('span', { style: 'font-size:0.8em;' }, iconMinus())),
-    h('button.btn', {
-      onclick: () => model.zoom.resetZoom(),
-      disabled: model.zoom.level === 1,
-      id: 'reset-zoom-button',
-      title: 'Reset zoom',
-    }, h('span', { style: 'font-size:0.9em;' }, iconMagnifyingGlass())),
-    h('button.btn', {
-      onclick: () => model.zoom.zoomIn(),
-      disabled: model.zoom.level >= model.zoom.max,
-      id: 'zoom-in-button',
-      title: 'Zoom in (Ctrl/Cmd + +)',
-    }, h('span', { style: 'font-size:0.8em;' }, iconPlus())),
-  ]),
+  zoomButtonGroup(model.zoom),
 ];
 
 /**
@@ -177,6 +157,33 @@ const downloadButtonGroup = (logModel) =>
         onclick: () => logModel.removeLogDownloadContent(),
       }, 'Visible Logs Only'),
     ]),
+  ]);
+
+/**
+ * Group of buttons for controlling log table zoom level
+ * @param {Zoom} zoom - the zoom model
+ * @returns {vnode} - the view of the zoom button group
+ */
+const zoomButtonGroup = (zoom) =>
+  h('.btn-group', [
+    h('button.btn', {
+      onclick: () => zoom.zoomOut(),
+      disabled: zoom.level <= zoom.min,
+      id: 'zoom-out-button',
+      title: 'Zoom out (Ctrl/Cmd + -)',
+    }, h('span', { style: 'font-size:0.8em' }, iconMinus())),
+    h('button.btn', {
+      onclick: () => zoom.resetZoom(),
+      disabled: zoom.level === 1,
+      id: 'reset-zoom-button',
+      title: 'Reset zoom',
+    }, h('span', { style: 'font-size:0.9em' }, iconMagnifyingGlass())),
+    h('button.btn', {
+      onclick: () => zoom.zoomIn(),
+      disabled: zoom.level >= zoom.max,
+      id: 'zoom-in-button',
+      title: 'Zoom in (Ctrl/Cmd + +)',
+    }, h('span', { style: 'font-size:0.8em' }, iconPlus())),
   ]);
 
 /**
