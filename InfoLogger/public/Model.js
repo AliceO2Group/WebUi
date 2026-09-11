f6302da22f875fc5359aaa7914bc48157acaaaed
Isaac Hill <71404865+isaachilly@users.noreply.github.com>
2026-09-11 18:06:36 +0200

    Document better the shareable URL getter

--- InfoLogger/public/Model.js
+++ InfoLogger/public/Model.js
@@ -392,6 +392,7 @@ export default class Model extends Observable {
 
   /**
    * Get the shareable URL with the current filter query string
+   * Built from the model rather than the address bar, which only updates on a 500 ms rate limit.
    * @returns {string} - the shareable URL
    */
   get shareableURL() {
