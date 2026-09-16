d99a0a3475c576b6bdb745e9b4c5718510deb834
Isaac Hill <71404865+isaachilly@users.noreply.github.com>
2026-09-16 18:03:24 +0200

    Center copy URL button text

--- InfoLogger/public/log/commandLogs.js
+++ InfoLogger/public/log/commandLogs.js
@@ -88,6 +88,7 @@ const copyURLButton = (url, showNotification) => h(
     id: 'url',
     className: '',
     style: { minWidth: '100px' },
+    contentClassName: 'justify-center',
     onFailure: ({ error }) => showNotification(`Could not copy URL: ${error}`, 'danger', 3000),
   },
   'Copy URL',
