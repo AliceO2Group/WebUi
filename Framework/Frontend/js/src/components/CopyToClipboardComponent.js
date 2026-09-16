63bc8ca5174438823a50576a3ea2b9d849fc8079
Isaac Hill <71404865+isaachilly@users.noreply.github.com>
2026-09-16 15:54:03 +0200

    Add loading class to copy button example className attr

--- Framework/Frontend/js/src/components/CopyToClipboardComponent.js
+++ Framework/Frontend/js/src/components/CopyToClipboardComponent.js
@@ -112,7 +112,7 @@ export class CopyToClipboardComponent extends StatefulComponent {
    * @param {object} vnode.attrs The attributes passed to the component.
    * @param {string} [vnode.attrs.value=''] The text to be copied to the clipboard.
    * @param {string} [vnode.attrs.id] The unique identifier for the copy button will become 'copy-{id}'.
-   * @param {string} [vnode.attrs.className='btn-primary'] The CSS classes to be applied to the copy button.
+   * @param {string} [vnode.attrs.className='btn-primary loading'] The CSS classes to be applied to the copy button.
    * @param {string|object} [vnode.attrs.style] The inline styles to be applied to the copy button.
    * @param {(error: Error) => void} [vnode.attrs.onFailure] The callback function to be invoked if copying to the clipboard fails.
    * @returns {Component} The copyToClipboard button component
