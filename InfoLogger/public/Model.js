31ec70c7a8c1a2e327402cdbb27f0ab31d1681c5
Isaac Hill <71404865+isaachilly@users.noreply.github.com>
2026-09-21 16:29:15 +0200

    Remove old and unneeded arg

--- InfoLogger/public/Model.js
+++ InfoLogger/public/Model.js
@@ -408,7 +408,7 @@ export default class Model extends Observable {
     try {
       this.log.liveStart();
       // the buttons should read the model.log.activeMode and decide what to do
-      setToLiveMode(this);
+      setToLiveMode();
       this.log.enableAutoScroll();
       setBrowserTabTitle(`${window.ILG.name} LIVE`);
       this.notify();
